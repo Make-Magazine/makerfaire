5240930bca6acb0c5c9419614c3282f2b0345042
Jay Z <jayzalowitz@gmail.com>
2014-09-12 13:38:36 -0700

    limit to this faire

--- api/v2/category/index.php
+++ api/v2/category/index.php
@@ -13,6 +13,18 @@
 // Stop any direct calls to this file
 defined( 'ABSPATH' ) or die( 'This file cannot be called directly!' );
 
+//Make a silly call that gets around the need to hit the database with a tremdously complicated join and just puts most of the operation on cpu
+$all_events = file_get_contents("http://makerfaire.com/query/?key=d184iC3I5cw4eeC&type=entity&faire=".sanitize_title($faire));
+$all_events = json_decode($all_events,1);
+$allowed_categories = array();
+foreach ($all_events['entity'] as $event) {
+  foreach ($event['category_id_refs'] as $category_id) {
+    if(!in_array($category_id, $allowed_categories)){
+     array_push($allowed_categories, $category_id);
+    }
+  }
+}
+
 $taxonomies = array(
 	'category',
 	'post_tag',
@@ -45,8 +57,10 @@ if ( $type == 'category') {
 		// REQUIRED: Category Name
 		$venue['name'] = html_entity_decode( esc_js( $term->name ) );
 
-		// Put the application into our list of apps
+		// Put the application into our list of apps if in allowed array
+		if(in_array($venue['id'], $allowed_categories)){
 		array_push( $venues, $venue );
+		}
 	}
 
 	$merged = array_merge( $header, array( 'entity' => $venues, ) );
