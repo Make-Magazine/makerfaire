db13c80bdd48c3da258860399dc90a409660bbc3
Bill Olson <bolson@makermedia.com>
2014-10-06 16:34:03 -0700

    Show newsletter signup drop down

--- footer.php
+++ footer.php
@@ -36,7 +36,7 @@
 				                <form class="form-inline" action="http://makermedia.createsend.com/t/r/s/jjuruj/" method="post" id="subForm">
 				                        <input type="text" placeholder="Enter your email" class="news-signup" name="cm-jjuruj-jjuruj" id="jjuruj-jjuruj">
 				                        <input type="image" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/go.png" value="Go!" class="btn">
-				                        <div class="control-group hidden">
+				                        <div class="control-group">
 				                                <h5>Call For Makers</h5>
 				                                <label for="fielddduult-0"><input id="fielddduult-0" name="cm-fo-dduult" value="621683" type="checkbox" checked/> Inform me About the Call for Makers</label><br>
 				                                <h5>Preferred Faire?</h5>
