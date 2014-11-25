f38a3aa9cdf952c609bec367c33db812b0ca901f
Bill Olson <bolson@makermedia.com>
2014-11-25 13:23:15 -0800

    shed promo for sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,14 @@
 <div class="span4">
+	<div class="center">
+		<a href="http://www.makershed.com/collections/make-gear" target="_blank">
+			<img src="https://makerfaire.files.wordpress.com/2014/11/mf-300x250_holidaymerch_v1.jpg" />
+		</a>
+	</div>
 
          <div class="center twitter">
+         	<h3 style="margin-top:15px;"><a href="https://twitter.com/search?q=%23makerfaire">Tweets about #makerfaire</a></h3>
           <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
           <a class="twitter-timeline" href="https://twitter.com/search?q=%23makerfaire" data-widget-id="322225978648698880">Tweets about "#makerfaire"</a>
-          <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
           <a href="https://twitter.com/makerfaire" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @makerfaire</a>
         </div>
 
