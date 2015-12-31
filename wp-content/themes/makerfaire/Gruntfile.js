f55c3634fb06ded4bdd033a930461857e54e2907
Wesley Wilson <wes@wesww.com>
2015-12-30 16:08:32 -0800

    removing less manifest file, handling with match expression in grunt task

--- wp-content/themes/makerfaire/Gruntfile.js
+++ wp-content/themes/makerfaire/Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
           compress: true
         },
         files: {
-          'css/style.css': 'less/style.less',
+          'css/style.css': ['less/global.less', 'less/**/*.less'],
         }
       }
     },
