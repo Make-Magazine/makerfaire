2c61d7b696ffab7525f12db0f9d6ee4481554253
Wesley Wilson <wes@wesww.com>
2016-01-05 13:38:30 -0800

    added dev task with src line number comments for less

--- wp-content/themes/makerfaire/Gruntfile.js
+++ wp-content/themes/makerfaire/Gruntfile.js
@@ -5,7 +5,16 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     // Compile the less to css
     less: {
-      development: {
+      dev: {
+        options: {
+          compress: false,
+          dumpLineNumbers: 'comments'
+        },
+        files: {
+          'css/style.css': ['less/global.less', 'less/**/*.less'],
+        }
+      },
+      prod: {
         options: {
           compress: true
         },
@@ -30,9 +39,13 @@ module.exports = function(grunt) {
     },
     // Watch for changes on save and livereload
     watch: {
-      noreload: {
+      dev: {
         files: ['less/**/*.less', 'js/src/*.js'],
-        tasks: ['less', 'concat']
+        tasks: ['less:dev', 'concat']
+      },
+      prod: {
+        files: ['less/**/*.less', 'js/src/*.js'],
+        tasks: ['less:prod', 'concat']
       },
       reload: {
         files: ['less/**/*.less', 'js/**/*.js'],
@@ -49,7 +62,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   // Register the tasks with Grunt
   // To only watch for less changes and process without browser reload type in "grunt"
-  grunt.registerTask('default', ['less', 'concat', 'watch:noreload']);
+  grunt.registerTask('default', ['less:prod', 'concat', 'watch:prod']);
+  // To only watch for less changes and process without browser reload type in "grunt"
+  grunt.registerTask('dev', ['less:dev', 'concat', 'watch:dev']);
   // To watch for less changes and process them with livereload type in "grunt reload"
   grunt.registerTask('reload', ['less', 'watch:reload']);
 };
