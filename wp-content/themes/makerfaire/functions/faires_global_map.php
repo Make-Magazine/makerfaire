5c1637be4f5e7ec9c785161f1c797144bc59be12
Wesley Wilson <wes@wesww.com>
2016-02-11 12:18:03 -0800

    cleanup

--- wp-content/themes/makerfaire/functions/faires_global_map.php
+++ wp-content/themes/makerfaire/functions/faires_global_map.php
@@ -1,24 +1,5 @@
 <?php
 // Faires data API
-function get_faires_data() {
-  $data = TEMPLATEPATH . '/functions/faires_global_map/faires.json';
-  header('Content-Type: application/json');
-  include_once $data;
-  exit;
-}
-add_action('wp_ajax_get_faires_data', 'get_faires_data');
-add_action('wp_ajax_nopriv_get_faires_data', 'get_faires_data');
-
-// Faires data API
-function get_faires_data_kml() {
-  $data = TEMPLATEPATH . '/functions/faires_global_map/faires.kml';
-  include_once $data;
-  exit;
-}
-add_action('wp_ajax_get_faires_data_kml', 'get_faires_data_kml');
-add_action('wp_ajax_nopriv_get_faires_data_kml', 'get_faires_data_kml');
-
-// Faires data API
 function get_faires_map_data() {
   $data = TEMPLATEPATH . '/functions/faires_global_map/faires_map_data.json';
   include_once $data;
