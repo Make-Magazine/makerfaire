5be0e131d86ce5eb3bba55f9d8ac92cdd4e1484f
Alicia Williams <alicia.williams130@gmail.com>
2016-03-23 11:11:08 -0600

    changing location comment to confirmation comment per kate

--- wp-content/themes/makerfaire/functions.php
+++ wp-content/themes/makerfaire/functions.php
@@ -146,7 +146,7 @@ function load_admin_scripts() {
   wp_enqueue_script( 'admin-scripts', get_stylesheet_directory_uri() . '/js/built-admin-scripts.js', array( 'jquery' ) );
   wp_enqueue_script( 'sack' );
   //styles
-  wp_enqueue_style( 'make-bootstrap', get_stylesheet_directory_uri() . '/css/admin.bootstrap.min.css' );
+  wp_enqueue_style( 'make-bootstrap', get_stylesheet_directory_uri() . '/css/admin.bootstrap.css' );
   wp_enqueue_style('jquery-datetimepicker-css',  get_stylesheet_directory_uri() . '/css/jquery.datetimepicker.css');
   wp_enqueue_style('wp-admin-style',  'http://makerfaire.staging.wpengine.com/wp-admin/load-styles.php?c=0&dir=ltr&load=dashicons,admin-bar,wp-admin,buttons,wp-auth-check&ver=1.35');
   wp_enqueue_style('made-admin-style',  get_stylesheet_directory_uri() . '/css/make.admin.css');
@@ -1768,8 +1768,8 @@ function mf_custom_merge_tags($merge_tags, $form_id, $fields, $element_id) {
       $merge_tags[] = array('label' => 'Attribute - '.$result->category, 'tag' => '{EA_'.$result->ID.'}');
     }
 
-    //add merge tag for Attention field - Location Comment
-    $merge_tags[] = array('label' => 'Location Comment', 'tag' => '{LOC_COMMENT}');
+    //add merge tag for Attention field - Confirmation Comment
+    $merge_tags[] = array('label' => 'Confirmation Comment', 'tag' => '{CONF_COMMENT}');
 
     return $merge_tags;
 }
