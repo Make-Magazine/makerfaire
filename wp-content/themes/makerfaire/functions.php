05d18bb0f11d40f1fd2b7f4e82361240edeba97b
Rich Haynie <rhaynie@slashdotmedia.com>
2016-03-23 13:46:33 -0600

    Adding SMTP settings to functions for phpmailer.

--- wp-content/themes/makerfaire/functions.php
+++ wp-content/themes/makerfaire/functions.php
@@ -2544,3 +2544,34 @@ function mf_custom_export_entries() {
   GFExport::page_footer();
 
 }
+
+/**
+ * This function will connect wp_mail to your authenticated
+ * SMTP server. This improves reliability of wp_mail, and 
+ * avoids many potential problems.
+ */
+add_action( 'phpmailer_init', 'send_smtp_email' );
+function send_smtp_email( $phpmailer ) {
+
+	// Define that we are sending with SMTP
+	$phpmailer->isSMTP();
+
+	// The hostname of the mail server
+	$phpmailer->Host = "smtp.mandrillapp.com";
+
+	// Use SMTP authentication (true|false)
+	$phpmailer->SMTPAuth = true;
+
+	// SMTP port number - likely to be 25, 465 or 587
+	$phpmailer->Port = "587";
+
+	// Username to use for SMTP authentication
+	$phpmailer->Username = "webmaster@makermedia.com";
+
+	// Password to use for SMTP authentication
+	$phpmailer->Password = "VyprCy78ZO0LRNwCTMVn2Q";
+
+	// Encryption system to use - ssl or tls
+  $phpmailer->SMTPSecure = "";
+
+}
