ece44ec5eee9b535dbf28a06fac758a334820c5b
Alicia Williams <alicia.williams130@gmail.com>
2016-03-24 17:20:37 -0600

    maker class changes

--- wp-content/themes/makerfaire/classes/gf-helper.php
+++ wp-content/themes/makerfaire/classes/gf-helper.php
@@ -311,3 +311,18 @@ function set_export_values( $value, $form_id, $field_id, $lead ) {
     }
     return $value;
 }
+
+function createGUID($id){
+
+        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+        $charid = strtoupper(md5(uniqid($id, true)));
+        $hyphen = chr(45);// "-"
+        $uuid = chr(123)// "{"
+            .substr($charid, 0, 8).$hyphen
+            .substr($charid, 8, 4).$hyphen
+            .substr($charid,12, 4).$hyphen
+            .substr($charid,16, 4).$hyphen
+            .substr($charid,20,12)
+            .chr(125);// "}"
+        return $uuid;
+}
