7d7c4e28ae37f94fb573c8171777e2d9b171b3b9
Alicia Williams <alicia.williams130@gmail.com>
2016-05-13 17:23:46 -0600

    tableTag changes

--- wp-content/themes/makerfaire/fpdi/tabletag.php
+++ wp-content/themes/makerfaire/fpdi/tabletag.php
@@ -20,10 +20,6 @@ class PDF extends FPDF{
   function Header(){
     global $root;
     $faire = (isset($_GET['faire']) && $_GET['faire']!='' ? $_GET['faire'].'-':'');
-   // $image = $root.'/wp-content/themes/makerfaire/images/'.$faire.'maker_sign.png';
-    // Logo
-   // $this->Image($image, 0, 0, $this->w, $this->h);
-    // Arial bold 15
     $this->SetFont('Benton Sans','B',15);
   }
 }
@@ -34,8 +30,10 @@ $pdf->AddFont('Benton Sans','B', 'bentonsans-bold-webfont.php');
 $pdf->AddFont('Benton Sans','', 'bentonsans-regular-webfont.php');
 $pdf->AddPage('L',array(139.7,215.9));
 $pdf->SetFont('Benton Sans','',12);
+$pdf->SetMargins(3,3);
 $pdf->SetFillColor(255,255,255);
 
+$pdf->SetAutoPageBreak(false);
 //get the entry-id, if one isn't set return an error
 if(isset($_GET['eid']) && $_GET['eid']!=''){
   $faire = (isset($_GET['faire']) && $_GET['faire']!='' ? $_GET['faire']:'');
@@ -65,69 +63,89 @@ if(isset($_GET['eid']) && $_GET['eid']!=''){
 
 function createOutput($entry_id,$pdf){
     $entry = GFAPI::get_entry( $entry_id );
-    $project_title = (isset($entry['151']) ? filterText((string)$entry['151']) : '');
 
-    $project_title  = preg_replace('/\v+|\\\[rn]/','<br/>',$project_title);
-
-    // Project ID
-    $pdf->SetFont('Benton Sans','',12);
-    $pdf->setTextColor(168,170,172);
-    $pdf->SetXY(240, 20);
-    $pdf->MultiCell(115, 10, $entry_id,0,'L');
-
-    // Project Title
+    //entry ID
     $pdf->setTextColor(0);
-    $pdf->SetXY(50, 25);
-
-    //auto adjust the font so the text will fit
-    $pdf->SetFont( 'Benton Sans','B',105);
+    $pdf->SetXY(2, 27);
+    $pdf->SetFont( 'Benton Sans','B',174);
     $lineHeight = 60*0.2645833333333*1.3;
 
-    /* Output the title at the required font size */
-    $pdf->MultiCell(0, $lineHeight, $entry_id,0,'L');
+    /* Output the entry ID at the required font size */
+    $pdf->MultiCell(0, $lineHeight, $entry_id,0,'C');
 
-    //field 22 - QRCode
-    $project_photo = 'https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl='.urlencode('http://www.makerfaire.com/maker/entry/'.$entry_id.'/');
-    $pdf->Image($project_photo,155,70,null,null,image_type_to_extension(IMAGETYPE_PNG,false));
-
-    //Resource data?
-    $pdf->SetXY(160, 107);
-    $pdf->SetFont( 'Benton Sans','',12);
-    $lineHeight = 14*0.2645833333333*1.3;
-    $pdf->MultiCell(0, $lineHeight, "What goes here? \nResource data?",0,'L');
 
     //Project Title
-    $pdf->SetXY(90, 50);
-    $x = 15;    // set the starting font size
-    $pdf->SetFont( 'Benton Sans','B',15);
+    $project_title = (isset($entry['151']) ? filterText((string)$entry['151']) : '');
+    $project_title  = preg_replace('/\v+|\\\[rn]/','<br/>',$project_title);
+    $pdf->SetXY(4, 62);
+    $x = 25;    // set the starting font size
+    $pdf->SetFont( 'Benton Sans','B',25);
 
     /* Cycle thru decreasing the font size until it's width is lower than the max width */
     while( $pdf->GetStringWidth( utf8_decode( $project_title)) > 400 ){
         $x--;   // Decrease the variable which holds the font size
         $pdf->SetFont( 'Benton Sans','B',$x);
     }
-    $lineHeight = $x*0.2645833333333*1.3;
+    $lineHeight = $x*0.2645833333333*1.4;
 
     /* Output the title at the required font size */
-    $pdf->MultiCell(0, $lineHeight, $project_title,0,'L');
+    $pdf->MultiCell(0, $lineHeight, $project_title,0,'C');
 
 
-     
 
+    //field 22 - QRCode
+    $project_photo = 'https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl='.urlencode('http://www.makerfaire.com/maker/entry/'.$entry_id.'/');
+    $pdf->Image($project_photo,7,80,null,null,image_type_to_extension(IMAGETYPE_PNG,false));
+
+    //qr text
+    $pdf->SetXY(10, 120);
+    $pdf->SetFont( 'Benton Sans','',12);
+    $lineHeight = 13*0.2645833333333*1.3;
+    $qrText = "Want to be found on the Maker Faire mobile app?\nScan this code at your booth location and follow the prompts.";
+    $pdf->MultiCell(0, $lineHeight, $qrText,0,'L');
+
+    // Location Info
+    /* retSubAreaByEntry: function to return location information by entry id
+     * returned as array keyed by location id
+     * fields: area, subarea, nicename, location, start_dt and end_dt
+     */
+    $locTable = retSubAreaByEntry($entry_id);
+    $disp = '';
+    foreach($locTable as $location){
+      $disp .= $location['area'].': '.($location['nicename']!=''?$location['nicename']:$location['subarea']).': '.$location['location'];
+    }
+
+    $pdf->SetXY(100, 85);
+    $pdf->SetFont( 'Benton Sans','B',14);
+    $lineHeight = 14*0.2645833333333*1.3;
+    $pdf->Cell( 0, 10, $disp, 0, 0, 'R' );
+
+    //Resource information
+    $pdf->SetXY(100, 93);
+    $pdf->SetFont('Benton Sans','',14);
+    $lineHeight = 15*0.2645833333333*1.3;
+    /* Return array of resource information for lead*/
+    $entResources = retResByEntry($entry_id);
+    $eightFt = 0;
+    $sixFt   = 0;
+    $chairs  = 0;
+    $elect   = 0;
+
+    $disp = "";
+    //loop thru and display chairs, table and electrical
+    foreach($entResources as $resource){
+      $disp .= $resource['item'] . ' - ' .$resource['type'].': '. $resource['qty']."\n";
+    }
+    $pdf->MultiCell(0, $lineHeight, $disp,0,'R');
 }
 
-function filterText($text)
-{
-
-
+function filterText($text){
 	$string = iconv('UTF-8', 'windows-1252',$text);
 
 	//now translate any unicode stuff...
 	$conv = array(
       "&amp;" => '&');
 	return strtr($string, $conv);
-
-
 }
 
 ?>
--- wp-content/themes/makerfaire/functions/wpAdminHelper.php
+++ wp-content/themes/makerfaire/functions/wpAdminHelper.php
@@ -77,3 +77,86 @@ function retSubAreaByFaire($faire) {
 	return $subAreaArr;
 }
 
+//returns array of area/subarea by entry
+function retSubAreaByEntry($entry_id) {
+  global $wpdb;
+  $sql = "select  location.entry_id, area.area, subarea.subarea, location.subarea_id,
+                  subarea.nicename, location.location, schedule.start_dt, schedule.end_dt,
+                  location.id as location_id
+            from  wp_mf_location location
+            join  wp_mf_faire_subarea subarea
+                ON  location.subarea_id = subarea.ID
+            join wp_mf_faire_area area
+                ON subarea.area_id = area.ID
+            left join wp_mf_schedule schedule
+                ON location.ID = schedule.location_id
+             where location.entry_id=$entry_id";
+  $results = $wpdb->get_results($sql);
+
+  //return array key = location ID
+  // fields: area, subarea, nicename, location, start_dt and end_dt
+  $retArray = array();
+  if($wpdb->num_rows > 0){
+    foreach($results as $row){
+      $retArray[$row->location_id] = array(
+            'area'      =>  $row->area,
+            'subarea'   =>  $row->subarea,
+            'nicename'  =>  $row->nicename,
+            'location'  =>  $row->location,
+            'start_dt'  =>  $row->start_dt,
+            'end_dt'    =>  $row->end_dt
+          );
+    }
+  }
+
+	return $retArray;
+}
+
+//used to find the current resource information for a specific entry
+/* Return array of resource information for lead */
+function retResByEntry($entry_id) {
+  global $wpdb;
+  $return = array();
+
+  if($entry_id!=''){
+    //gather resource data
+    $sql = "SELECT er.qty, type, wp_rmt_resource_categories.category as item, wp_rmt_resources.token "
+            . "FROM `wp_rmt_entry_resources` er, wp_rmt_resources, wp_rmt_resource_categories "
+            . "where er.resource_id = wp_rmt_resources.ID "
+            . "and resource_category_id = wp_rmt_resource_categories.ID  "
+            . "and er.entry_id = ".$entry_id." order by item ASC, type ASC";
+    $results = $wpdb->get_results($sql);
+    foreach($results as $result){
+      $return[]= array('item'=>$result->item, 'type'=>$result->type, 'qty'=> $result->qty,'token'=>$result->token);
+    }
+  }
+  return $return;
+}
+
+
+//function to create table tags by faire
+function genTableTags($faire) {
+  global $wpdb;
+  //error_log('faire is '.$faire);
+  //find the exhibit and sponsor forms by faire
+  $sql = "select form_ids from wp_mf_faire where faire='".$faire."'";
+  $formIds = $wpdb->get_var($sql);
+  //remove any spaces
+  $formIds = str_replace(' ', '', $formIds);
+  $forms = explode(",", $formIds);
+  foreach($forms as $formId){
+    //error_log($formId .' is a type of '.$form['form_type'] );
+    $form = GFAPI::get_form($formId);
+    if($form['form_type']=='Exhibit' || $form['form_type']=='Sponsor' || $form['form_type']=='Startup Sponsor'){
+      $search_criteria['status'] = 'active';
+      $entries         = GFAPI::get_entries( $formId, $search_criteria );
+      foreach($entries as $entry){
+        $url = TEMPLATEPATH.'/fpdi/tabletag.php?eid='.$entry['id'].'&faire='.$faire.'&type=save';
+        error_log($url);
+        file_get_contents($url);
+      }
+    }
+  }
+}
+
+add_action( 'gen_table_tags', 'genTableTags', 10, 1 );
