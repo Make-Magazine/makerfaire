4db2c8cd4929268190b460dfcec691d3cf821275
Rich Haynie <rhaynie@slashdotmedia.com>
2016-05-18 14:25:08 -0700

    Fixing flow and text.

--- wp-content/themes/makerfaire/page-onsite-checkin.php
+++ wp-content/themes/makerfaire/page-onsite-checkin.php
@@ -57,38 +57,40 @@ $project_title = (isset($entry['151']) ? (string) $entry['151'] : '');
   }
 
   jQuery(window).load(function () {
+       
     BootstrapDialog.confirm({
-      title: 'Welcome to MakerFaire! First Question...',
-      message: 'Are you <?= $entryID ?>, <?= $project_title ?>?',
+      title: '<h2>Welcome to MakerFaire! First Question...</h2>',
+      message: '<h1>Are you <?= $entryID ?>, <?= $project_title ?>?</h1>',
       type: BootstrapDialog.TYPE_INFO, // <-- Default value is BootstrapDialog.TYPE_PRIMARY
       closable: false, // <-- Default value is false
       draggable: false, // <-- Default value is false
-      btnCancelLabel: 'No', // <-- Default value is 'Cancel',
-      btnOKLabel: 'Yes', // <-- Default value is 'OK',
+      btnCancelLabel: '<h1>No</h1>', // <-- Default value is 'Cancel',
+      btnOKLabel: '<h1>Yes</h1>', // <-- Default value is 'OK',
       btnOKClass: 'btn-info', // <-- If you didn't specify it, dialog type will be used,
       callback: function (result) {
         // result will be true if button was click, while it will be false if users close the dialog directly.
         if (result) {
           BootstrapDialog.confirm({
-            title: 'Next Question...',
-            message: 'Are you standing in your location at Maker Faire Bay Area 2016?',
+            title: '<h2>Next Question...</h2>',
+            message: '<h1>Are you standing in your location at Maker Faire Bay Area 2016?</h1>',
             type: BootstrapDialog.TYPE_INFO, // <-- Default value is BootstrapDialog.TYPE_PRIMARY
             closable: false, // <-- Default value is false
             draggable: false, // <-- Default value is false
-            btnCancelLabel: 'No', // <-- Default value is 'Cancel',
-            btnOKLabel: 'Yes', // <-- Default value is 'OK',
+            btnCancelLabel: '<h1>No</h1>', // <-- Default value is 'Cancel',
+            btnOKLabel: '<h1>Yes</h1>', // <-- Default value is 'OK',
             btnOKClass: 'btn-info', // <-- If you didn't specify it, dialog type will be used,
             callback: function (result) {
               // result will be true if button was click, while it will be false if users close the dialog directly.
               if (result) {
                 getLocation();
+                jQuery("#entrygeolocate").show();
               } else {
-                jQuery("#entrygeolocate").hide();
+                jQuery("#error").show();
               }
             }
           });
         } else {
-          jQuery("#entrygeolocate").hide();
+          jQuery("#optout").show();
 
         }
       }
@@ -102,31 +104,34 @@ $project_title = (isset($entry['151']) ? (string) $entry['151'] : '');
 
   <div class="row">
 
-    <div id="entrygeolocate" class="content col-md-8">
+    <div id="entrygeolocate" style="display:none" class="content col-md-8">
       <div class="jumbotron">
         <h1>Welcome, <?= $entryID ?>!</h1>
+        <h1>Your pin will show on the MakerFaire App in 15-30 minutes.</h1>
+        <h2>Thank you for pinning your location.</h2>
 
           <input id="entryID"   name="entryID"   type="hidden" value ="<?php echo $entryID; ?>" />
-            <div class="input-group">
-              <span class="input-group-addon" id="latitude-addon1">latitude</span>
-              <input id="latitude" type="text" class="form-control" placeholder="latitude" aria-describedby="latitude-addon1">
-            </div>
-            <br />
-            <div class="input-group">
-              <span class="input-group-addon" id="longitude-addon1">longitude</span>
-              <input  id="longitude" name="longitude"  type="text" class="form-control" placeholder="latitude" aria-describedby="longitude-addon1">
-            </div>
-            <br />
+            <input id="latitude" type="hidden" class="form-control" placeholder="latitude" aria-describedby="latitude-addon1">
+              <input  id="longitude" name="longitude"  type="hidden" class="form-control" placeholder="latitude" aria-describedby="longitude-addon1">
             <div class="pull-left flagship-icon-link">
             <a class="flagship-icon-link" href="/app/">
               <img src="/wp-content/uploads/2016/01/icon-mobile.png" width="40px" scale="0">
-              Download the Maker Faire App
+              If you don't have it...download the MakerFaire App
             </a>
             </div>  
-            <input type="submit" class="btn btn-primary btn-lg" role="button" Submit Location />
-      
       </div>
+      
     </div><!--content-->
+    <div id="error" style="display:none" class="content col-md-8">
+      <div class="jumbotron">
+        <h1>Sorry, <?= $entryID ?>.  There was a problem and support has been notified.</h1>
+      </div>
+      </div>
+       <div id="optout" style="display:none" class="content col-md-8">
+      <div class="jumbotron">
+        <h1>Rescan your code later, after your situation changes.</h1>
+      </div>
+      </div>
   </div><!--row-->
 </div><!--container-->
 
