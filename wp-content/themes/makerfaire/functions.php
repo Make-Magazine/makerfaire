6b900f03c3c59d5a2b93a32c034e120e613abdd1
Alicia Williams <alicia@makermedia.com>
2016-05-30 17:12:50 -0600

    adding NMF forms to the pre renderer for auto populate.  we need to find a better way to do this :(

--- wp-content/themes/makerfaire/functions.php
+++ wp-content/themes/makerfaire/functions.php
@@ -1296,16 +1296,21 @@ function custom_validation( $validation_result ) {
   $validation_result['form'] = $form;
   return $validation_result;
 };
-add_filter( 'gform_pre_render_35', 'populate_html' );
-add_filter( 'gform_pre_render_36', 'populate_html' );
-add_filter( 'gform_pre_render_37', 'populate_html' );
-add_filter( 'gform_pre_render_38', 'populate_html' );
-add_filter( 'gform_pre_render_39', 'populate_html' );
-add_filter( 'gform_pre_render_55', 'populate_html' );
-add_filter( 'gform_pre_render_56', 'populate_html' );
-add_filter( 'gform_pre_render_57', 'populate_html' );
-add_filter( 'gform_pre_render_58', 'populate_html' );
-add_filter( 'gform_pre_render_59', 'populate_html' );
+add_filter( 'gform_pre_render_35', 'populate_html' ); //BA16 FSP
+add_filter( 'gform_pre_render_36', 'populate_html' ); //BA16 GSP
+add_filter( 'gform_pre_render_37', 'populate_html' ); //NY15 Special Request
+add_filter( 'gform_pre_render_38', 'populate_html' ); //NY15 ETA and Heavy EQMT
+add_filter( 'gform_pre_render_39', 'populate_html' ); //BA16 Maker Shipping
+
+add_filter( 'gform_pre_render_55', 'populate_html' ); //Payment Form Master
+add_filter( 'gform_pre_render_56', 'populate_html' ); //Payment Form Test
+add_filter( 'gform_pre_render_57', 'populate_html' ); //BA16 Payment Form Commercial Maker
+add_filter( 'gform_pre_render_58', 'populate_html' ); //BA16 Payment Form Health Permit Fee
+add_filter( 'gform_pre_render_59', 'populate_html' ); //BA16 Payment Form Non Profit
+
+add_filter( 'gform_pre_render_82', 'populate_html' ); //NMF16 - FSP [form 82]
+add_filter( 'gform_pre_render_83', 'populate_html' ); //NMF16 - GSP [form 83]
+add_filter( 'gform_pre_render_84', 'populate_html' ); //NMF16 - shipping forms [84]
 
 
 /*
@@ -1328,9 +1333,9 @@ function populate_html( $form ) {
     $fieldIDarr['exhibit-contain-fire']  = 83;
     $fieldIDarr['interactive-exhibit']   = 84;
     $fieldIDarr['fire-safety-issues']    = 85;
-    $fieldIDarr['serving-food']    = 44;
-    $fieldIDarr['you-are-entity']    = 45;
-    $fieldIDarr['plans-type']    = "55";
+    $fieldIDarr['serving-food']          = 44;
+    $fieldIDarr['you-are-entity']        = 45;
+    $fieldIDarr['plans-type']            = "55";
 
     //find the project name for submitted entry-id
     $entry = GFAPI::get_entry( $entry_id );
@@ -1350,7 +1355,6 @@ function populate_html( $form ) {
       }
     }
   }
-
   return($form);
 }
 
