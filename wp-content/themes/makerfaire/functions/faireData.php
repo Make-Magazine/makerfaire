6cad9dc8c62a70669c8a1db35d186754e831c30e
Alicia Williams <alicia@makermedia.com>
2016-10-04 13:58:06 -0600

    changes to create new shortcode to display sponsor slider

---
+++ wp-content/themes/makerfaire/functions/faireData.php
@@ -0,0 +1,27 @@
+<?php
+
+function get_faire_by_shortid($faire_id='') {
+  global $wpdb;
+  
+  $faireData = array();
+  if($faire_id!=''){
+    //based on maker email retrieve maker information from the DB
+    $results = $wpdb->get_row("SELECT * FROM wp_mf_faire WHERE faire='$faire_id'", ARRAY_A );
+;
+    //if faire found
+    if ( null !== $results ) {
+      $faireData['faire_name']        = $results['faire_name'];
+      $faireData['url_path']          = $results['url_path'];
+      $faireData['faire_location']    = $results['faire_location'];
+      $faireData['form_ids']          = $results['form_ids'];
+      $faireData['non_public_forms']  = $results['non_public_forms'];
+      $faireData['start_dt']          = $results['start_dt'];
+      $faireData['end_dt']            = $results['end_dt'];
+      $faireData['time_zone']         = $results['time_zone'];
+      $faireData['show_sched']        = $results['show_sched'];
+      $faireData['faire_logo']        = $results['faire_logo'];
+    }
+  }
+  return $faireData;
+}
+
--- wp-content/themes/makerfaire/functions/register-shortcodes.php
+++ wp-content/themes/makerfaire/functions/register-shortcodes.php
@@ -302,3 +302,176 @@ function  createExportLink($atts){
 }
 
 add_shortcode( 'mfExportLink', 'createExportLink' );
+
+/*
+ * Used to create the sponsor slideshow
+ */
+function createSponsSlide($atts) {
+  extract( shortcode_atts( array(
+    'faire_id'  => '',
+    'url'   => ''
+  ), $atts ) );
+
+  //if($faire_id=='')  // required field - quit there was an error
+  //if($url=='')    // required field - quit there was an error
+
+  $faireData = get_faire_by_shortid($faire_id);
+  $postid  = url_to_postid( $url );
+
+  ?>
+  <!-- IF CUSTOM FIELD FOR SPONSOR SLIDER HAS A URL THEN SHOW THAT URL'S SPONSORS -->
+  <?php if( have_rows('goldsmith_sponsors', $postid) || have_rows('silversmith_sponsors', $postid) || have_rows('coppersmith_sponsors', $postid) || have_rows('media_sponsors', $postid) ): ?>
+  <div class="sponsor-slide">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-7">
+          <h4 class="sponsor-slide-title"><?php echo $faireData['faire_name'];?> Sponsors: <span class="sponsor-slide-cat"></span></h4>
+        </div>
+        <div class="col-sm-5">
+          <h5><a href="/sponsors">Become a sponsor</a></h5>
+          <h5><a href="<?php echo $url;?>">All sponsors</a></h5>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12">
+
+          <div id="carousel-sponsors-slider" class="carousel slide" data-ride="carousel">
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+              <!-- GOLDSMITH SPONSORS -->
+              <?php if( have_rows('goldsmith_sponsors', $postid) ): ?>
+              <div class="item">
+                <div class="row spnosors-row">
+                  <div class="col-xs-12">
+                    <h3 class="sponsors-type text-center">GOLDSMITH</h3>
+                      <div class="sponsors-box">
+                      <?php
+                        while( have_rows('goldsmith_sponsors', $postid) ): the_row();
+                          $sub_field_1 = get_sub_field('image'); //Photo
+                          $sub_field_2 = get_sub_field('url'); //URL
+
+                          echo '<div class="sponsors-box-md">';
+                          if( get_sub_field('url') ):
+                            echo '<a href="' . $sub_field_2 . '" target="_blank">';
+                          endif;
+                          echo '<img src="' . $sub_field_1 . '" alt="Maker Faire sponsor logo" class="img-responsive" />';
+                          if( get_sub_field('url') ):
+                            echo '</a>';
+                          endif;
+                          echo '</div>';
+                        endwhile; ?>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <?php endif; ?>
+
+              <!-- SILVERSMITH SPONSORS -->
+              <?php if( have_rows('silversmith_sponsors', $postid) ): ?>
+              <div class="item">
+                <div class="row spnosors-row">
+                  <div class="col-xs-12">
+                    <h3 class="sponsors-type text-center">SILVERSMITH</h3>
+                      <div class="sponsors-box">
+                      <?php
+                        while( have_rows('silversmith_sponsors', $postid) ): the_row();
+                          $sub_field_1 = get_sub_field('image'); //Photo
+                          $sub_field_2 = get_sub_field('url'); //URL
+
+                          echo '<div class="sponsors-box-md">';
+                          if( get_sub_field('url') ):
+                            echo '<a href="' . $sub_field_2 . '" target="_blank">';
+                          endif;
+                          echo '<img src="' . $sub_field_1 . '" alt="Maker Faire sponsor logo" class="img-responsive" />';
+                          if( get_sub_field('url') ):
+                            echo '</a>';
+                          endif;
+                          echo '</div>';
+                        endwhile; ?>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <?php endif; ?>
+
+              <!-- COPPERSMITH SPONSORS -->
+              <?php if( have_rows('coppersmith_sponsors', $postid) ): ?>
+              <div class="item">
+                <div class="row spnosors-row">
+                  <div class="col-xs-12">
+                    <h3 class="sponsors-type text-center">COPPERSMITH</h3>
+                      <div class="sponsors-box">
+                      <?php
+                        while( have_rows('coppersmith_sponsors', $postid) ): the_row();
+                          $sub_field_1 = get_sub_field('image'); //Photo
+                          $sub_field_2 = get_sub_field('url'); //URL
+
+                          echo '<div class="sponsors-box-md">';
+                          if( get_sub_field('url') ):
+                            echo '<a href="' . $sub_field_2 . '" target="_blank">';
+                          endif;
+                          echo '<img src="' . $sub_field_1 . '" alt="Maker Faire sponsor logo" class="img-responsive" />';
+                          if( get_sub_field('url') ):
+                            echo '</a>';
+                          endif;
+                          echo '</div>';
+                        endwhile; ?>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <?php endif; ?>
+
+              <!-- MEDIA SPONSORS -->
+              <?php if( have_rows('media_sponsors', $postid) ): ?>
+              <div class="item">
+                <div class="row spnosors-row">
+                  <div class="col-xs-12">
+                    <h3 class="sponsors-type text-center">MEDIA</h3>
+                      <div class="sponsors-box">
+                      <?php
+                        while( have_rows('media_sponsors', $postid) ): the_row();
+                          $sub_field_1 = get_sub_field('image'); //Photo
+                          $sub_field_2 = get_sub_field('url'); //URL
+
+                          echo '<div class="sponsors-box-md">';
+                          if( get_sub_field('url') ):
+                            echo '<a href="' . $sub_field_2 . '" target="_blank">';
+                          endif;
+                          echo '<img src="' . $sub_field_1 . '" alt="Maker Faire sponsor logo" class="img-responsive" />';
+                          if( get_sub_field('url') ):
+                            echo '</a>';
+                          endif;
+                          echo '</div>';
+                        endwhile; ?>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <?php endif; ?>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php endif; ?>
+  <!-- END SPONSOR SLIDER -->
+
+
+<script>
+// Update the sponsor slide title each time the slide changes
+jQuery('.carousel-inner .item:first-child').addClass('active');
+jQuery(function() {
+  var title = jQuery('.item.active .sponsors-type').html();
+  jQuery('.sponsor-slide-cat').text(title);
+  jQuery('#carousel-sponsors-slider').on('slid.bs.carousel', function () {
+    var title = jQuery('.item.active .sponsors-type').html();
+    jQuery('.sponsor-slide-cat').text(title);
+  })
+});
+</script>
+  <?php
+}
+add_shortcode('sponsor_slideshow', 'createSponsSlide');
