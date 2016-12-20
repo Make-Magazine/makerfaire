18e01ac3def60b432bc778d315a13bdf6d675d2e
Alicia Williams <alicia@makermedia.com>
2016-12-20 11:28:24 -0700

    MF-1343 CM Field is not resetting based on conditions
    Changes to the fee indicator dynamic set logic to have a higher priority than others who call that action.  this will cause the change to be done before the change report is done.  also made a gv change as the form it was sending did not contain the admin fields

--- wp-content/themes/makerfaire/functions/gravity_forms/gravityforms_dynamic_populate.php
+++ wp-content/themes/makerfaire/functions/gravity_forms/gravityforms_dynamic_populate.php
@@ -3,8 +3,8 @@
  * This function is to dynamically populate any form field based on parameter name
  */
 
-add_action( 'gform_entry_post_save', 'calc_field_ind', 10, 2);
-add_action('gform_after_update_entry', 'calc_field_pre_process', 10, 3 );
+add_action('gform_entry_post_save', 'calc_field_ind', 5, 2);
+add_action('gform_after_update_entry', 'calc_field_pre_process', 5, 3 );
 function calc_field_pre_process($form,$entry_id,$orig_entry=array()){
   $entry = GFAPI::get_entry(esc_attr($entry_id));
   calc_field_ind($entry, $form);
@@ -18,8 +18,10 @@ function calc_field_pre_process($form,$entry_id,$orig_entry=array()){
 function calc_field_ind($entry, $form) {
   $entry_id = $entry['id'];
   $form_id  = $entry['form_id'];
+
   foreach ($form['fields'] as &$field) {
     if ($field->inputName == 'field-ind') {
+
       $field_id = $field->id;
       if(GFCommon::evaluate_conditional_logic( rgar( $field, 'conditionalLogic' ), $form, $entry ) ){
         $updField = 'Yes';
