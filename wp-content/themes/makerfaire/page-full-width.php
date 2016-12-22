263d899c640183e9aaca8c8b71b32b85f7f2990c
David Beauchamp <dbeauchamp@makermedia.com>
2016-12-22 14:04:10 -0800

    Adding demo content to the Media Center pages

--- wp-content/themes/makerfaire/page-full-width.php
+++ wp-content/themes/makerfaire/page-full-width.php
@@ -54,7 +54,7 @@ get_header(); ?>
   </header>
 
 
-  <div class="container">
+  <div class="container mcp-body">
 
     <div class="row">
 
@@ -67,7 +67,7 @@ get_header(); ?>
               <a href="#a" data-toggle="tab">Welcome</a>
             </li>
             <li>
-              <a href="#b" data-toggle="tab">Media Registration</a>
+              <a href="#b" data-toggle="tab">Event Registration</a>
             </li>
             <li>
               <a href="#c" data-toggle="tab">Fast Facts</a>
@@ -90,15 +90,145 @@ get_header(); ?>
           </ul>
 
           <article class="tab-content col-md-9">
-            <div class="tab-pane active" id="a">Lorem ipsum dolor sit amet, charetra varius rci quis tortor imperdiet venenatis quam sit amet vulputate. Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero.</div>
-            <div class="tab-pane" id="b">Secondo sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. Aliquam in felis sit amet augue.</div>
-            <div class="tab-pane" id="c">Thirdamuno, ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum auctor accumsan. Duis pharetra
-            varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt condimentum vitae. </div>
-            <div class="tab-pane" id="d">dddddddd</div>
-            <div class="tab-pane" id="e">eeeeeeee</div>
-            <div class="tab-pane" id="f">ffffffff</div>
-            <div class="tab-pane" id="g">gggggggg</div>
-            <div class="tab-pane" id="h">hhhhhhhh</div>
+            <div class="tab-pane mcp-welcome active" id="a">
+              <h2><strong>What</strong> We Do</h2>
+              <p class="lead">Suspendisse ut mollis nibh. Nam velit est, bibendum quis fringilla sit amet, cursus vel erat. Aenean blandit, urna lobortis varius ullamcorper, massa eros vulputate ipsum, eu lacinia nunc nunc eleifend ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo nisl, auctor at lorem at, rutrum tristique nisl.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac condimentum eros, at porta orci. Cras dictum sodales nunc sed vulputate. Suspendisse at arcu in risus consectetur maximus. Donec eu fringilla massa, ut convallis nunc. Mauris tempor, justo vel pulvinar finibus, magna ipsum pulvinar enim, et eleifend velit nisl ac felis. Vivamus porta leo sit amet nibh sagittis, nec faucibus neque consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris tempus, nisl finibus euismod molestie, ligula mi convallis erat, ut tincidunt sem enim tincidunt ex. Nulla ligula libero, consectetur in faucibus eu, lobortis non justo.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac condimentum eros, at porta orci. Cras dictum sodales nunc sed vulputate. Suspendisse at arcu in risus consectetur maximus. Donec eu fringilla massa, ut convallis nunc. Mauris tempor, justo vel pulvinar finibus, magna ipsum pulvinar enim, et eleifend velit nisl ac felis. Vivamus porta leo sit amet nibh sagittis, nec faucibus neque consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris tempus, nisl finibus euismod molestie, ligula mi convallis erat, ut tincidunt sem enim tincidunt ex. Nulla ligula libero, consectetur in faucibus eu, lobortis non justo.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac condimentum eros, at porta orci. Cras dictum sodales nunc sed vulputate. Suspendisse at arcu in risus consectetur maximus. Donec eu fringilla massa, ut convallis nunc. Mauris tempor, justo vel pulvinar finibus, magna ipsum pulvinar enim, et eleifend velit nisl ac felis. Vivamus porta leo sit amet nibh sagittis, nec faucibus neque consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris tempus, nisl finibus euismod molestie, ligula mi convallis erat, ut tincidunt sem enim tincidunt ex. Nulla ligula libero, consectetur in faucibus eu, lobortis non justo.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac condimentum eros, at porta orci. Cras dictum sodales nunc sed vulputate. Suspendisse at arcu in risus consectetur maximus. Donec eu fringilla massa, ut convallis nunc. Mauris tempor, justo vel pulvinar finibus, magna ipsum pulvinar enim, et eleifend velit nisl ac felis. Vivamus porta leo sit amet nibh sagittis, nec faucibus neque consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris tempus, nisl finibus euismod molestie, ligula mi convallis erat, ut tincidunt sem enim tincidunt ex. Nulla ligula libero, consectetur in faucibus eu, lobortis non justo.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac condimentum eros, at porta orci. Cras dictum sodales nunc sed vulputate. Suspendisse at arcu in risus consectetur maximus. Donec eu fringilla massa, ut convallis nunc. Mauris tempor, justo vel pulvinar finibus, magna ipsum pulvinar enim, et eleifend velit nisl ac felis. Vivamus porta leo sit amet nibh sagittis, nec faucibus neque consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris tempus, nisl finibus euismod molestie, ligula mi convallis erat, ut tincidunt sem enim tincidunt ex. Nulla ligula libero, consectetur in faucibus eu, lobortis non justo.</p>
+            </div>
+
+            <div class="tab-pane mcp-registration" id="b">
+              <h2><strong>Event</strong> Registration</h2>
+              <p class="lead">Suspendisse ut mollis nibh. Nam velit est, bibendum quis fringilla sit amet, cursus vel erat. Aenean blandit, urna lobortis varius ullamcorper, massa eros vulputate ipsum, eu lacinia nunc nunc eleifend ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo nisl, auctor at lorem at, rutrum tristique nisl.</p>
+
+            </div>
+            <div class="tab-pane mcp-facts" id="c">
+              <h2><strong>Fast</strong> Facts</h2>
+              <p class="lead">Suspendisse ut mollis nibh. Nam velit est, bibendum quis fringilla sit amet, cursus vel erat. Aenean blandit, urna lobortis varius ullamcorper, massa eros vulputate ipsum, eu lacinia nunc nunc eleifend ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo nisl, auctor at lorem at, rutrum tristique nisl.</p>
+              <img src="https://i1.wp.com/makerfaire.com/wp-content/uploads/2013/05/Maker-Faire-Growth-Chart-2015.jpg" class="img-responsive" alt="Maker Faire Ten Year Growth" />
+            </div>
+
+            <div class="tab-pane mcp-news" id="d">
+              <h2><strong>Recent</strong> Press</h2>
+
+              <div class="media">
+                <div class="media-left">
+                  <i class="fa fa-video-camera fa-lg" aria-hidden="true"></i>
+                </div>
+                <div class="media-body">
+                  <a href="#">
+                    <h4 class="media-heading">Video: Maker Faire Bay Area 2016</h4>
+                  </a>
+                  <p>mercurynews.com, May 20, 2016</p>
+                </div>
+              </div>
+
+              <div class="media">
+                <div class="media-left">
+                  <i class="fa fa-newspaper-o fa-lg" aria-hidden="true"></i>
+                </div>
+                <div class="media-body">
+                  <a href="#">
+                    <h4 class="media-heading">Video: Maker Faire Bay Area 2016</h4>
+                  </a>
+                  <p>mercurynews.com, May 20, 2016</p>
+                </div>
+              </div>
+
+              <div class="media">
+                <div class="media-left">
+                  <i class="fa fa-video-camera fa-lg" aria-hidden="true"></i>
+                </div>
+                <div class="media-body">
+                  <a href="#">
+                    <h4 class="media-heading">Video: Maker Faire Bay Area 2016</h4>
+                  </a>
+                  <p>mercurynews.com, May 20, 2016</p>
+                </div>
+              </div>
+
+              <div class="media">
+                <div class="media-left">
+                  <i class="fa fa-newspaper-o fa-lg" aria-hidden="true"></i>
+                </div>
+                <div class="media-body">
+                  <a href="#">
+                    <h4 class="media-heading">Video: Maker Faire Bay Area 2016</h4>
+                  </a>
+                  <p>mercurynews.com, May 20, 2016</p>
+                </div>
+              </div>
+
+              <div class="media">
+                <div class="media-left">
+                  <i class="fa fa-video-camera fa-lg" aria-hidden="true"></i>
+                </div>
+                <div class="media-body">
+                  <a href="#">
+                    <h4 class="media-heading">Video: Maker Faire Bay Area 2016</h4>
+                  </a>
+                  <p>mercurynews.com, May 20, 2016</p>
+                </div>
+              </div>
+
+              <div class="media">
+                <div class="media-left">
+                  <i class="fa fa-newspaper-o fa-lg" aria-hidden="true"></i>
+                </div>
+                <div class="media-body">
+                  <a href="#">
+                    <h4 class="media-heading">Video: Maker Faire Bay Area 2016</h4>
+                  </a>
+                  <p>mercurynews.com, May 20, 2016</p>
+                </div>
+              </div>
+              
+              <div class="mcp-news-btm-border"></div>
+
+              <div class="mcp-news-notworthy">
+                <h3>Noteworthy</h3>
+                <a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a>
+                <a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a>
+                <a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a>
+                <a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a>
+                <a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a>
+                <a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a>
+                <a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a>
+              </div>
+            </div>
+
+            <div class="tab-pane mcp-newsroom" id="e">
+              <h2><strong>Sample</strong> Title</h2>
+              <p class="lead">Suspendisse ut mollis nibh. Nam velit est, bibendum quis fringilla sit amet, cursus vel erat. Aenean blandit, urna lobortis varius ullamcorper, massa eros vulputate ipsum, eu lacinia nunc nunc eleifend ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo nisl, auctor at lorem at, rutrum tristique nisl.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac condimentum eros, at porta orci. Cras dictum sodales nunc sed vulputate. Suspendisse at arcu in risus consectetur maximus. Donec eu fringilla massa, ut convallis nunc. Mauris tempor, justo vel pulvinar finibus, magna ipsum pulvinar enim, et eleifend velit nisl ac felis. Vivamus porta leo sit amet nibh sagittis, nec faucibus neque consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris tempus, nisl finibus euismod molestie, ligula mi convallis erat, ut tincidunt sem enim tincidunt ex. Nulla ligula libero, consectetur in faucibus eu, lobortis non justo.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac condimentum eros, at porta orci. Cras dictum sodales nunc sed vulputate. Suspendisse at arcu in risus consectetur maximus. Donec eu fringilla massa, ut convallis nunc. Mauris tempor, justo vel pulvinar finibus, magna ipsum pulvinar enim, et eleifend velit nisl ac felis. Vivamus porta leo sit amet nibh sagittis, nec faucibus neque consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris tempus, nisl finibus euismod molestie, ligula mi convallis erat, ut tincidunt sem enim tincidunt ex. Nulla ligula libero, consectetur in faucibus eu, lobortis non justo.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac condimentum eros, at porta orci. Cras dictum sodales nunc sed vulputate. Suspendisse at arcu in risus consectetur maximus. Donec eu fringilla massa, ut convallis nunc. Mauris tempor, justo vel pulvinar finibus, magna ipsum pulvinar enim, et eleifend velit nisl ac felis. Vivamus porta leo sit amet nibh sagittis, nec faucibus neque consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris tempus, nisl finibus euismod molestie, ligula mi convallis erat, ut tincidunt sem enim tincidunt ex. Nulla ligula libero, consectetur in faucibus eu, lobortis non justo.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac condimentum eros, at porta orci. Cras dictum sodales nunc sed vulputate. Suspendisse at arcu in risus consectetur maximus. Donec eu fringilla massa, ut convallis nunc. Mauris tempor, justo vel pulvinar finibus, magna ipsum pulvinar enim, et eleifend velit nisl ac felis. Vivamus porta leo sit amet nibh sagittis, nec faucibus neque consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris tempus, nisl finibus euismod molestie, ligula mi convallis erat, ut tincidunt sem enim tincidunt ex. Nulla ligula libero, consectetur in faucibus eu, lobortis non justo.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac condimentum eros, at porta orci. Cras dictum sodales nunc sed vulputate. Suspendisse at arcu in risus consectetur maximus. Donec eu fringilla massa, ut convallis nunc. Mauris tempor, justo vel pulvinar finibus, magna ipsum pulvinar enim, et eleifend velit nisl ac felis. Vivamus porta leo sit amet nibh sagittis, nec faucibus neque consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris tempus, nisl finibus euismod molestie, ligula mi convallis erat, ut tincidunt sem enim tincidunt ex. Nulla ligula libero, consectetur in faucibus eu, lobortis non justo.</p>
+            </div>
+            <div class="tab-pane mcp-photos" id="f">
+            </div>
+            <div class="tab-pane mcp-videos" id="g">
+            </div>
+
+            <div class="tab-pane mcp-logos" id="h">
+              <h2><strong>Brand</strong> Assets</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac condimentum eros, at porta orci. Cras dictum sodales nunc sed vulputate. Suspendisse at arcu in risus consectetur maximus. Donec eu fringilla massa, ut convallis nunc. Mauris tempor, justo vel pulvinar finibus, magna ipsum pulvinar enim, et eleifend velit nisl ac felis. Vivamus porta leo sit amet nibh sagittis, nec faucibus neque consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris tempus, nisl finibus euismod molestie, ligula mi convallis erat, ut tincidunt sem enim tincidunt ex. Nulla ligula libero, consectetur in faucibus eu, lobortis non justo.</p>
+              <a href="//i0.wp.com/cdn.makezine.com/uploads/2013/01/mf_logo_rgb_tm.gif" class="mcp-download-btn" download>MF Logo</a>
+              <a href="//i0.wp.com/cdn.makezine.com/uploads/2013/01/mf_logo_rgb_tm.gif" class="mcp-download-btn" download>Makey</a>
+              <a href="https://i0.wp.com/cdn.makezine.com/uploads/2013/01/mf_logo_rgb_tm.gif" class="mcp-download-btn" download>Color Palette</a>
+
+              <h2><strong>Promotional</strong> Items</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac condimentum eros, at porta orci. Cras dictum sodales nunc sed vulputate. Suspendisse at arcu in risus consectetur maximus. Donec eu fringilla massa, ut convallis nunc. Mauris tempor, justo vel pulvinar finibus, magna ipsum pulvinar enim, et eleifend velit nisl ac felis. Vivamus porta leo sit amet nibh sagittis, nec faucibus neque consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris tempus, nisl finibus euismod molestie, ligula mi convallis erat, ut tincidunt sem enim tincidunt ex. Nulla ligula libero, consectetur in faucibus eu, lobortis non justo.</p>
+              <a href="https://i0.wp.com/cdn.makezine.com/uploads/2013/01/mf_logo_rgb_tm.gif" class="mcp-download-btn" download>Web Ads</a>
+              <a href="https://i0.wp.com/cdn.makezine.com/uploads/2013/01/mf_logo_rgb_tm.gif" class="mcp-download-btn" download>Social Posts</a>
+              <a href="https://i0.wp.com/cdn.makezine.com/uploads/2013/01/mf_logo_rgb_tm.gif" class="mcp-download-btn" download>Ad Copy</a>
+              <a href="https://i0.wp.com/cdn.makezine.com/uploads/2013/01/mf_logo_rgb_tm.gif" class="mcp-download-btn" download>Postcards</a>
+              <a href="https://i0.wp.com/cdn.makezine.com/uploads/2013/01/mf_logo_rgb_tm.gif" class="mcp-download-btn" download>Flyers</a>
+            </div>
           </article>
 
   <!--           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
