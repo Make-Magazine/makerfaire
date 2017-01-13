b3eb2664a3084c6264abf7a59b9a6e90093af9d9
David Beauchamp <dbeauchamp@makermedia.com>
2017-01-12 18:02:21 -0800

    More style changes to the Gravity Form, adding url tracking to the BS tabs

--- wp-content/themes/makerfaire/page-content-tabs.php
+++ wp-content/themes/makerfaire/page-content-tabs.php
@@ -43,27 +43,30 @@ get_header(); ?>
 
           <div class="tabbable">
 
-            <ul class="nav nav-tabs nav-stacked navbar-collapse col-xs-12 col-sm-3">
+            <ul class="nav nav-tabs nav-stacked nav-tabs-responsive navbar-collapse col-xs-12 col-sm-3">
 
               <?php if( have_rows('tabs') ):
 
                 $counter = 1;
 
-                while( have_rows('tabs') ): the_row(); 
+                while( have_rows('tabs') ): the_row();
 
                   $tab_title = get_sub_field('tab_title');
+                  $replace_these = array("/", "&");
+                  $tab_title2 = (str_replace($replace_these, '', $tab_title));
+                  $tab_url = (str_replace(' ', '-', strtolower($tab_title2)));
 
                   if ($counter == 1) { ?>
 
                     <li class="active">
-                      <a href="#mcp-tab<?php echo $counter; ?>" data-toggle="tab"><span><?php echo $tab_title; ?></span></a>
+                      <a href="#<?php echo $tab_url ?>" data-toggle="tab"><span><?php echo $tab_title; ?></span></a>
                     </li>
 
                   <?php
                   } else { ?>
 
                     <li>
-                      <a href="#mcp-tab<?php echo $counter; ?>" data-toggle="tab"><span><?php echo $tab_title; ?></span></a>
+                      <a href="#<?php echo $tab_url ?>" data-toggle="tab"><span><?php echo $tab_title; ?></span></a>
                     </li>
 
                   <?php
@@ -86,17 +89,21 @@ get_header(); ?>
                 while( have_rows('tabs') ): the_row(); 
 
                   $tab_content = get_sub_field('tab_content');
+                  $tab_title = get_sub_field('tab_title');
+                  $replace_these = array("/", "&");
+                  $tab_title2 = (str_replace($replace_these, '', $tab_title));
+                  $tab_url = (str_replace(' ', '-', strtolower($tab_title2)));
 
                   if ($counter == 1) { ?>
 
-                    <div class="tab-pane active" id="mcp-tab<?php echo $counter; ?>">
+                    <div class="tab-pane active" id="<?php echo $tab_url ?>">
                       <?php echo $tab_content; ?>
                     </div>
 
                   <?php
                   } else { ?>
 
-                    <div class="tab-pane" id="mcp-tab<?php echo $counter; ?>">
+                    <div class="tab-pane" id="<?php echo $tab_url ?>">
                       <?php echo $tab_content; ?>
                     </div>
 
@@ -125,12 +132,82 @@ get_header(); ?>
   </div><!--Container-->
 
 </div><!--#media-center-page-->
-
 <script src="https://rawgit.com/flatlogic/bootstrap-tabcollapse/master/bootstrap-tabcollapse.js"></script>
 <script>
-  jQuery('.nav-tabs').tabCollapse({
+  jQuery('.nav-tabs-responsive').tabCollapse({
     tabsClass: 'hidden-sm hidden-xs',
-    accordionClass: 'visible-sm visible-xs'
+    accordionClass: 'visible-sm visible-xs nav-tabs-responsive'
+  });
+</script>
+<script>
+  //Script to add url navigation to the bootstrap tabs: jquery.stickytabs.js
+  (function ( $ ) {
+      $.fn.stickyTabs = function( options ) {
+          var context = this
+
+          var settings = $.extend({
+              getHashCallback: function(hash, btn) { return hash },
+              selectorAttribute: "href",
+              backToTop: false,
+              initialTab: $('li.active > a','h4 > a:not(.collapsed)',context)
+          }, options );
+
+          // Show the tab corresponding with the hash in the URL, or the first tab.
+          var showTabFromHash = function() {
+            var hash = settings.selectorAttribute == "href" ? window.location.hash : window.location.hash.substring(1);
+            if (hash != '') {
+                
+                if ($( window ).width() < 991) {
+                  var selector = hash ? 'a[' + settings.selectorAttribute +'="' + hash + '-collapse"]' : settings.initialTab;
+                  $(selector).click();
+                } else {
+                  var selector = hash ? 'a[' + settings.selectorAttribute +'="' + hash + '"]' : settings.initialTab;
+                  $(selector, context).tab('show');
+                }
+                setTimeout(backToTop, 1);
+            }
+          }
+
+          // We use pushState if it's available so the page won't jump, otherwise a shim.
+          var changeHash = function(hash) {
+            if (history && history.pushState) {
+              history.pushState(null, null, window.location.pathname + window.location.search + '#' + hash);
+            } else {
+              scrollV = document.body.scrollTop;
+              scrollH = document.body.scrollLeft;
+              window.location.hash = hash;
+              document.body.scrollTop = scrollV;
+              document.body.scrollLeft = scrollH;
+            }
+          }
+
+          var backToTop = function() {
+            if (settings.backToTop === true) {
+              window.scrollTo(0, 0);
+            }
+          }
+
+          // Set the correct tab when the page loads
+          showTabFromHash();
+
+          // Set the correct tab when a user uses their back/forward button
+          $(window).on('hashchange', showTabFromHash);
+
+          // Change the URL when tabs are clicked
+          $('a', context).on('click', function(e) {
+            var hash = this.href.split('#')[1];
+            if (typeof hash != 'undefined' && hash != '') {
+                var adjustedhash = settings.getHashCallback(hash, this);
+                changeHash(adjustedhash);
+                setTimeout(backToTop, 1);
+            }
+          });
+
+          return this;
+      };
+  }( jQuery ));
+  jQuery( document ).ready(function() {
+    jQuery('.nav-tabs-responsive').stickyTabs();
   });
 </script>
 <?php get_footer(); ?>
