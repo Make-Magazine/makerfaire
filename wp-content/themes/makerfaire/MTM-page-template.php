1eb2aa9ac611dd2e5ecb477fa5a4e2f39fe2c915
David Beauchamp <dbeauchamp@makermedia.com>
2017-05-08 14:18:59 +0200

    Adjusting responsive retina image size on MTM template

--- wp-content/themes/makerfaire/MTM-page-template.php
+++ wp-content/themes/makerfaire/MTM-page-template.php
@@ -70,9 +70,9 @@
                   <a href="/maker/entry/<?php echo $randEntry['id']; ?>">
                     <span class="maker-slider-btn">Learn More About This Maker</span>
                     <img class="img-responsive cycle-gallery-slide" src="<?php echo legacy_get_resized_remote_image_url($randPhoto,1134,442); ?>" srcset="<?php echo legacy_get_resized_remote_image_url($randPhoto,767,575); ?> 1x, 
-                                                                                                                                                          <?php echo legacy_get_resized_remote_image_url($randPhoto,1534,1150); ?> 2x,
+                                                                                                                                                          <?php echo legacy_get_resized_remote_image_url($randPhoto,800,600); ?> 2x,
                                                                                                                                                           <?php echo legacy_get_resized_remote_image_url($randPhoto,1134,442); ?> 1x
-                                                                                                                                                          <?php echo legacy_get_resized_remote_image_url($randPhoto,2268,884); ?> 2x" alt="Slide Show from Maker Faire <?php echo $faireName;?>">
+                                                                                                                                                          <?php echo legacy_get_resized_remote_image_url($randPhoto,1400,546); ?> 2x" alt="Slide Show from Maker Faire <?php echo $faireName;?>">
                   </a>
 									<a href="/maker/entry/<?php echo $randEntry['id']; ?>">
                     <div class="text-holder">
@@ -91,9 +91,9 @@
                       $overrideImg = findOverride($entries[$i]['id'],'mtm');
                       $projPhoto = ($overrideImg==''?$entries[$i]['22']:$overrideImg);?>
                       <img class="img-responsive cycle-gallery-slide" src="<?php echo legacy_get_resized_remote_image_url($projPhoto,1134,442); ?>" srcset="<?php echo legacy_get_resized_remote_image_url($projPhoto,767,575); ?> 1x,
-                                                                                                                                                            <?php echo legacy_get_resized_remote_image_url($projPhoto,1534,1150); ?> 2x,
+                                                                                                                                                            <?php echo legacy_get_resized_remote_image_url($projPhoto,800,600); ?> 2x,
                                                                                                                                                             <?php echo legacy_get_resized_remote_image_url($projPhoto,1134,442); ?> 1x,
-                                                                                                                                                            <?php echo legacy_get_resized_remote_image_url($projPhoto,2268,884); ?> 2x" alt="Slide Show from Maker Faire <?php echo $faireName;?>">
+                                                                                                                                                            <?php echo legacy_get_resized_remote_image_url($projPhoto,1400,546); ?> 2x" alt="Slide Show from Maker Faire <?php echo $faireName;?>">
                     </a>
                     <a href="/maker/entry/<?php echo $entries[$i]['id']; ?>">
   					  				<div class="text-holder">
