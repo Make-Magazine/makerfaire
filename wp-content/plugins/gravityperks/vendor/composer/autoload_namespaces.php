6a75e01af41df88ca7c069aa5389ff5b443fe2ef
Alicia Williams <alicia@makermedia.com>
2017-07-31 13:15:12 -0600

    plugin updates for gravityperks and redirection

--- wp-content/plugins/gravityperks/vendor/composer/autoload_namespaces.php
+++ wp-content/plugins/gravityperks/vendor/composer/autoload_namespaces.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Michelf' => array($vendorDir . '/michelf/php-markdown'),
+    'Parsedown' => array($vendorDir . '/erusev/parsedown'),
 );
