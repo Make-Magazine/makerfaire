e002556d95ca8f2887f4da16833ba96f908bca61
Alicia Williams <alicia@makermedia.com>
2017-08-30 14:16:20 -0600

    plugin update gwreadonly

--- wp-content/plugins/gwreadonly/gwreadonly.php
+++ wp-content/plugins/gwreadonly/gwreadonly.php
@@ -1,14 +1,16 @@
 <?php
 /**
-* Plugin Name: GP Read Only
-* Description: Mark your form fields as read-only to allow users to see field data but not modify it.
-* Plugin URI: http://gravitywiz.com/
-* Version: 1.3
-* Author: David Smith
-* Author URI: http://gravitywiz.com/
-* License: GPL2
-* Perk: True
-*/
+ * Plugin Name: GP Read Only
+ * Description: Mark your form fields as read-only to allow users to see field data but not modify it.
+ * Plugin URI: http://gravitywiz.com/
+ * Version: 1.3.1
+ * Author: David Smith
+ * Author URI: http://gravitywiz.com/
+ * License: GPL2
+ * Perk: True
+ */
+
+define( 'GP_READ_ONLY_VERSION', '1.3.1' );
 
 require 'includes/class-gp-bootstrap.php';
 
