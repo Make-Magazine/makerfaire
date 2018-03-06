5c0610da712b84a1c1e7b616fe1ce868fc5db8f4
Alicia Williams <alicia@makermedia.com>
2018-03-06 11:20:07 -0700

    Gravity Perks - preview submission plugin update

--- wp-content/plugins/gp-preview-submission/gp-preview-submission.php
+++ wp-content/plugins/gp-preview-submission/gp-preview-submission.php
@@ -3,12 +3,14 @@
  * Plugin Name: GP Preview Submission
  * Plugin URI: http://gravitywiz.com/
  * Description: Add a simple submission preview to allow users to confirm their submission is correct before submitting the form.
- * Version: 1.2.7
+ * Version: 1.2.8
  * Author: David Smith
  * Author URI: http://gravitywiz.com
  * License: GPL2
  * Perk: True
  */
+
+define( 'GP_PREVIEW_SUBMISSION_VERSION', '1.2.8' );
 
 require 'includes/class-gp-bootstrap.php';
 
