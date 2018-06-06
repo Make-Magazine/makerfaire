c0407c0e15daddf3f6d84b81005ed1a5eeadb59e
Rio Roth-Barrerio <rio@makermedia.com>
2018-06-06 14:01:39 -0700

    delay nav scripts until after form scripts

--- wp-content/themes/makerfaire/js/src/navbar.js
+++ wp-content/themes/makerfaire/js/src/navbar.js
@@ -4,158 +4,159 @@ jQuery(function() {
 });
 
 (function($) {
-   // don't display avatar until login's gone
+   // keep these from happening before any oangular or login scripts
    $(window).bind("load", function() {
-    $(".nav-level-1-auth #profile-view .avatar").css("display","block");
-  });
-  jQuery('#hamburger-icon, #hamburger-makey, .nav-flyout-underlay').click(function() {
-    jQuery('.stagingMsg').toggleClass('gone');
-    jQuery('#hamburger-icon').toggleClass('open');
-    jQuery('#hamburger-makey').animate({opacity: 'toggle'})
-    jQuery('#nav-flyout').animate({opacity: 'toggle'});
-    jQuery('body').toggleClass('nav-open-no-scroll');
-    jQuery('html').toggleClass('nav-open-no-scroll');
-    jQuery('.nav-flyout-underlay').animate({opacity: 'toggle'});
-  });
+      $(".nav-level-1-auth #profile-view .avatar").css("display","block");
 
-  jQuery('.nav-flyout-column').on('click', '.expanding-underline', function(event) {
-    if (jQuery(window).width() < 577) { 
-      event.preventDefault();
-      jQuery(this).toggleClass('underline-open');
-      jQuery(this).next('.nav-flyout-ul').slideToggle();
-    }
-  });
-  // fix nav to top on scrolldown, stay fixed for transition from mobile to desktop
-  var e = jQuery(".universal-nav");
-  var hamburger = jQuery(".nav-hamburger");
-  var y_pos = jQuery(".nav-level-2").offset().top;
-  var nextItemUnderNav = jQuery("#main");
+      jQuery('#hamburger-icon, #hamburger-makey, .nav-flyout-underlay').click(function() {
+        jQuery('.stagingMsg').toggleClass('gone');
+        jQuery('#hamburger-icon').toggleClass('open');
+        jQuery('#hamburger-makey').animate({opacity: 'toggle'})
+        jQuery('#nav-flyout').animate({opacity: 'toggle'});
+        jQuery('body').toggleClass('nav-open-no-scroll');
+        jQuery('html').toggleClass('nav-open-no-scroll');
+        jQuery('.nav-flyout-underlay').animate({opacity: 'toggle'});
+      });
 
-  if (jQuery(window).width() < 578) {
-          jQuery(".auth-target").append(jQuery(".nav-level-1-auth"));
-  }
-  jQuery(window).on('resize', function(){
-      if (jQuery(window).width() < 767) {
-          y_pos = 0;
-          nextItemUnderNav.css("margin-top", "55px");
-      }else{
-          y_pos = 75;
-          nextItemUnderNav.css("margin-top", "0px");
-      }
-      if (jQuery(window).width() < 578) {
-          jQuery(".auth-target").append(jQuery(".nav-level-1-auth"));
-      }else{
-          jQuery("nav.container").append(jQuery(".nav-level-1-auth"));
-      }
-  });
-  jQuery(document).scroll(function() {
-      var scrollTop = jQuery(this).scrollTop();
-      if(scrollTop > y_pos && jQuery(window).width() > 748){
-          e.addClass("main-nav-scrolled"); 
-          hamburger.addClass("ham-menu-animate");
-          nextItemUnderNav.css("margin-top", "55px");
-      }else if(scrollTop <= y_pos){
-          e.removeClass("main-nav-scrolled"); 
-          hamburger.removeClass("ham-menu-animate");
-          if (jQuery(window).width() > 767) {
-            nextItemUnderNav.css("margin-top", "0px");
-          }
-      }
-  });
-    
-  /* Bring back if we need search for any reason
-  jQuery("#search-modal").fancybox({
-        wrapCSS : 'search-modal-wrapper',
-        //autoSize : true,
-        //width  : 400,
-        autoHeight : true,
-        padding : 0,
-        closeClick  : false, 
-        afterShow   : function() {
-            // keep it from reloading upon clicks
-            var modalWidth = jQuery('.fancybox-inner').width();
-            jQuery('.fancybox-inner').css("width", modalWidth + 1);
-            jQuery('#search-modal').bind('click', false);
-            jQuery(".sb-search-submit").click(function(e){
-                if(jQuery("#search").val() && jQuery("#search").val() != ""){
-                    var searchForm = jQuery(".search-form");
-                    window.location.href = searchForm.attr("action") +"?s=" + jQuery("#search").val();
-                }else{
-                    jQuery("#search").attr('placeholder', "Please enter in some text to search for...");            
-                }
-            });
-            jQuery(".sb-search-input").focus();
-        },
-        afterClose: function () {
-            jQuery('#search-modal').unbind('click', false);
+      jQuery('.nav-flyout-column').on('click', '.expanding-underline', function(event) {
+        if (jQuery(window).width() < 577) { 
+          event.preventDefault();
+          jQuery(this).toggleClass('underline-open');
+          jQuery(this).next('.nav-flyout-ul').slideToggle();
         }
-  });
-  jQuery(".fa-search").click(function(e){
-       jQuery("#search-modal").trigger('click');
-  });*/
-    
-  // to keep this nav universal, let's not have each site's style sheet highlight a different active manually
-  var site = window.location.hostname;
-  var firstpath = jQuery(location).attr('pathname');
-    firstpath.indexOf(1);
-    firstpath.toLowerCase();
-    firstpath = firstpath.split("/")[1];
-  var shareSection = site + "/" + firstpath;
-  function universalNavActive( site ) {
-    jQuery(".nav-" + site).addClass("active-site");
-    jQuery(".nav-" + site + " .nav-level-2-arrow").addClass("active-site")
-  }
-  // each one has to apply to a number of environments
-  switch(site) {
-    case "make-zine":
-    case "makezine":
-    case "makezine.wpengine.com":
-    case "makezine.staging.wpengine.com":
-    case "makezine.com":
-        universalNavActive("zine");
-        break;
-    case "makeco":
-    case "makeco.wpengine.com":
-    case "makeco.staging.wpengine.com/":
-    case "makeco.com":
-        universalNavActive("make");
-        break;
-    case "makershed.com":
-        universalNavActive("shed")
-        break;  
-    case "maker-faire":
-    case "makerfaire":
-    case "makerfaire.wpengine.com":
-    case "makerfaire.staging.wpengine.com":
-    case "makerfaire.com":
-        universalNavActive("faire")
-        break;
-    default:
-          break;
-  }
-  switch(shareSection) {
-    case "maker-share/learning":
-    case "makershare/learning":
-    case "makeshare.wpengine.com/learning":
-    case "makershare.staging.wpengine.com/learning":
-    case "makershare.com/learning":
-        universalNavActive("share")
-        break;
-    case "maker-share/":
-    case "makershare/":
-    case "makeshare.wpengine.com/":
-    case "makershare.staging.wpengine.com/":
-    case "makershare.com/":
-        universalNavActive("share-p")
-        break;
-    default:
-          break;
-  }
-/* Adjustments for Safari on Mac */
-  if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Mac') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-        // console.log('Safari on Mac detected, applying class...');
-        $('html').addClass('safari-mac'); // provide a class for the safari-mac specific css to filter with
-  }
+      });
+      // fix nav to top on scrolldown, stay fixed for transition from mobile to desktop
+      var e = jQuery(".universal-nav");
+      var hamburger = jQuery(".nav-hamburger");
+      var y_pos = jQuery(".nav-level-2").offset().top;
+      var nextItemUnderNav = jQuery("#main");
+
+      if (jQuery(window).width() < 578) {
+              jQuery(".auth-target").append(jQuery(".nav-level-1-auth"));
+      }
+      jQuery(window).on('resize', function(){
+          if (jQuery(window).width() < 767) {
+              y_pos = 0;
+              nextItemUnderNav.css("margin-top", "55px");
+          }else{
+              y_pos = 75;
+              nextItemUnderNav.css("margin-top", "0px");
+          }
+          if (jQuery(window).width() < 578) {
+              jQuery(".auth-target").append(jQuery(".nav-level-1-auth"));
+          }else{
+              jQuery("nav.container").append(jQuery(".nav-level-1-auth"));
+          }
+      });
+      jQuery(document).scroll(function() {
+          var scrollTop = jQuery(this).scrollTop();
+          if(scrollTop > y_pos && jQuery(window).width() > 748){
+              e.addClass("main-nav-scrolled"); 
+              hamburger.addClass("ham-menu-animate");
+              nextItemUnderNav.css("margin-top", "55px");
+          }else if(scrollTop <= y_pos){
+              e.removeClass("main-nav-scrolled"); 
+              hamburger.removeClass("ham-menu-animate");
+              if (jQuery(window).width() > 767) {
+                nextItemUnderNav.css("margin-top", "0px");
+              }
+          }
+      });
+
+      /* Bring back if we need search for any reason
+      jQuery("#search-modal").fancybox({
+            wrapCSS : 'search-modal-wrapper',
+            //autoSize : true,
+            //width  : 400,
+            autoHeight : true,
+            padding : 0,
+            closeClick  : false, 
+            afterShow   : function() {
+                // keep it from reloading upon clicks
+                var modalWidth = jQuery('.fancybox-inner').width();
+                jQuery('.fancybox-inner').css("width", modalWidth + 1);
+                jQuery('#search-modal').bind('click', false);
+                jQuery(".sb-search-submit").click(function(e){
+                    if(jQuery("#search").val() && jQuery("#search").val() != ""){
+                        var searchForm = jQuery(".search-form");
+                        window.location.href = searchForm.attr("action") +"?s=" + jQuery("#search").val();
+                    }else{
+                        jQuery("#search").attr('placeholder', "Please enter in some text to search for...");            
+                    }
+                });
+                jQuery(".sb-search-input").focus();
+            },
+            afterClose: function () {
+                jQuery('#search-modal').unbind('click', false);
+            }
+      });
+      jQuery(".fa-search").click(function(e){
+           jQuery("#search-modal").trigger('click');
+      });*/
+
+      // to keep this nav universal, let's not have each site's style sheet highlight a different active manually
+      var site = window.location.hostname;
+      var firstpath = jQuery(location).attr('pathname');
+        firstpath.indexOf(1);
+        firstpath.toLowerCase();
+        firstpath = firstpath.split("/")[1];
+      var shareSection = site + "/" + firstpath;
+      function universalNavActive( site ) {
+        jQuery(".nav-" + site).addClass("active-site");
+        jQuery(".nav-" + site + " .nav-level-2-arrow").addClass("active-site")
+      }
+      // each one has to apply to a number of environments
+      switch(site) {
+        case "make-zine":
+        case "makezine":
+        case "makezine.wpengine.com":
+        case "makezine.staging.wpengine.com":
+        case "makezine.com":
+            universalNavActive("zine");
+            break;
+        case "makeco":
+        case "makeco.wpengine.com":
+        case "makeco.staging.wpengine.com/":
+        case "makeco.com":
+            universalNavActive("make");
+            break;
+        case "makershed.com":
+            universalNavActive("shed")
+            break;  
+        case "maker-faire":
+        case "makerfaire":
+        case "makerfaire.wpengine.com":
+        case "makerfaire.staging.wpengine.com":
+        case "makerfaire.com":
+            universalNavActive("faire")
+            break;
+        default:
+              break;
+      }
+      switch(shareSection) {
+        case "maker-share/learning":
+        case "makershare/learning":
+        case "makeshare.wpengine.com/learning":
+        case "makershare.staging.wpengine.com/learning":
+        case "makershare.com/learning":
+            universalNavActive("share")
+            break;
+        case "maker-share/":
+        case "makershare/":
+        case "makeshare.wpengine.com/":
+        case "makershare.staging.wpengine.com/":
+        case "makershare.com/":
+            universalNavActive("share-p")
+            break;
+        default:
+              break;
+      }
+    /* Adjustments for Safari on Mac */
+      if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Mac') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+            // console.log('Safari on Mac detected, applying class...');
+            $('html').addClass('safari-mac'); // provide a class for the safari-mac specific css to filter with
+      }
+   });
 })(jQuery);
 
 
