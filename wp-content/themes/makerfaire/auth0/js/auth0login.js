d3f1c99d1965234acec3250ed2281c3efb179e76
Alicia Williams <alicia@makermedia.com>
2018-06-22 13:28:40 -0600

    fix to correctly set the redirect link when users are taken to the wp login

--- wp-content/themes/makerfaire/auth0/js/auth0login.js
+++ wp-content/themes/makerfaire/auth0/js/auth0login.js
@@ -1,6 +1,21 @@
 /*
  * This JS is used on the WP login page to force the user to the auth0 login page
  */
+function GetURLParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
 var baseurl = window.location.origin;
 
 var AUTH0_CLIENT_ID    = 'Ya3K0wmP182DRTexd1NdoeLolgXOlqO1';
@@ -18,5 +33,10 @@ var webAuth = new auth0.WebAuth({
   leeway: 60
 });
 
-localStorage.setItem('redirect_to',baseurl + "/manage-entries/");
+var redirect_to = GetURLParameter('redirect_to');
+if(redirect_to ==='') redirect_to = baseurl;
+localStorage.setItem('redirect_to', redirect_to);
+console.log(redirect_to);
+
 webAuth.authorize(); //login to auth0
+
