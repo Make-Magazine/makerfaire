c8ab6394d6d6bbc0d96d71eb3614d9ab34b84ba5
Rio Roth-Barrerio <rio@makermedia.com>
2018-07-03 13:46:32 -0700

    authentication page billboarding

--- wp-content/themes/makerfaire/page-authentication.php
+++ wp-content/themes/makerfaire/page-authentication.php
@@ -10,12 +10,36 @@
       <img src="/wp-content/themes/makerfaire/img/makey-stickers-slanted.png"  class="img-responsive" />
     </div>
     <div class="col-sm-9 col-xs-12">
-      <h2 class="text-center">You are Now Logged In</h2>
-      <h3 class="text-center">Please wait while we get you back to the right place.</h3>
+      <h2 class="text-center" style="margin-top:25px;">You are Now Logged In</h2>
+      <h3 class="text-center billboard">Please wait while we get you back to the right place.</h3>
     </div>
   </div>
 </div><!-- end .page-content -->
-  <div class="container">
+<script type="text/javascript">
+ $('.billboard').billboard({
+    messages: [
+      'Looking for Makey.',
+      'Plugging in the router.',
+      'Someone tripped over the power cord.',
+      'Searching for the cookies.',
+      'Is it faire time yet?.',
+      'Updating the countdown clock.',
+      'Looking for the lost drones.',
+      'adding neon to the tubes.',
+      'Greeting our speakers.',
+      'Checking the mics.',
+      'Thanking our sponsors.',
+      'Looking at projects on MakerShare.',
+      'Reading Make: magazine.',
+      'Thanking our Make: members.',
+      'Are we there yet?.',
+      'I\'m still working on it.',
+    ]
+  });
+});
+</script>
+
+<div class="container">
     <div class="row">
       <div class="col-xs-12">
         <?php // theloop
@@ -27,5 +51,5 @@
         <?php endif; ?>
       </div>
     </div>
-  </div>
+</div>
 <?php get_footer();
