76bf78dd2d99ea2036037f023c1f4620d43d59ce
Rio Roth-Barrerio <rio@makermedia.com>
2018-07-05 10:55:21 -0700

    Fix package.json to specific versions and check for subdomain differently

--- wp-content/themes/makerfaire/header.php
+++ wp-content/themes/makerfaire/header.php
@@ -245,9 +245,8 @@
                 'header'  => "Authorization: Basic " . base64_encode("$username:$password")
             )
         ));
-        echo($_SERVER['HTTP_HOST'] . "<hr />");
-        echo(explode('.', $_SERVER['HTTP_HOST'])[0]);
-        if(strpos($_SERVER['HTTP_HOST'] , "staging") !== false) {
+        echo(explode('.', $_SERVER['HTTP_HOST'])[1]);
+        if(explode('.', $_SERVER['HTTP_HOST'])[1] == "staging") {
           echo file_get_contents('https://makeco.staging.wpengine.com/wp-content/themes/memberships/universal-nav/universal-megamenu.html', false, $context); 
         }else{
           echo file_get_contents('https://make.co/wp-content/themes/memberships/universal-nav/universal-megamenu.html'); 
