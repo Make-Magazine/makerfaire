70a2b5757a1f18422219e19ffccac2a74112a97d
Rio Roth-Barrerio <rio@makermedia.com>
2018-07-05 11:28:58 -0700

    style authenticate page

--- wp-content/themes/makerfaire/page-authentication.php
+++ wp-content/themes/makerfaire/page-authentication.php
@@ -13,7 +13,7 @@
       <h2 class="text-center">Please wait while we complete the login process.</h2>
       <h3 class="text-center">You will be redirected to the page you were trying to access shortly.</h3>
       <h3 class="text-center billboard">Please wait while we get you back to the right place.</h3>
-      <p style="font-size:14px;margin:20px auto;">** This site utilizes cookies to log you in. Please ensure you have cookies enabled. **</p>
+      <p class="text-center"><i>** This site utilizes cookies to log you in. Please ensure you have cookies enabled. **</i></p>
     </div>
   </div>
 </div><!-- end .page-content -->
