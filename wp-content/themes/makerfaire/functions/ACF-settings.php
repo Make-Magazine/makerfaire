ab742fe0897784181a2993f4621eec5d298c5900
Alicia Williams <alicia@makermedia.com>
2018-08-21 09:42:58 -0600

    ACF fix for preview changes

--- wp-content/themes/makerfaire/functions/ACF-settings.php
+++ wp-content/themes/makerfaire/functions/ACF-settings.php
@@ -9,3 +9,14 @@ function mf_acf_init() {
 }
 
 add_action('acf/init', 'mf_acf_init');
+
+add_filter('_wp_post_revision_fields', 'add_field_debug_preview');
+function add_field_debug_preview($fields){
+   $fields["debug_preview"] = "debug_preview";
+   return $fields;
+}
+
+add_action( 'edit_form_after_title', 'add_input_debug_preview' );
+function add_input_debug_preview() {
+   echo '<input type="hidden" name="debug_preview" value="debug_preview">';
+}
