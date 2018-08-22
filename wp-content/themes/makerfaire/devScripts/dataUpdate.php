258f669b95ab2a7383433f0d1f45f277973751a8
Alicia Williams <alicia@makermedia.com>
2018-08-22 14:24:26 -0600

    MTM page updates

--- wp-content/themes/makerfaire/devScripts/dataUpdate.php
+++ wp-content/themes/makerfaire/devScripts/dataUpdate.php
@@ -9,90 +9,99 @@ include '../wp-load.php';
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-  </head>
-  <body>
-    <?php
-    //retrieve all BA2016 entries
-    $search_criteria = array();
-    $search_criteria['status'] = 'active';
-    $offset = $_GET['offset'];
-    $sorting = array( 'key' => 'id', 'direction' => 'DESC', 'is_numeric' => true );
-    $paging = array( 'offset' => $offset, 'page_size' => 200 );
-    $total_count = 0;
-    $entries         = GFAPI::get_entries( array(46,45,49,51,47,48), $search_criteria,$sorting,$paging,$total_count );
-    echo 'Total # of Records = '.$total_count;
-    ?>
-
-    <table border="1">
-      <tr>
-        <th>Entry ID</th>
-        <th>Form</th>
-        <th>Field 83</th>
-        <th>Field 84</th>
-        <th>Field 293</th>
-        <th>Field 60</th>
-        <th>Field 73</th>
-        <th>Field 75</th>
-        <th>Field 64</th>
-        <th>Status</th>
-        <th>Assign</th>
-      </tr>
+   <head>
+      <meta charset="UTF-8">
+   </head>
+   <body>
+      To Update the Hidden/Not Hidden indicator for Entry passes
+      &type=entry_passes
+      &go_live=yes (if, = no this will just display what needs updated
+      
+      select eb_entry_access_code.*, wp_rg_lead.form_id, eb_eventToTicket.ticket_type, eb_ticket_type.*
+FROM eb_entry_access_code
+LEFT OUTER JOIN wp_rg_lead ON wp_rg_lead.id = eb_entry_access_code.entry_id
+LEFT OUTER JOIN eb_eventToTicket ON eb_eventToTicket.ticketID = eb_entry_access_code.EBticket_id
+LEFT OUTER JOIN eb_ticket_type on form_type = 1 and eb_ticket_type.ticket_type = eb_eventToTicket.ticket_type
+WHERE form_id = 208 AND eb_ticket_type.event_id in(11,12)
+ORDER BY eb_entry_access_code.id DESC
       <?php
-      foreach($entries as $entry){
-        echo '<tr>';
-        echo '<td>'.$entry['id'].'</td>';
-        echo '<td>'.$entry['form_id'].'</td>';
-        echo '<td>'.$entry['83'].'</td>';
-        echo '<td>'.$entry['84'].'</td>';
-        echo '<td>'.$entry['293'].'</td>';
-        echo '<td>'.$entry['60'].'</td>';
-        echo '<td>'.$entry['73'].'</td>';
-        echo '<td>'.$entry['75'].'</td>';
-        echo '<td>'.$entry['64'].'</td>';
+      //retrieve all BA2016 entries
+      $search_criteria = array();
+      $search_criteria['status'] = 'active';
+      $offset = $_GET['offset'];
+      $sorting = array('key' => 'id', 'direction' => 'DESC', 'is_numeric' => true);
+      $paging = array('offset' => $offset, 'page_size' => 200);
+      $total_count = 0;
+      $entries = GFAPI::get_entries(array(46, 45, 49, 51, 47, 48), $search_criteria, $sorting, $paging, $total_count);
+      echo 'Total # of Records = ' . $total_count;
+      ?>
 
-        $status   = gform_get_meta( $entry['id'], 'res_status' );
-        $assignTo = gform_get_meta( $entry['id'], 'res_assign' );
-        echo '<td>'.$status.'</td><td>'.$assignTo.'</td>';
-        echo '</tr>';
-      if($status=='' || $assignTo==''){
-        $assignTo    = 'na';//not assigned to anyone
-        $status      = 'ready';//ready
-        //field ID 83
-        if( $entry['83']  == 'Yes' ||
-            $entry['84']  == 'Yes' ||
-            $entry['293'] == 'Yes' ||
-            $entry['60']  == "Other"){
-          $status   = 'review';
-          $assignTo = 'jay'; //Jay
-        }elseif($entry['73'] == 'Yes' &&
-                $entry['75'] == 'Other. Power request specified in the Special Power Requirements box'){
-          $status   = 'review';
-          $assignTo = 'kerry'; //Kerry
-        }elseif($entry['64'] != ''){
-          $status   = 'review';
-          $assignTo = 'kerry'; //Kerry
-        }
-        $resUpdate[]=array($entry['id'], $status,   $assignTo );
-      }
+      <table border="1">
+         <tr>
+            <th>Entry ID</th>
+            <th>Form</th>
+            <th>Field 83</th>
+            <th>Field 84</th>
+            <th>Field 293</th>
+            <th>Field 60</th>
+            <th>Field 73</th>
+            <th>Field 75</th>
+            <th>Field 64</th>
+            <th>Status</th>
+            <th>Assign</th>
+         </tr>
+         <?php
+         foreach ($entries as $entry) {
+            echo '<tr>';
+            echo '<td>' . $entry['id'] . '</td>';
+            echo '<td>' . $entry['form_id'] . '</td>';
+            echo '<td>' . $entry['83'] . '</td>';
+            echo '<td>' . $entry['84'] . '</td>';
+            echo '<td>' . $entry['293'] . '</td>';
+            echo '<td>' . $entry['60'] . '</td>';
+            echo '<td>' . $entry['73'] . '</td>';
+            echo '<td>' . $entry['75'] . '</td>';
+            echo '<td>' . $entry['64'] . '</td>';
 
+            $status = gform_get_meta($entry['id'], 'res_status');
+            $assignTo = gform_get_meta($entry['id'], 'res_assign');
+            echo '<td>' . $status . '</td><td>' . $assignTo . '</td>';
+            echo '</tr>';
+            if ($status == '' || $assignTo == '') {
+               $assignTo = 'na'; //not assigned to anyone
+               $status = 'ready'; //ready
+               //field ID 83
+               if ($entry['83'] == 'Yes' ||
+                       $entry['84'] == 'Yes' ||
+                       $entry['293'] == 'Yes' ||
+                       $entry['60'] == "Other") {
+                  $status = 'review';
+                  $assignTo = 'jay'; //Jay
+               } elseif ($entry['73'] == 'Yes' &&
+                       $entry['75'] == 'Other. Power request specified in the Special Power Requirements box') {
+                  $status = 'review';
+                  $assignTo = 'kerry'; //Kerry
+               } elseif ($entry['64'] != '') {
+                  $status = 'review';
+                  $assignTo = 'kerry'; //Kerry
+               }
+               $resUpdate[] = array($entry['id'], $status, $assignTo);
+            }
+         }
+         ?>
+      </table>
+      <?php
+      foreach ($resUpdate as $update) {
+         $entryID = $update[0];
+         $status = $update[1];
+         $assignTo = $update[2];
+
+         echo 'gform_update_meta( ' . $entryID . ', ' . $status . ',   \'res_status\' )<br/>';
+         echo 'gform_update_meta( ' . $entryID . ', ' . $assignTo . ', \'res_assign\' )<br/><br/>';
+         // update custom meta field
+         gform_update_meta($entryID, 'res_status', $status);
+         gform_update_meta($entryID, 'res_assign', $assignTo);
       }
       ?>
-    </table>
-    <?php
-
-    foreach($resUpdate as $update){
-      $entryID  = $update[0];
-      $status   = $update[1];
-      $assignTo = $update[2];
-
-      echo 'gform_update_meta( '.$entryID.', '.$status.',   \'res_status\' )<br/>';
-      echo 'gform_update_meta( '.$entryID.', '.$assignTo.', \'res_assign\' )<br/><br/>';
-      // update custom meta field
-      gform_update_meta( $entryID, 'res_status', $status );
-      gform_update_meta( $entryID, 'res_assign', $assignTo );
-    }
-    ?>
-  </body>
+   </body>
 </html>
--- wp-content/themes/makerfaire/page-meet-the-makers.php
+++ wp-content/themes/makerfaire/page-meet-the-makers.php
@@ -15,56 +15,56 @@ if($noMakerText =='') $noMakerText = 'No makers found';
     <div class="container">
       <h1 class="text-center"><?php echo get_the_title(); ?></h1>
     </div>
-    <div class="mtm-carousel-cont">
-      <div id="carouselImgs" class="mtm-carousel owl-carousel"></div>
 
-      <a id="left-trigger" class="left carousel-control" href="#" role="button" data-slide="prev">
-        <img class="glyphicon-chevron-right" src="<?php echo get_bloginfo('template_directory');?>/img/arrow_left.png" alt="Image Carousel button left" />
-        <span class="sr-only"><?php _e("Previous",'makerfaire')?></span>
-      </a>
-      <a id="right-trigger" class="right carousel-control" href="#" role="button" data-slide="next">
-        <img class="glyphicon-chevron-right" src="<?php echo get_bloginfo('template_directory');?>/img/arrow_right.png" alt="Image Carousel button right" />
-        <span class="sr-only"><?php _e("Next",'makerfaire')?></span>
-      </a>
-    </div>
-    <!--//end old-->
-    <div class="container">
-      <h2 class="text-center"><?php _e("Explore our Maker Exhibits!",'makerfaire')?></h2>
-    </div>
     <div class="flag-banner"></div>
 
     <div class="mtm-search">
       <form class="form-inline">
-        <label for="mtm-search-input"><?php _e("Search:",'makerfaire')?></label>
-        <input ng-model="makerSearch.$" id="mtm-search-input" class="form-control" placeholder="<?php _e("Looking for a specific Exhibit or Maker?",'makerfaire')?>" type="text">
+        <label for="mtm-search-input"><?php _e("Search by category, keyword, project, sponsor or maker name",'makerfaire')?></label>
+        <input ng-model="makerSearch.$" id="mtm-search-input" class="form-control" placeholder="<?php _e("Enter your search",'makerfaire')?>" type="text">
         <!--input class="form-control btn-w-ghost" value="GO" type="submit"-->
       </form>
     </div>
 
 
     <div class="mtm-filter container">
-      <div class="mtm-filter-view">
-        <span class="mtm-view-by"><?php _e("View by:",'makerfaire')?></span>
-        <a ng-class="{active: layout == 'grid'}" ng-click="layout = 'grid'" class="mtm-filter-g pointer-on-hover box gallery"><i class="fa fa-picture-o" aria-hidden="true"></i> <?php _e("GALLERY",'makerfaire')?></a>
-        <span class="mtm-pipe">|</span>
-        <a ng-class="{active: layout == 'list'}" ng-click="layout = 'list'" class="mtm-filter-l pointer-on-hover box list" ><i class="fa fa-th-list" aria-hidden="true"></i> <?php _e("LIST",'makerfaire')?></a>
-      </div>
+      <div class="row">
+         <div class="col-md-4">
+            <div class="mtm-filter-view">
+               <span class="mtm-view-by"><?php _e("View by:",'makerfaire')?></span>
+               <a ng-class="{active: layout == 'grid'}" ng-click="layout = 'grid'" class="mtm-filter-g pointer-on-hover box gallery"><i class="fa fa-picture-o" aria-hidden="true"></i> <?php _e("GALLERY",'makerfaire')?></a>
+               <span class="mtm-pipe">|</span>
+               <a ng-class="{active: layout == 'list'}" ng-click="layout = 'list'" class="mtm-filter-l pointer-on-hover box list" ><i class="fa fa-th-list" aria-hidden="true"></i> <?php _e("LIST",'makerfaire')?></a>
+            </div>
+         </div>
+         <div class="col-md-4">
+            <ul class="nav nav-pills">
+               <li class="active">
+                  <button type="button" ng-click="makerSearch.flag='Featured Maker'" class="btn btn-default">Featured</button>
+               </li>
+               <li>
+                  <button type="button" ng-click="makerSearch.flag=''" class="btn btn-default">All</button>
+               </li>   
+            </ul>            
+         </div>
+         <div class="col-md-4">
+            <div class="dropdown">
+               <button class="btn btn-link dropdown-toggle" type="button" id="mtm-dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                 <?php _e("Filter by Topics:",'makerfaire')?> {{category}}
+                 <i class="fa fa-chevron-down" aria-hidden="true"></i>
+               </button>
 
-      <div class="dropdown">
-        <button class="btn btn-link dropdown-toggle" type="button" id="mtm-dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-          <?php _e("Filter by Topics:",'makerfaire')?> {{category}}
-          <i class="fa fa-chevron-down" aria-hidden="true"></i>
-        </button>
-
-        <ul class="dropdown-menu" aria-labelledby="mtm-dropdownMenu">
-          <li>
-            <a class="pointer-on-hover" ng-click="clearFilter()"><?php _e("All",'makerfaire')?></a>
-          </li>
-          <li ng-repeat="tag in tags | orderBy: tag">
-            <a class="pointer-on-hover" ng-click="setTagFilter(tag)">{{ tag }}</a>
-          </li>
-        </ul>
-      </div>
+               <ul class="dropdown-menu" aria-labelledby="mtm-dropdownMenu">
+                 <li>
+                   <a class="pointer-on-hover" ng-click="clearFilter()"><?php _e("All",'makerfaire')?></a>
+                 </li>
+                 <li ng-repeat="tag in tags | orderBy: tag">                     
+                   <a class="pointer-on-hover" ng-click="setTagFilter(tag)">{{ tag }}</a>
+                 </li>
+               </ul>
+            </div>
+         </div>
+      </div>     
     </div>
 
     <div class="mtm-results">
@@ -74,8 +74,8 @@ if($noMakerText =='') $noMakerText = 'No makers found';
       </div>
       <!-- Grid View -->
       <div ng-show="layout == 'grid'" class="mtm-results-cont">
-        <div ng-repeat="maker in makers | filter : makerSearch | byCategory:category">
-          <a href="/maker/entry/{{maker.id}}">
+        <div ng-repeat="maker in makers | filter : makerSearch | byCategory:category | limitTo:50">
+          <a target="none" href="/maker/entry/{{maker.id}}">
             <article class="mtm-maker" style="background-image: url('{{ maker.large_img_url }}')">
               <h3>{{ maker.name }}</h3>
             </article>
@@ -88,7 +88,7 @@ if($noMakerText =='') $noMakerText = 'No makers found';
       <div ng-show="layout == 'list'" class="mtm-results-cont container">
         <div class="filter-alpha-wrapper">
           <span class="filterAlpha" ng-repeat="searchLetter in alphabet.split('') track by $index">
-            <a href=""  class="pointer-on-hover" ng-click="setLetter(searchLetter)">{{ searchLetter }}</a>
+            <a href=""  target="none" class="pointer-on-hover" ng-click="setLetter(searchLetter)">{{ searchLetter }}</a>
           </span>
           <span class="filterAlpha" ><a href=""  class="pointer-on-hover" ng-click="setLetter('')">Reset</a></span>
         </div>
