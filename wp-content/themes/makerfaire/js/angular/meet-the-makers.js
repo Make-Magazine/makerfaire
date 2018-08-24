fdd1bf05d5765f3833994096cb053a211295ad7b
Alicia Williams <alicia@makermedia.com>
2018-08-24 15:33:46 -0600

    fix initial jquery error

--- wp-content/themes/makerfaire/js/angular/meet-the-makers.js
+++ wp-content/themes/makerfaire/js/angular/meet-the-makers.js
@@ -114,10 +114,12 @@ app.filter('startsWithLetter', function () {
    return function (items, letter) {
       var filtered = [];
       var letterMatch = new RegExp(letter, 'i');
-      for (var i = 0; i < items.length; i++) {
-         var item = items[i];
-         if (letterMatch.test(item.name.substring(0, 1))) {
-            filtered.push(item);
+      if(jQuery(items).length){
+         for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            if (letterMatch.test(item.name.substring(0, 1))) {
+               filtered.push(item);
+            }
          }
       }
       return filtered;
