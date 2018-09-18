91d779cecb298022bff9020ed0b58a4359a188ee
Alicia Williams <alicia@makermedia.com>
2018-09-18 09:21:16 -0600

    Change to the register text

--- wp-content/themes/makerfaire/page-schedule.php
+++ wp-content/themes/makerfaire/page-schedule.php
@@ -165,7 +165,7 @@ if ($schedule_ids && $schedule_ids != '') { //display the new schedule page
 										
 										<div class="sched-registration" ng-show="schedule.flags!=NULL && schedule.flags.indexOf('Registration Required')!=-1">
 											<hr />
-											<div ng-click="schedule.isCollapsed = !schedule.isCollapsed">Requires Add-On Ticket</div>
+											<div ng-click="schedule.isCollapsed = !schedule.isCollapsed">Register Here</div>
 										</div>
                            </div>
 
