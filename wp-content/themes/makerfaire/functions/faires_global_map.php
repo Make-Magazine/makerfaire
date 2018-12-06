40365a5a6e1a2cfeb06d280339dcd13c3485d32c
Alicia Williams <alicia@makermedia.com>
2018-12-06 14:50:37 -0700

    add versioning to maps js

--- wp-content/themes/makerfaire/functions/faires_global_map.php
+++ wp-content/themes/makerfaire/functions/faires_global_map.php
@@ -1,26 +1,23 @@
 <?php
+
 function faire_maps_scripts() {
-  if (is_page_template('pages/page-global-maker-faire-map.php')) {
-    wp_enqueue_script(
-      'faires-global-map-scripts',
-      get_stylesheet_directory_uri() . '/js/angular/global-faires-map-app.js',
-      array('angularjs', 'ordinal-filter', 'angular-utils-pagination')
-    );
-    wp_enqueue_script(
-      'angular-utils-pagination',
-      get_stylesheet_directory_uri() . '/bower_components/angularUtils-pagination/dirPagination.js',
-      array('angularjs')
-    );
-    wp_enqueue_script(
-      'ordinal-filter',
-      get_stylesheet_directory_uri() . '/bower_components/angularjs-ordinal-filter/ordinal-browser.js',
-      array('angularjs')
-    );
-    wp_enqueue_script(
-      'angularjs',
-      get_stylesheet_directory_uri() . '/bower_components/angular/angular.min.js'
-    );
-  }
+   if (is_page_template('pages/page-global-maker-faire-map.php')) {
+      $my_theme = wp_get_theme();
+      $my_version = $my_theme->get('Version');
+      wp_enqueue_script(
+              'faires-global-map-scripts', get_stylesheet_directory_uri() . '/js/angular/global-faires-map-app.js', array('angularjs', 'ordinal-filter', 'angular-utils-pagination'), $my_version
+      );
+      wp_enqueue_script(
+              'angular-utils-pagination', get_stylesheet_directory_uri() . '/bower_components/angularUtils-pagination/dirPagination.js', array('angularjs')
+      );
+      wp_enqueue_script(
+              'ordinal-filter', get_stylesheet_directory_uri() . '/bower_components/angularjs-ordinal-filter/ordinal-browser.js', array('angularjs')
+      );
+      wp_enqueue_script(
+              'angularjs', get_stylesheet_directory_uri() . '/bower_components/angular/angular.min.js'
+      );
+   }
 }
-add_action( 'wp_enqueue_scripts', 'faire_maps_scripts' );
+
+add_action('wp_enqueue_scripts', 'faire_maps_scripts');
 ?>
