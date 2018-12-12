218baee749f42867e02023d98cf6e4e88312c1c2
Rio Roth-Barrerio <rio@makermedia.com>
2018-12-12 10:29:12 -0800

    add usersnap plugin to repo and then switch to using a code snippet in the footer

--- wp-content/themes/makerfaire/footer.php
+++ wp-content/themes/makerfaire/footer.php
@@ -248,5 +248,20 @@
   })();
 </script>
 
+<!-- Usersnap feedback and bug tracker -->
+<script>
+  	window.onUsersnapLoad = function(api) {
+	api.on('open', function(event) {
+       // extend
+	});
+	api.init();
+	window.Usersnap = api;
+  	}
+  	var script = document.createElement('script');
+	script.async = 1;
+	script.src = 'https://api.usersnap.com/load/1c756098-d8b1-4791-bd55-e6de2f5b933b.js?onload=onUsersnapLoad';
+	document.getElementsByTagName('head')[0].appendChild(script);
+</script>
+
 </body>
 </html>
