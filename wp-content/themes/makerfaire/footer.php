68b47f11ee065b44aa8c07de869f4711f2203ea0
Rio Roth-Barrerio <rio@makermedia.com>
2018-12-21 15:43:45 -0800

    load last no async

--- wp-content/themes/makerfaire/footer.php
+++ wp-content/themes/makerfaire/footer.php
@@ -250,22 +250,15 @@
 
 <!-- Usersnap feedback and bug tracker -->
 <script>
-  	window.onUsersnapLoad = function(api) {
-		api.on('open', function(event) {
-			 // extend
-		});
-		api.init();
-		window.Usersnap = api;
-  	}
-  	var script = document.createElement('script');
-	script.async = 1;
-	script.src = 'https://api.usersnap.com/load/1c756098-d8b1-4791-bd55-e6de2f5b933b.js?onload=onUsersnapLoad';
-	document.getElementsByTagName('html')[0].appendChild(script);
+	(function() {
+		var script = document.createElement('script');
+		script.async = 1;
+		script.src = 'https://api.usersnap.com/load/1c756098-d8b1-4791-bd55-e6de2f5b933b.js';
+		document.getElementsByTagName('html')[0].appendChild(script);
+	})();
 </script>
 
-
 </body>
-
 
 
 </html>
