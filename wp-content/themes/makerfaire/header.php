424113401b052cd0ccf4cba35a84f7b6421d7df8
Rio Roth-Barrerio <rio@makermedia.com>
2018-12-21 15:58:54 -0800

    promising, let's try narrow it down to just the remarking pixel

--- wp-content/themes/makerfaire/header.php
+++ wp-content/themes/makerfaire/header.php
@@ -29,8 +29,46 @@
   <!-- Le styles -->
     <?php wp_head(); ?>
 
+
   <?php get_template_part('dfp'); ?>
 
+  <script>
+    var _prum = [['id', '53fcea2fabe53d341d4ae0eb'],
+                ['mark', 'firstbyte', (new Date()).getTime()]];
+    (function() {
+        var s = document.getElementsByTagName('script')[0]
+          , p = document.createElement('script');
+        p.async = 'async';
+        p.src = '//rum-static.pingdom.net/prum.min.js';
+        s.parentNode.insertBefore(p, s);
+    })();
+  </script>
+
+  <!-- Facebook Pixel Code -->
+  <script>
+  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+  n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+  document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+  fbq('init', '399923000199419');
+  fbq('track', "PageView");
+  </script>
+  <noscript></noscript>
+  <!-- End Facebook Pixel Code -->
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-51157-7', 'auto');
+    ga('send', 'pageview', {
+    'page': location.pathname + location.search  + location.hash
+    });
+  </script>
 
   <?php if ( is_404() ) : // Load this last. ?>
     <script>
