8b2b29ee746ee4d993497d29d960f238b924e606
Rio Roth-Barrerio <rio@makermedia.com>
2019-01-24 13:44:02 -0800

    Add new custom field group for meet the maker pages and remove the acf metabox workaround

--- wp-content/themes/makerfaire/functions/ACF-settings.php
+++ wp-content/themes/makerfaire/functions/ACF-settings.php
@@ -2,13 +2,12 @@
 
 /*
  * As of ACF 5.60, they default removed the custom fields option on pages. This adds it back
- */
 
 function mf_acf_init() {
 	acf_update_setting('remove_wp_meta_box', false);
 }
+add_action('acf/init', 'mf_acf_init'); */
 
-add_action('acf/init', 'mf_acf_init');
 
 add_filter('_wp_post_revision_fields', 'add_field_debug_preview');
 function add_field_debug_preview($fields){
