0636b16848dea43821a4a6971e796b1e295965d5
Travis Stone <travis@makermedia.com>
2019-03-01 14:21:23 -0800

    Responsive tweaks for "promote" image grid

--- wp-content/themes/makerfaire/functions/ACF_displays.php
+++ wp-content/themes/makerfaire/functions/ACF_displays.php
@@ -16,14 +16,14 @@ function get_acf_content() {
             $return .= '<div class="row">';
             while (have_rows('image_section')) {
                the_row();
-               $return .= '<div class="col-md-2 grid-padding">';               
-               $imageArr = get_sub_field('grid_image');               
+               $return .= '<div class="col-xs-4 col-sm-3 col-md-2 grid-padding">';
+               $imageArr = get_sub_field('grid_image');
                
                $image_url = $imageArr['url'];
                $return .= '<a target="_blank" href="'.$image_url.'"><div class="grid-image" style="background-image: url('.$image_url.');"></div></a>';
                
-               $return .= '<p>' . $imageArr['width'] .' x ' .$imageArr['height']. '</p>';
-               $return .= '<button class="btn universal-btn btn-info" onclick="copyMe(\'img_'.$imageArr['id'].'\')">COPY HTML</button>';
+               $return .= '<div class="img-size">' . $imageArr['width'] .' x ' .$imageArr['height']. '</div>';
+               $return .= '<button class="btn universal-btn btn-info btn-copy-html" onclick="copyMe(\'img_'.$imageArr['id'].'\')">COPY HTML</button>';
                $return .= '<div class="copyDiv" id="img_'.$imageArr['id'].'"><a href="https://makerfaire.com/bay-area/"><img src="'.$imageArr['url'].'" alt="'.$imageArr['title'].'" width="'.$imageArr['width'].'" height="'.$imageArr['height'].'" border="0" /></a></div>';
                
                $return .= '</div>';
