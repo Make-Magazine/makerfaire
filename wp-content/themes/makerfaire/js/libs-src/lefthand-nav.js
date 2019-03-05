217c9734a48e0a33d151f7859b17bccf6cb19646
Rio Roth-Barrerio <rio@makermedia.com>
2019-03-05 15:28:11 -0800

    lefthand nav js

---
+++ wp-content/themes/makerfaire/js/libs-src/lefthand-nav.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function(){
+	jQuery('a[href^="#"]:not(a[href="#"])').click(function(){
+		  jQuery('html, body').animate({
+				scrollTop: jQuery('[name="' + jQuery.attr(this, 'href').substr(1) + '"]').offset().top
+		  }, 500);
+		  return false;
+	});
+	jQuery(document).scroll(function() {
+		var lnavBottom = jQuery("#menu-toolkit-left-hand-nav").offset().top + jQuery("#menu-toolkit-left-hand-nav").height();
+		var footerTop = jQuery("footer").offset().top;
+		console.log(lnavBottom);
+		console.log(footerTop);
+		if(lnavBottom > footerTop - 200 ) {
+			jQuery("#menu-toolkit-left-hand-nav").fadeOut("fast");
+		} else {
+			jQuery("#menu-toolkit-left-hand-nav").fadeIn("fast");
+		}
+	});
+});
