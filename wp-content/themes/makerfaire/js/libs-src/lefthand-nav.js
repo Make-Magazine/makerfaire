2296523913749132703c8abea3adb5e0b86ede5e
Rio Roth-Barrerio <rio@makermedia.com>
2019-03-06 10:20:05 -0800

    forget all this javascript crap, we're going with position: sticky!

--- wp-content/themes/makerfaire/js/libs-src/lefthand-nav.js
+++ wp-content/themes/makerfaire/js/libs-src/lefthand-nav.js
@@ -5,16 +5,4 @@ jQuery(document).ready(function(){
 		  }, 500);
 		  return false;
 	});
-	jQuery(document).scroll(function() {
-		var lnavBottom = jQuery("#menu-toolkit-left-hand-nav").offset().top + jQuery("#menu-toolkit-left-hand-nav").height();
-		var footerTop = jQuery("footer").offset().top;
-		// console.log( lnavBottom );
-		//console.log( footerTop - lnavBottom );
-		if( (footerTop - lnavBottom) <= 65  ) {
-			jQuery("#menu-toolkit-left-hand-nav").fadeOut("fast");
-		} else {
-			jQuery("#menu-toolkit-left-hand-nav").fadeIn("fast");
-		}
-	});
-
 });
