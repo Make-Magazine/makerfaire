246b6859072556a08fe8bbfdcf8cf69c4e0467bd
Travis Stone <travis@makermedia.com>
2019-03-12 13:29:38 -0700

    Tweak for hero image

--- wp-content/themes/makerfaire/page-flagship-faire-panels.php
+++ wp-content/themes/makerfaire/page-flagship-faire-panels.php
@@ -12,8 +12,8 @@ get_header(); ?>
     <!-- <div class="container"> -->
    
       <?php the_content(); ?>
-      
-      <div class="mf-full-hero" style="background-image: url('../wp-content/themes/makerfaire/images/hero_image_ba2019.png');">
+
+      <div class="mf-full-hero" style="background-image: url('../wp-content/themes/makerfaire/images/hero_ba2019_lg.jpg');">
          <div class="mf-left-text">
             <div class="mf-title-text">
                <h1>Maker Faire Bay Area 2019 <span>May 17-19</span></h1>
