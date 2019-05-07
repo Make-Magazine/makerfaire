0ee4caf3bf76de63f2362ea6dcbc2a74ed659772
Rio Roth-Barrerio <rio@makermedia.com>
2019-05-07 16:13:54 -0700

    fix for the old style of hero image

--- wp-content/themes/makerfaire/functions/panels.php
+++ wp-content/themes/makerfaire/functions/panels.php
@@ -562,7 +562,7 @@ function get1ColLayout() {
    }
 
    // Because of the aggressive caching on prod, it makes more sense to shuffle the array in javascript
-   $return .= '</section><script type="text/javascript">var heroArray = ' . json_encode($hero_array) . ';heroArray.sort(function(a, b){return 0.5 - Math.random()});jQuery(document).ready(function(){jQuery(".hero-img").attr("data-bg",heroArray[0]);bgSize( jQuery( ".hero-img" ), function( width, height ){if( height > 450 ) {jQuery(".hero-img").css( "height", "450px" );} else if( height > 320 ){jQuery( ".hero-img" ).css( "height", height + "px" );}});});</script>'; 
+   $return .= '</section><script type="text/javascript">var heroArray = ' . json_encode($hero_array) . ';heroArray.sort(function(a, b){return 0.5 - Math.random()});jQuery(document).ready(function(){jQuery(".hero-img").html(heroArray[0]);bgSize( jQuery( ".hero-img" ), function( width, height ){if( height > 450 ) {jQuery(".hero-img").css( "height", "450px" );} else if( height > 320 ){jQuery( ".hero-img" ).css( "height", height + "px" );}});});</script>'; 
    return $return;
 }
 
