829a760cfddf0ebf7758a0fa0ece78a87cfeba06
Rio Roth-Barrerio <rio@makermedia.com>
2019-05-13 11:44:11 -0700

    fix filter overlap and default filter value

--- wp-content/themes/makerfaire/page-meet-the-makers.php
+++ wp-content/themes/makerfaire/page-meet-the-makers.php
@@ -74,7 +74,7 @@ if ($noMakerText == '')
 						</ul>
 					</div>
             </div>
-            <div class="col-sm-4">
+            <div class="col-sm-4 filter-section">
                <div class="row">
                   <div class="col-sm-12 col-md-6">                     
                      <div class="dropdown" ng-if="locations.length > 0">
@@ -102,12 +102,12 @@ if ($noMakerText == '')
                            <i class="fa fa-chevron-down" aria-hidden="true"></i>
                         </button>
 
-                        <ul class="dropdown-menu" aria-labelledby="mtm-dropdownMenu">
+                        <ul class="dropdown-menu topic-menu" aria-labelledby="mtm-dropdownMenu">
                            <li>
                               <a class="pointer-on-hover" ng-click="makerSearch.categories = ''"><?php _e("All", 'makerfaire') ?></a>
                            </li>
-                           <li ng-repeat="tag in tags| orderBy: tag">                     
-                              <a class="pointer-on-hover" ng-click="makerSearch.categories = tag">{{ tag}}</a>
+                           <li ng-repeat="tag in tags | orderBy: tag">                     
+                              <a class="pointer-on-hover" ng-click="makerSearch.categories = tag">{{tag}}</a>
                            </li>
                         </ul>
                      </div>
