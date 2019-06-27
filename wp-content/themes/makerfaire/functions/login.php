f2b25ce3e070897e00de0f32a891d34af6abce9e
Alicia Williams <alicia@makermedia.com>
2019-06-27 11:37:07 -0600

    add invisible recaptcha to wp-login.php to deter spam bots

--- wp-content/themes/makerfaire/functions/login.php
+++ wp-content/themes/makerfaire/functions/login.php
@@ -1,70 +1,114 @@
 <?php
+
 function cookie_login_warning() { ?>
     <style type="text/css">
-		 .wp-core-ui #login { width: 80%; }
-		 #login::before {
-			 content: "We are unable to process your login as we have detected that you have cookies blocked. Please make sure cookies are enabled in your browser and try again.";
-			 text-align: center;
-			 font-size:42px;
-			 line-height: 46px;
-		 }
-		 #form-signin-wrapper {
-			 display: none;
-		 }
+        .wp-core-ui #login { width: 80%; }
+        #login::before {
+            content: "We are unable to process your login as we have detected that you have cookies blocked. Please make sure cookies are enabled in your browser and try again.";
+            text-align: center;
+            font-size:42px;
+            line-height: 46px;
+        }
+        #form-signin-wrapper {
+            display: none;
+        }
     </style>
-<?php }
+    <?php
 
-add_action( 'login_enqueue_scripts', 'cookie_login_warning' );
-
-/* redirect wp-login.php to the auth0 login page */
-function load_auth0_js() {
-  //auth0
-  wp_enqueue_script('auth0', 'https://cdn.auth0.com/js/auth0/9.6.1/auth0.min.js', array(), false );
-  wp_enqueue_script('auth0Login', get_stylesheet_directory_uri() . '/auth0/js/auth0login.js', array(), false);
 }
 
-add_action( 'login_enqueue_scripts', 'load_auth0_js',10 );
+add_action('login_enqueue_scripts', 'cookie_login_warning');
+
+/* redirect wp-login.php to the auth0 login page */
+
+function load_auth0_js() {
+    //auth0
+    wp_enqueue_script('auth0', 'https://cdn.auth0.com/js/auth0/9.6.1/auth0.min.js', array(), false);
+    wp_enqueue_script('auth0Login', get_stylesheet_directory_uri() . '/auth0/js/auth0login.js', array(), false);
+}
+
+add_action('login_enqueue_scripts', 'load_auth0_js', 10);
 
 /** Set up the Ajax Logout */
-add_action( 'wp_ajax_mm_wplogout',        'MM_wordpress_logout' );
-add_action( 'wp_ajax_nopriv_mm_wplogout', 'MM_wordpress_logout' );
+add_action('wp_ajax_mm_wplogout', 'MM_wordpress_logout');
+add_action('wp_ajax_nopriv_mm_wplogout', 'MM_wordpress_logout');
 
-function MM_wordpress_logout(){
+function MM_wordpress_logout() {
     //check_ajax_referer( 'ajax-logout-nonce', 'ajaxsecurity' );
     wp_logout();
     ob_clean(); // probably overkill for this, but good habit
     wp_send_json_success();
 }
 
-add_action( 'wp_ajax_mm_wplogin', 'MM_WPlogin' );
-add_action( 'wp_ajax_nopriv_mm_wplogin', 'MM_WPlogin' );
+add_action('wp_ajax_mm_wplogin', 'MM_WPlogin');
+add_action('wp_ajax_nopriv_mm_wplogin', 'MM_WPlogin');
 
 /** Set up the Ajax WP Login */
-function MM_WPlogin(){
-  //check_ajax_referer( 'ajax-login-nonce', 'ajaxsecurity' );
-  global $wpdb; // access to the database
+function MM_WPlogin() {
+    //check_ajax_referer( 'ajax-login-nonce', 'ajaxsecurity' );
+    global $wpdb; // access to the database
+    //use auth0 plugin to log people into wp
+    $a0_plugin = new WP_Auth0();
+    $a0_options = WP_Auth0_Options::Instance();
+    $users_repo = new WP_Auth0_UsersRepo($a0_options);
+    $users_repo->init();
 
-  //use auth0 plugin to log people into wp
-  $a0_plugin  = new WP_Auth0();
-  $a0_options = WP_Auth0_Options::Instance();
-  $users_repo = new WP_Auth0_UsersRepo( $a0_options );
-  $users_repo->init();
+    $login_manager = new WP_Auth0_LoginManager($users_repo, $a0_options);
+    $login_manager->init();
 
-  $login_manager = new WP_Auth0_LoginManager( $users_repo, $a0_options );
-  $login_manager->init();
+    //get the user information passed from auth0
+    $userinput = filter_input_array(INPUT_POST);
+    $userinfo = (object) $userinput['auth0_userProfile'];
+    $userinfo->email_verified = true;
+    $access_token = filter_input(INPUT_POST, 'auth0_access_token', FILTER_SANITIZE_STRING);
+    $id_token = filter_input(INPUT_POST, 'auth0_id_token', FILTER_SANITIZE_STRING);
 
-  //get the user information passed from auth0
-  $userinput     = filter_input_array(INPUT_POST);
-  $userinfo      = (object) $userinput['auth0_userProfile'];
-  $userinfo->email_verified = true;
-  $access_token = filter_input(INPUT_POST, 'auth0_access_token', FILTER_SANITIZE_STRING);
-  $id_token     = filter_input(INPUT_POST, 'auth0_id_token', FILTER_SANITIZE_STRING);
+    if ($login_manager->login_user($userinfo, $id_token, $access_token)) {
+        wp_send_json_success();
+    } else {
+        error_log('Failed login');
+        error_log(print_r($userinput, TRUE));
+        wp_send_json_error();
+    }
+}
 
-  if($login_manager->login_user( $userinfo, $id_token, $access_token)) {
-    wp_send_json_success();
-  }else{
-    error_log('Failed login');
-    error_log(print_r($userinput, TRUE));
-    wp_send_json_error();
-  }
+/**
+ * These Functions Add and Verify the Invisible Google reCAPTCHA on Login
+ * Normal users never see the wp-login.php page as they are forwarded to Auth0. 
+ * this will stop spam bots from signing up
+ */
+add_action('login_enqueue_scripts', 'login_recaptcha_script');
+
+function login_recaptcha_script() {
+    wp_register_script('recaptcha_login', 'https://www.google.com/recaptcha/api.js');
+    wp_enqueue_script('recaptcha_login');
+}
+
+add_action('login_form', 'display_recaptcha_on_login');
+
+function display_recaptcha_on_login() {
+    echo "<script>
+            function onSubmit(token) {
+                document.getElementById('loginform').submit();
+            }
+          </script>
+            <button class='g-recaptcha' data-sitekey='6Lf_-kEUAAAAAHtDfGBAleSvWSynALMcgI1hc_tP' data-callback='onSubmit' data-size='invisible' style='display:none;'>Submit</button>";
+}
+
+add_filter('wp_authenticate_user', 'verify_recaptcha_on_login', 10, 2);
+
+function verify_recaptcha_on_login($user, $password) {
+    if (isset($_POST['g-recaptcha-response'])) {
+        $response = wp_remote_get('https://www.google.com/recaptcha/api/siteverify?secret=6Lf_-kEUAAAAAHtDfGBAleSvWSynALMcgI1hc_tP&response=' . $_POST['g-recaptcha-response']);
+        $response = json_decode($response['body'], true);
+
+        if (true == $response['success']) {
+            return $user;
+        } else {
+            // FIXME: This one fires if your password is incorrect... Check if password was incorrect before returning this error...
+            return new WP_Error( 'Captcha Invalid', __('<strong>ERROR</strong>: You are a bot') );
+        }
+    } else {
+        return new WP_Error('Captcha Invalid', __('<strong>ERROR</strong>: You are a bot. If not then enable JavaScript.'));
+    }
 }
