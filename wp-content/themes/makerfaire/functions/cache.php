a3af8ca8628b5d88f6a4f2ac4e1d5a077affde27
Rio Roth-Barrerio <rio@makermedia.com>
2020-03-03 11:34:24 -0800

    autoptimize automated cache clearing when cache gets over 256MB

---
+++ wp-content/themes/makerfaire/functions/cache.php
@@ -0,0 +1,15 @@
+add_action( 'init', 'clear_large_autoptimize_cache' );
+
+function clear_large_autoptimize_cache () {
+	# Automatically clear autoptimizeCache if it goes beyond 256MB
+	if (class_exists('autoptimizeCache')) {
+		 $myMaxSize = 256000; # You may change this value to lower like 100000 for 100MB if you have limited server space
+		 $statArr=autoptimizeCache::stats(); 
+		 $cacheSize=round($statArr[1]/1024);
+		 if ($cacheSize>$myMaxSize){
+			 autoptimizeCache::clearall();
+			 header("Refresh:0"); # Refresh the page so that autoptimize can create new cache files
+		 }
+	}
+}
+
