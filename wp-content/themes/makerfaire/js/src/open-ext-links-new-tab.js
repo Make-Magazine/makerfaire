029bfe753853981dd5e95d22c0fdab1fe2cdcc75
Rio Roth-Barrerio <rio@makermedia.com>
2020-03-05 11:53:07 -0800

    stop the javascript links from opening new tabs

--- wp-content/themes/makerfaire/js/src/open-ext-links-new-tab.js
+++ wp-content/themes/makerfaire/js/src/open-ext-links-new-tab.js
@@ -1,7 +1,7 @@
 // Open external links in new tab, unless it's a make site, or targeted self, or an email link
 jQuery(document).ready(function($) {
  $(document.links).filter(function() {
-   if($(this).attr("target") != "_self" && $(this).not('[href*="mailto:"]') && $(this).not('[href*="javascript:void(0);"]')){
+   if($(this).attr("target") != "_self" && $(this).not('[href*="mailto:"]') && $(this).not('[href*="javascript:void(0);"]') && $(this).attr("href") != "javascript:void(0);"){
        return this.hostname.indexOf("make") == -1;
    }
  }).attr('target', '_blank');
