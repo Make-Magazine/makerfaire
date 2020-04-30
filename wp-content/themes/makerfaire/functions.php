c9d5de5ce028bca79ca4e37ff6f120ff081f75d5
Rio Roth-Barrerio <rio@makermedia.com>
2020-04-30 10:08:25 -0700

    remove gf-limit-checkboxes class, fix modal and button styling for mobile

--- wp-content/themes/makerfaire/functions.php
+++ wp-content/themes/makerfaire/functions.php
@@ -53,9 +53,6 @@ include_once dirname(__FILE__) . '/api/admin-settings.php';
 // Load the functions for the Applications API
 include_once dirname(__FILE__) . '/api/v2/functions.php';
 
-// Gravity Forms Specific Plugins and Classes
-include_once TEMPLATEPATH . '/classes/gf-limit-checkboxes.php';
-
 //include_once TEMPLATEPATH. '/classes/gf-entry-datatables.php';
 include_once TEMPLATEPATH . '/classes/gf-helper.php';
 include_once TEMPLATEPATH . '/classes/makerfaire-helper.php';
