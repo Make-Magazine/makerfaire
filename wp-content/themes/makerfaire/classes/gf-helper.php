2650ff8ca910dd60f9a413dcbf999c577af6abda
Alicia Williams <alicia@make.co>
2020-05-15 16:18:02 -0600

    return taxonomy test instead of code

--- wp-content/themes/makerfaire/classes/gf-helper.php
+++ wp-content/themes/makerfaire/classes/gf-helper.php
@@ -202,9 +202,7 @@ function setCatName($value, $field, $lead, $form) {
     } else {
         $field_type = RGFormsModel::get_input_type($field);
         if (in_array($field_type, array('checkbox', 'radio'))) {
-            //echo '$value before='.$value.'<br/>';        
             $value = RGFormsModel::get_lead_field_value($lead, $field);
-            //echo '$value after ='.GFCommon::get_lead_field_display($field, $value, $lead["currency"], true).'<br/>';
             return GFCommon::get_lead_field_display($field, $value, $lead["currency"], true);
             $value = get_CPT_name($value);
         }
@@ -217,6 +215,13 @@ add_filter('gform_export_field_value', 'set_export_values', 10, 4);
 function set_export_values($value, $form_id, $field_id, $lead) {
     if ($field_id == 320 || strpos($field_id, '302.') !== false) {
         $value = get_CPT_name($value);
+    } else {
+        $field_type = RGFormsModel::get_input_type($field);
+        if (in_array($field_type, array('checkbox', 'radio'))) {
+            $value = RGFormsModel::get_lead_field_value($lead, $field);
+            return GFCommon::get_lead_field_display($field, $value, $lead["currency"], true);
+            $value = get_CPT_name($value);
+        }
     }
     return $value;
 }
