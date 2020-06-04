31d97c372df1b98dcc4598ceb470aef003ff689a
Rio Roth-Barrerio <rio@makermedia.com>
2020-06-04 11:24:35 -0700

    reenable richedit, add rest cache

--- wp-content/themes/makerfaire/functions.php
+++ wp-content/themes/makerfaire/functions.php
@@ -255,6 +255,7 @@ function load_admin_scripts() {
     wp_enqueue_script('make-gravityforms-admin', get_stylesheet_directory_uri() . '/js/libs/gravityformsadmin.js', array('jquery', 'jquery-ui-tabs'), $my_version);
     wp_enqueue_script('make-fairesigns-admin', get_stylesheet_directory_uri() . '/js/libs/mf_fairesigns.js', array('jquery'), $my_version);
     wp_enqueue_script('jquery-datetimepicker', get_stylesheet_directory_uri() . '/js/libs/jquery.datetimepicker.js', array('jquery'), null);
+
     //wp_enqueue_script('make-bootstrap', get_stylesheet_directory_uri() . '/js/built-libs.min.js', array('jquery'));
     wp_enqueue_script('admin-scripts', get_stylesheet_directory_uri() . '/js/built-admin-scripts.min.js', array('jquery'), $my_version);
 
@@ -280,7 +281,7 @@ function load_admin_scripts() {
 add_action('admin_enqueue_scripts', 'load_admin_scripts');
 
 // Remove richedit
-add_filter('user_can_richedit', '__return_false', 50);
+// add_filter('user_can_richedit', '__return_false', 50);
 
 //this function is used to enqueue the VUE map
 function mf_map() {
