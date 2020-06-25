27788f4db851d7daec5a6cb781307a1fe1e0591d
Rio Roth-Barrerio <rio@makermedia.com>
2020-06-25 16:07:39 -0700

    show the span, not the svg

--- wp-content/themes/makerfaire/js/libs-src/global.js
+++ wp-content/themes/makerfaire/js/libs-src/global.js
@@ -3,6 +3,6 @@ jQuery(document).ready(function(){
 	if(jQuery("#cliModalClose").length) {
 		jQuery("#cliModalClose").css({"width": "180px", "top": "20px", "color": "#005E9A"});
 		jQuery("#cliModalClose svg").remove();
-		jQuery("#cliModalClose .wt-cli-sr-only").text("Save Configuration");
+		jQuery("#cliModalClose .wt-cli-sr-only").text("Save Configuration").css("display", "block");
 	}
 });
