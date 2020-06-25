e8c452dfa0826e0086e5c7b151a69f28b5a44ea5
Rio Roth-Barrerio <rio@makermedia.com>
2020-06-25 16:21:42 -0700

    insert after ordering

--- wp-content/themes/makerfaire/js/libs-src/global.js
+++ wp-content/themes/makerfaire/js/libs-src/global.js
@@ -1,7 +1,8 @@
+
 jQuery(document).ready(function(){
 	// change up the cookie privacy popup styles and language
 	if(jQuery("#cliModalClose").length) {
-		jQuery("#cliModalClose").after(".cli-tab-section-container").css({"width": "180px", "position": "relative", "color": "#005E9A"});
+		jQuery("#cliModalClose").insertAfter(jQuery(".cli-tab-section-container")).css({"width": "180px", "position": "relative", "color": "#005E9A"});
 		jQuery("#cliModalClose svg").remove();
 		jQuery("#cliModalClose .wt-cli-sr-only").text("Save Configuration").css("display", "block");
 	}
