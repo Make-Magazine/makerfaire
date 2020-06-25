0bb510a9719a9835fe5e4b0a6c28b7cdb4a3e03e
Rio Roth-Barrerio <rio@makermedia.com>
2020-06-25 16:47:50 -0700

    final modal padding

--- wp-content/themes/makerfaire/js/libs-src/global.js
+++ wp-content/themes/makerfaire/js/libs-src/global.js
@@ -2,7 +2,7 @@
 jQuery(document).ready(function(){
 	// change up the cookie privacy popup styles and language
 	if(jQuery("#cliModalClose").length) {
-		jQuery("#cliModalClose").insertAfter(jQuery(".cli-tab-section-container")).css({"width": "180px", "position": "relative", "color": "#005E9A"});
+		jQuery("#cliModalClose").insertAfter(jQuery(".cli-tab-section-container")).css({"width": "180px", "position": "relative", "color": "#005E9A", "margin-top": "10px", "margin-left": "-10px"});
 		jQuery("#cliModalClose svg").remove();
 		jQuery("#cliModalClose .wt-cli-sr-only").text("Save Configuration").css("display", "block");
 	}
