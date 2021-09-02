b8a4302000025edb7910cf285a64482b1f2c9512
Rio Roth-Barreiro <rio@make.co>
2021-09-02 14:23:42 -0700

    need all the category functions back

---
+++ wp-content/themes/makerfaire/functions/makerfaire_categories.php
@@ -0,0 +1,146 @@
+<?php
+function makerfaire_category_init() {
+    register_taxonomy( 'makerfaire_category', array( 'post', 'page' ), array(
+        'hierarchical'            => true,
+        'show_in_rest'            => true,
+        'public'                  => true,
+        'show_in_nav_menus'       => true,
+        'show_ui'                 => true,
+        'query_var'               => 'makerfaire_category',
+        'rewrite'                 => true,
+        'capabilities'            => array(
+            'manage_terms'  => 'edit_posts',
+            'edit_terms'    => 'edit_posts',
+            'delete_terms'  => 'edit_posts',
+            'assign_terms'  => 'edit_posts'
+        ),
+        'labels'                  => array(
+            'name'                       =>  __( 'makerfaire categories', 'makerfaire' ),
+            'singular_name'              =>  __( 'makerfaire category', 'makerfaire' ),
+            'search_items'               =>  __( 'Search makerfaire categories', 'makerfaire' ),
+            'popular_items'              =>  __( 'Popular makerfaire categories', 'makerfaire' ),
+            'all_items'                  =>  __( 'All makerfaire categories', 'makerfaire' ),
+            'parent_item'                =>  __( 'Parent makerfaire category', 'makerfaire' ),
+            'parent_item_colon'          =>  __( 'Parent makerfaire category:', 'makerfaire' ),
+            'edit_item'                  =>  __( 'Edit makerfaire category', 'makerfaire' ),
+            'update_item'                =>  __( 'Update makerfaire category', 'makerfaire' ),
+            'add_new_item'               =>  __( 'New makerfaire category', 'makerfaire' ),
+            'new_item_name'              =>  __( 'New makerfaire category', 'makerfaire' ),
+            'separate_items_with_commas' =>  __( 'makerfaire categories separated by comma', 'makerfaire' ),
+            'add_or_remove_items'        =>  __( 'Add or remove makerfaire categories', 'makerfaire' ),
+            'choose_from_most_used'      =>  __( 'Choose from the most used makerfaire categories', 'makerfaire' ),
+            'menu_name'                  =>  __( 'MF Categories', 'makerfaire' ),
+        ),
+    ) );
+    //Make Project Categories
+    register_taxonomy( 'makeprojects_category', array( 'post', 'page' ), array(
+        'hierarchical'            => true,
+        'public'                  => true,
+        'show_in_rest'            => true,
+        'show_in_nav_menus'       => true,
+        'show_ui'                 => true,
+        'query_var'               => 'makeprojects_category',
+        'rewrite'                 => true,
+        'capabilities'            => array(
+            'manage_terms'  => 'edit_posts',
+            'edit_terms'    => 'edit_posts',
+            'delete_terms'  => 'edit_posts',
+            'assign_terms'  => 'edit_posts'
+        ),
+        'labels'                  => array(
+            'name'                       =>  __( 'makeprojects categories', 'makerfaire' ),
+            'singular_name'              =>  __( 'makeprojects category', 'makerfaire' ),
+            'search_items'               =>  __( 'Search makeprojects categories', 'makerfaire' ),
+            'popular_items'              =>  __( 'Popular makeprojects categories', 'makerfaire' ),
+            'all_items'                  =>  __( 'All makeprojects categories', 'makerfaire' ),
+            'parent_item'                =>  __( 'Parent makeprojects category', 'makerfaire' ),
+            'parent_item_colon'          =>  __( 'Parent makeprojects category:', 'makerfaire' ),
+            'edit_item'                  =>  __( 'Edit makeprojects category', 'makerfaire' ),
+            'update_item'                =>  __( 'Update makeprojects category', 'makerfaire' ),
+            'add_new_item'               =>  __( 'New makeprojects category', 'makerfaire' ),
+            'new_item_name'              =>  __( 'New makeprojects category', 'makerfaire' ),
+            'separate_items_with_commas' =>  __( 'makerfaire makeprojects separated by comma', 'makerfaire' ),
+            'add_or_remove_items'        =>  __( 'Add or remove makeprojects categories', 'makerfaire' ),
+            'choose_from_most_used'      =>  __( 'Choose from the most used makeprojects categories', 'makerfaire' ),
+            'menu_name'                  =>  __( 'MP Categories', 'makerfaire' ),
+        ),
+    ) );
+    
+}
+add_action( 'init', 'makerfaire_category_init' );
+
+//prelim locations
+function prelim_loc_init() {
+    //      Bay Area
+    register_taxonomy( 'ba_prelim_loc', array( 'post', 'page' ), array(
+        'hierarchical'            => false,
+        'show_in_nav_menus'       => true,
+        'show_ui'                 => true,
+        'query_var'               => 'ba_prelim_loc',
+        'capabilities'            => array(
+            'manage_terms'  => 'edit_posts',
+            'edit_terms'    => 'edit_posts',
+            'delete_terms'  => 'edit_posts',
+            'assign_terms'  => 'edit_posts'
+        ),
+        'labels'                  => array(
+            'name'                       =>  __( 'Bay Area Preliminary Locations', 'makerfaire' ),
+            'singular_name'              =>  __( 'Bay Area Preliminary Location', 'makerfaire' ),
+            'search_items'               =>  __( 'Search Locations', 'makerfaire' ),
+            'popular_items'              =>  __( 'Popular Bay Area Preliminary Locations', 'makerfaire' ),
+            'all_items'                  =>  __( 'All Bay Area Preliminary Locations', 'makerfaire' ),
+            'edit_item'                  =>  __( 'Edit Bay Area Preliminary Location', 'makerfaire' ),
+            'update_item'                =>  __( 'Update Bay Area Preliminary Location', 'makerfaire' ),
+            'add_new_item'               =>  __( 'Add Preliminary Location', 'makerfaire' ),
+            'new_item_name'              =>  __( 'New Bay Area Preliminary Location', 'makerfaire' ),
+            'separate_items_with_commas' =>  __( 'Bay Area Preliminary Locations separated by comma', 'makerfaire' ),
+            'add_or_remove_items'        =>  __( 'Add or remove Bay Area Preliminary Locations', 'makerfaire' ),
+            'choose_from_most_used'      =>  __( 'Choose from the most used BA Preliminary Locations', 'makerfaire' ),
+            'menu_name'                  =>  __( 'BA Prelim Locs', 'makerfaire' ),
+        ),
+    ) );
+    
+    //        New York
+    register_taxonomy( 'ny_prelim_loc', array( 'post', 'page' ), array(
+        'hierarchical'            => false,
+        'show_in_nav_menus'       => true,
+        'show_ui'                 => true,
+        'query_var'               => 'ny_prelim_loc',
+        'capabilities'            => array(
+            'manage_terms'  => 'edit_posts',
+            'edit_terms'    => 'edit_posts',
+            'delete_terms'  => 'edit_posts',
+            'assign_terms'  => 'edit_posts'
+        ),
+        'labels'                  => array(
+            'name'                       =>  __( 'New York Preliminary Locations', 'makerfaire' ),
+            'singular_name'              =>  __( 'New York Preliminary Location', 'makerfaire' ),
+            'search_items'               =>  __( 'Search Locations', 'makerfaire' ),
+            'popular_items'              =>  __( 'Popular Locations', 'makerfaire' ),
+            'all_items'                  =>  __( 'All New York Preliminary Locations', 'makerfaire' ),
+            'edit_item'                  =>  __( 'Edit New York Preliminary Location', 'makerfaire' ),
+            'update_item'                =>  __( 'Update New York Preliminary Location', 'makerfaire' ),
+            'add_new_item'               =>  __( 'Add Preliminary Location', 'makerfaire' ),
+            'new_item_name'              =>  __( 'New New York Preliminary Location', 'makerfaire' ),
+            'separate_items_with_commas' =>  __( 'New York Preliminary Locations separated by comma', 'makerfaire' ),
+            'add_or_remove_items'        =>  __( 'Add or remove New York Preliminary Locations', 'makerfaire' ),
+            'choose_from_most_used'      =>  __( 'Choose from the most used NY Preliminary Locations', 'makerfaire' ),
+            'menu_name'                  =>  __( 'NY Prelim Locs', 'makerfaire' ),
+        ),
+    ) );
+}
+add_action( 'init', 'prelim_loc_init' );
+
+function get_CPT_name($category){
+    $cat_id = (int) $category;
+    if($cat_id!=0){
+        $typeArr = array('makerfaire_category','ny_prelim_loc','ba_prelim_loc','makeprojects_category');
+        
+        foreach($typeArr as $type){
+            $cat = get_term( $cat_id, $type );
+            if ($cat && !is_wp_error( $cat ) )
+                return $cat->name;
+        }
+    }
+    return $category;
+}
