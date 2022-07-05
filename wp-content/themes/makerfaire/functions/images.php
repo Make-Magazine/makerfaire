c660e78df21510965a1a9e0290b17749a374f2b4
Rio Roth-Barreiro <rio@make.co>
2022-07-05 13:10:40 -0700

    get the url rather than the absolute path for random images for the upcoming faire widget

--- wp-content/themes/makerfaire/functions/images.php
+++ wp-content/themes/makerfaire/functions/images.php
@@ -3,5 +3,5 @@
 function random_pic($dir = '/uploads'){
     $files = glob($dir . '/*.*');
     $file = array_rand($files);
-    return $files[$file];
+    return str_replace(ABSPATH, get_site_url() . '/', $files[$file]);
 }
