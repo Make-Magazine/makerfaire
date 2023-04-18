5268656a99aaf4ec91db08d9578fc11f5ef058d0
Alicia Williams <alicia@make.co>
2023-04-18 09:10:38 -0600

    fix for country and region, you must put the name as the key or it saves the key and not hte name in the db

--- resource-mgmt/ajax/tableOptions.php
+++ resource-mgmt/ajax/tableOptions.php
@@ -50,13 +50,78 @@ $tableOptions['wp_rmt_entry_resources']['fkey']    = array(
 $tableOptions['wp_mf_global_faire']['addlFields'][] = array(
         'fieldName' => 'venue_address_region', 'filterType'=>'dropdown', 'fieldLabel'=>'Region', 
 		'enableCellEdit' => true, 'width' => 150,
-        'options' => array('Europe', 'North America', 'Asia', 'Australia', 'South America', 'Middle East', 'Pacific', 'Africa')
+        'options' => array('Europe'=>'Europe', 'North America'=>'North America', 'Asia'=>'Asia', 'Australia'=>'Australia', 
+                'South America'=>'South America', 'Middle East'=>'Middle East', 'Pacific'=>'Pacific', 'Africa'=>'Africa')
     );
 $tableOptions['wp_mf_global_faire']['addlFields'][] = array(
         'fieldName' => 'venue_address_country', 'filterType'=>'dropdown', 'fieldLabel'=>'Country', 
         'enableCellEdit' => true, 'width' => 150,
         
-        'options' => array("Afghanistan","Albania","Algeria","American Samoa", "Andorra", "Angola", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Costa Rica", "Côte D'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic People's Republic of Korea", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "England", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea Bissau", "Guinea", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macao", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar(Burma)", "Namibia", "Nauru", "Nepal", "Netherlands Antilles", "Netherlands", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Northern Ireland", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Republic of the Congo", "Réunion", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sào Tomé And Príncipe", "Saudi Arabia", "Scotland", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "US Virgin Islands", "Uzbekistan", "Vanuatu", "Vatican", "Venezuela", "Vietnam", "Wales", "Yemen", "Zambia", "Zimbabwe")
+        'options' => array("Afghanistan"=>"Afghanistan", "Albania"=>"Albania", "Algeria"=>"Algeria",
+                "American Samoa"=>"American Samoa", "Andorra"=>"Andorra", "Angola"=>"Angola", "Antarctica"=>"Antarctica", 
+                "Antigua and Barbuda"=>"Antigua and Barbuda", "Argentina"=>"Argentina", "Armenia"=>"Armenia", 
+                "Aruba"=>"Aruba", "Australia"=>"Australia", "Austria"=>"Austria", "Azerbaijan"=>"Azerbaijan", 
+                "Bahamas"=>"Bahamas", "Bahrain"=>"Bahrain", "Bangladesh"=>"Bangladesh", "Barbados"=>"Barbados", 
+                "Belarus"=>"Belarus", "Belgium"=>"Belgium", "Belize"=>"Belize", "Benin"=>"Benin", "Bermuda"=>"Bermuda", 
+                "Bhutan"=>"Bhutan", "Bolivia"=>"Bolivia", "Bosnia and Herzegovina"=>"Bosnia and Herzegovina", 
+                "Botswana"=>"Botswana", "Brazil"=>"Brazil", "British Virgin Islands"=>"British Virgin Islands", 
+                "Brunei"=>"Brunei", "Bulgaria"=>"Bulgaria", "Burkina Faso"=>"Burkina Faso", "Burundi"=>"Burundi",  
+                "Cambodia"=> "Cambodia",  "Cameroon"=> "Cameroon",  "Canada"=> "Canada",  "Cape Verde"=> "Cape Verde",
+                "Cayman Islands"=> "Cayman Islands",  "Central African Republic"=> "Central African Republic",  
+                "Chad"=> "Chad",  "Chile"=> "Chile",  "China"=> "China",  "Colombia"=> "Colombia",  "Comoros"=> "Comoros",
+                "Costa Rica"=> "Costa Rica",  "Côte D'Ivoire"=> "Côte D'Ivoire",  "Croatia"=> "Croatia",  "Cuba"=> "Cuba",  
+                "Cyprus"=> "Cyprus",  "Czech Republic"=> "Czech Republic",  
+                "Democratic People's Republic of Korea"=> "Democratic People's Republic of Korea",  
+                "Democratic Republic of the Congo"=> "Democratic Republic of the Congo",  "Denmark"=> "Denmark",  
+                "Djibouti"=> "Djibouti",  "Dominica"=> "Dominica",  "Dominican Republic"=> "Dominican Republic",  
+                "Ecuador"=> "Ecuador",  "Egypt"=> "Egypt",  "El Salvador"=> "El Salvador",  "England"=> "England",  
+                "Equatorial Guinea"=> "Equatorial Guinea",  "Eritrea"=> "Eritrea",  "Estonia"=> "Estonia",  
+                "Ethiopia"=> "Ethiopia",  "Faroe Islands"=> "Faroe Islands",  "Fiji"=> "Fiji",  "Finland"=> "Finland",  
+                "France"=> "France",  "French Polynesia"=> "French Polynesia",  "Gabon"=> "Gabon",  "Gambia"=> "Gambia",  
+                "Georgia"=> "Georgia",  "Germany"=> "Germany",  "Ghana"=> "Ghana",  "Greece"=> "Greece",  
+                "Greenland"=> "Greenland",  "Grenada"=> "Grenada",  "Guadeloupe"=> "Guadeloupe",  "Guam"=> "Guam",  
+                "Guatemala"=> "Guatemala",  "Guinea Bissau"=> "Guinea Bissau",  "Guinea"=> "Guinea",  "Guyana"=> "Guyana",  
+                "Haiti"=> "Haiti",  "Honduras"=> "Honduras",  "Hong Kong"=> "Hong Kong",  "Hungary"=> "Hungary",  
+                "Iceland"=> "Iceland",  "India"=> "India",  "Indonesia"=> "Indonesia",  "Iran"=> "Iran",  
+                "Iraq"=> "Iraq",  "Ireland"=> "Ireland",  "Israel"=> "Israel",  "Italy"=> "Italy",  "Jamaica"=> "Jamaica",  
+                "Japan"=> "Japan",  "Jersey"=> "Jersey",  "Jordan"=> "Jordan",  "Kazakhstan"=> "Kazakhstan",  
+                "Kenya"=> "Kenya",  "Kiribati"=> "Kiribati",  "Kosovo"=> "Kosovo",  "Kuwait"=> "Kuwait",  
+                "Kyrgyzstan"=> "Kyrgyzstan",  "Laos"=> "Laos",  "Latvia"=> "Latvia",  "Lebanon"=> "Lebanon",  
+                "Lesotho"=> "Lesotho",  "Liberia"=> "Liberia",  "Libya"=> "Libya",  "Liechtenstein"=> "Liechtenstein",  
+                "Lithuania"=> "Lithuania",  "Luxembourg"=> "Luxembourg",  "Macao"=> "Macao",  "Macedonia"=> "Macedonia",  
+                "Madagascar"=> "Madagascar",  "Malawi"=> "Malawi",  "Malaysia"=> "Malaysia",  "Maldives"=> "Maldives",  
+                "Mali"=> "Mali",  "Malta"=> "Malta",  "Marshall Islands"=> "Marshall Islands",  
+                "Mauritania"=> "Mauritania",  "Mauritania"=> "Mauritania",  "Mauritius"=> "Mauritius",  "Mexico"=> "Mexico",
+                "Micronesia"=> "Micronesia",  "Moldova"=> "Moldova",  "Monaco"=> "Monaco",  "Mongolia"=> "Mongolia",  
+                "Montenegro"=> "Montenegro",  "Morocco"=> "Morocco",  "Mozambique"=> "Mozambique",  
+                "Myanmar(Burma)"=> "Myanmar(Burma)",  "Namibia"=> "Namibia",  "Nauru"=> "Nauru",  "Nepal"=> "Nepal",  
+                "Netherlands Antilles"=> "Netherlands Antilles",  "Netherlands"=> "Netherlands",  
+                "New Caledonia"=> "New Caledonia",  "New Zealand"=> "New Zealand",  "Nicaragua"=> "Nicaragua",  
+                "Niger"=> "Niger",  "Nigeria"=> "Nigeria",  "Northern Ireland"=> "Northern Ireland",  
+                "Northern Mariana Islands"=> "Northern Mariana Islands",  "Norway"=> "Norway",  "Oman"=> "Oman",  
+                "Pakistan"=> "Pakistan",  "Palau"=> "Palau",  "Palestine"=> "Palestine",  "Panama"=> "Panama",  
+                "Papua New Guinea"=> "Papua New Guinea",  "Paraguay"=> "Paraguay",  "Peru"=> "Peru",  
+                "Philippines"=> "Philippines",  "Poland"=> "Poland",  "Portugal"=> "Portugal",  
+                "Puerto Rico"=> "Puerto Rico",  "Qatar"=> "Qatar",  "Republic of the Congo"=> "Republic of the Congo",  
+                "Réunion"=> "Réunion",  "Romania"=> "Romania",  "Russia"=> "Russia",  "Rwanda"=> "Rwanda",  
+                "Saint Kitts and Nevis"=> "Saint Kitts and Nevis",  "Saint Lucia"=> "Saint Lucia",  
+                "Saint Vincent and the Grenadines"=> "Saint Vincent and the Grenadines",  "Samoa"=> "Samoa",  
+                "San Marino"=> "San Marino",  "Sào Tomé And Príncipe"=> "Sào Tomé And Príncipe",  
+                "Saudi Arabia"=> "Saudi Arabia",  "Scotland"=> "Scotland",  "Senegal"=> "Senegal",  "Serbia"=> "Serbia",  
+                "Seychelles"=> "Seychelles",  "Sierra Leone"=> "Sierra Leone",  "Singapore"=> "Singapore", 
+                "Slovakia"=> "Slovakia",  "Slovenia"=> "Slovenia",  "Solomon Islands"=> "Solomon Islands",  
+                "Somalia"=> "Somalia",  "South Africa"=> "South Africa",  "South Korea"=> "South Korea",  "Spain"=> "Spain",
+                "Sri Lanka"=> "Sri Lanka",  "Sudan"=> "Sudan",  "Suriname"=> "Suriname",  "Swaziland"=> "Swaziland",  
+                "Sweden"=> "Sweden",  "Switzerland"=> "Switzerland",  "Syria"=> "Syria",  "Taiwan"=> "Taiwan",  
+                "Tajikistan"=> "Tajikistan",  "Tanzania"=> "Tanzania",  "Thailand"=> "Thailand",  
+                "Timor-Leste"=> "Timor-Leste",  "Togo"=> "Togo",  "Tonga"=> "Tonga",  
+                "Trinidad and Tobago"=> "Trinidad and Tobago",  "Tunisia"=> "Tunisia",  "Turkey"=> "Turkey", 
+                "Turkmenistan"=> "Turkmenistan",  "Tuvalu"=> "Tuvalu",  "Uganda"=> "Uganda",  "Ukraine"=> "Ukraine", 
+                "United Arab Emirates"=> "United Arab Emirates",  "United Kingdom"=> "United Kingdom",  
+                "United States"=> "United States",  "Uruguay"=> "Uruguay",  "US Virgin Islands"=> "US Virgin Islands",  
+                "Uzbekistan"=> "Uzbekistan",  "Vanuatu"=> "Vanuatu",  "Vatican"=> "Vatican",  "Venezuela"=> "Venezuela",  
+                "Vietnam"=> "Vietnam",  "Wales"=> "Wales",  "Yemen"=> "Yemen",  "Zambia"=> "Zambia",  
+                "Zimbabwe"=> "Zimbabwe")
            );
 $tableOptions['wp_mf_global_faire']['addlFields'][] = array(
     'fieldName' => 'event_type', 'filterType'=>'dropdown','fieldLabel'=>'Event Type', 'enableCellEdit' => true,
@@ -80,14 +145,12 @@ $tableOptions['wp_mf_global_faire']['addlFields'][] = array(
                 'null_on_blank'=>true
   );
 $tableOptions['wp_mf_global_faire']['addlFields'][] = array(
-                'fieldName' => 'lat', 'type'=>'number', 'fieldLabel' => 'Latitude', 
-                'null_on_blank'=>true,
-                'sort' => array('direction' => 'uiGridConstants.DESC', 'priority' => 1)
+                'fieldName' => 'lat', 'fieldLabel' => 'Latitude', 
+                'null_on_blank'=>true                
 );
 $tableOptions['wp_mf_global_faire']['addlFields'][] = array(
-                'fieldName' => 'lng', 'type'=>'number',  'fieldLabel' => 'Longitude', 
-                'null_on_blank'=>true,
-                'sort' => array('direction' => 'uiGridConstants.DESC', 'priority' => 1)
+                'fieldName' => 'lng', 'fieldLabel' => 'Longitude', 
+                'null_on_blank'=>true
 );
 $tableOptions['wp_mf_global_faire']['addlFields'][] = array(
 		'fieldName' => 'ID', 'type'=>'number',
