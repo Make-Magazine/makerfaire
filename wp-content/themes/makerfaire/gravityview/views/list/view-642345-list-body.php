db812d6f2b4df561d830cc5d3fdae9f94199c786
Alicia Williams <alicia@make.co>
2023-07-26 09:48:34 -0600

    new gravity view custom layout for the ba23 maker portal

---
+++ wp-content/themes/makerfaire/gravityview/views/list/view-642345-list-body.php
@@ -0,0 +1,318 @@
+<?php
+/**
+ * The entry loop for the list output.
+ *
+ * @global \GV\Template_Context $gravityview
+ */
+
+if ( ! isset( $gravityview ) || empty( $gravityview->template ) ) {
+	gravityview()->log->error( '{file} template loaded without context', array( 'file' => __FILE__ ) );
+	return;
+}
+$viewID = '642345'; 
+$publicInfoViewID = '642349';
+
+$template = $gravityview->template;
+$current_user = wp_get_current_user();
+
+//require_once our model
+require_once( get_template_directory().'/models/maker.php' );
+
+//instantiate the model
+$maker   = new maker($current_user->user_email);
+
+/** @action `gravityview/template/list/body/before` */
+$template::body_before( $gravityview );
+?>
+<div class="content col-md-12 maker-admin-manage-faire-entries-mobile">
+  <script>
+  jQuery(document).ready(function() {
+    jQuery('a[href$="jpg"], a[href$="png"], a[href$="jpeg"]').fancybox();
+    //jQuery('[data-toggle="tooltip"]').tooltip();
+    jQuery('body').tooltip( {selector: '[data-toggle=tooltip]'} );
+    //returns content based on separate element - For Notificationd, get your tickets and
+    jQuery(".toggle-popover").popover({
+        html : true,
+        placement : "auto bottom",
+        content: function() {
+          return jQuery(this).next('.popover-content').html();
+        },
+        title: ''
+    });
+    jQuery('body').on('click', function (e) {
+      //did not click a popover toggle or popover
+      if (jQuery(e.target).data('toggle') !== 'popover'
+          && jQuery(e.target).parents('.popover.in').length === 0) {
+          jQuery('[data-toggle="popover"]').popover('hide');
+      }
+    });
+  });
+  </script>
+<?php
+
+// There are no entries.
+if ( ! $gravityview->entries->count() ) {
+
+	$no_results_css_class = 'gv-no-results gv-no-results-text';
+
+	if ( 1 === (int) $gravityview->view->settings->get( 'no_entries_options', '0' ) ) {
+		$no_results_css_class = 'gv-no-results gv-no-results-form';
+	}
+
+	?>
+	<div class="gv-list-view <?php echo esc_attr( $no_results_css_class ); ?>">
+		<div class="gv-list-view-title">
+			<h3><?php echo gv_no_results( true, $gravityview ); ?></h3>
+		</div>
+	</div>
+	<?php
+} else {
+	$current_user = wp_get_current_user();
+	?>
+	<div class="clearfix">
+		<div>
+		<h2 style="float:left">Welcome to the Maker Faire Portal</h2>
+		<div class="clearfix"></div>
+		<h3 class="title-head pull-left" style="margin-top:0px;">Manage your Maker Faire Entries</h3>
+		<div class="clearfix"></div>
+		User <?php echo $current_user->user_email;?>
+		<div class="dropdown show">
+			<a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			Apply to Participate
+			</a>
+			<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+			<ul class="bulletless">
+				<li><a class="dropdown-item" href="/make-education-forum/" target="_blank">Make: Education Forum</a></li>
+				<!--<li><a class="dropdown-item" href="/virtually-maker-faire-call-2020-for-makers/" target="_blank">Virtually Maker Faire</a></li>-->
+				<!--<li><a class="dropdown-item" href="/new-york/call-for-makers/" target="_blank">World Maker Faire New York</a></li>-->
+				<!--<li><a class="dropdown-item" href="/bay-area/call-for-makers/" target="_blank">Maker Faire Bay Area</a></li>-->
+			</ul>
+			</div>
+		</div>
+		</div>
+	</div>
+
+	<hr class="header-break">
+	<?php
+	$editEntryLabel = 'Manage Photos';
+	//var_dump($gravityview->entries->all());
+	// There are entries. Loop through them.
+	foreach ( $gravityview->entries->all() as $multientry ) {		
+		$entryData = $multientry->as_entry();				
+		$image =  (isset($entryData['22']) && $entryData['22'] != '' ? $entryData['22']:get_template_directory_uri() .'/images/no-image.png');
+		$entryStatus = (isset($entryData['303']) && $entryData['303'] != '' ? $entryData['303'] : 'Unknown');
+
+		//edit media links (defined in the 'Edit Entry' section of the view)
+		$dispGVeditLink = TRUE;
+		$GVeditLink = do_shortcode('[gv_entry_link action="edit" return="url" view_id="'.$viewID.'" entry_id="'.$entryData['id'].'"]');
+        //$GVeditLink = str_replace('/view/', '/', $GVeditLink);  //remove view slug from URL
+		
+		//TBD - need to set these fields
+		$entryData['mat_message']   = '';		
+		$entryData['ticketing'] = array();
+		$entryData['tasks'] = $maker->get_tasks_by_entry($entryData['id']);		
+
+		$dispEditPub = FALSE;
+		$viewEditLink = '';
+		$dispRMTeditLink = FALSE;
+		$RMTeditLink = '';		
+		
+		$dispCancel = FALSE;
+		$dispDelete = FALSE;
+
+		//Maker Faire portal html
+		?>
+		<div class="maker-admin-list-wrp">
+			<div class="gv-list-view-title-maker-entry">
+				<div class="statusBox <?php echo $statusBlock;?>">
+					<!-- TBD set faire name dynamically -->
+					<div class="statusFaire"><span class="gv-field-label">Maker Faire Bay Area 2023</span> </div>
+					<div class="statusText"><span class="gv-field-label">Status: </span> <?php echo $entryStatus;?></div>
+				</div> <!-- close .statusBox -->
+				<div class="entryImg">
+					<div class="faire-entry-image-wrp">
+					<a href="<?php echo $image;?>">
+						<div class="image-container" style="background-image: url('<?php echo $image;?>');"></div>
+					</a>
+					</div>
+				</div> <!-- close .entryImg-->
+				<div class="entry-main-content">
+					<?php if($entryData['mat_message'] !='') { ?>
+						<!-- MAT messaging -->
+						<div class="hidden-xs mat_message" style="background-color:#f4d03e; padding:10px">
+							<?php echo $entryData['mat_message'];?>
+						</div>
+						<div class="clear"></div>
+					<?php }                                    ?>
+
+					<!-- Project Name -->
+					<div class="entryName entryData">
+						<h3 class="entry-title"><?php echo $entryData['151'];?></h3>
+					</div><!-- close .entryName -->
+
+					<!-- Form Type -->
+					<div class="exhibitID entryData">
+						<span class="entryStandout"><?php echo $entryData['id'];?></span>
+						</div> <!-- close exhibitID -->
+
+						<div>
+						<?php              
+						if($dispEditPub) { ?>
+							<span class="editLink">
+							<a href="<?php echo $viewEditLink;?>">
+								<i class="far fa-eye" aria-hidden="true"></i>
+								View/Edit Public Information
+							</a>
+							</span>
+						<?php
+						}
+						if($dispRMTeditLink) {
+							echo $RMTeditLink;
+						}elseif($dispGVeditLink){
+							?>
+							<span class="editLink">
+							<a href="<?php echo $GVeditLink;?>"><i class="fas fa-edit" aria-hidden="true"></i><?php echo $editEntryLabel;?></a>
+							</span>
+							<?php
+						}
+						
+						?>
+						</div>
+						<div class="clear"></div>
+
+						<div class="actionSection">
+						<div class="submit-date">
+							<span class="gv-field-label">Submitted: </span> <?php echo esc_html( GFCommon::format_date( $entryData['date_created'], false, 'm/d/Y' )); ?>
+						</div>
+						<!-- Action Bar for Entries -->
+						<div class="entry-action-buttons">
+							<!-- Get Your Tickets Section -->
+							<?php
+							//only display if there are tickets and if the entry has been accepted
+							if(!empty($entryData['ticketing']) && $entryStatus == 'Accepted'){ ?>
+							<button type="button" class="btn btn-default btn-no-border manage-button toggle-popover" data-toggle="popover">
+								<span class="hideSmall">GET YOUR </span>ENTRY PASSES
+								<div class="fas fa-ticket fa-lg toggle-popover" data-toggle="popover"></div>
+							</button>
+							<div class="popover-content hidden">
+								<?php
+								foreach($entryData['ticketing'] as $ticket){
+								?>
+								<div class="row mat-ticketing">
+									<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+									<div class="title"><?php echo $ticket['title'];?></div>
+									<div class="subtitle"><?php echo $ticket['subtitle'];?></div>
+									</div>
+									<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+									<a target="_blank" href="<?php echo $ticket['link'];?>">
+										<i class="fas fa-chevron-circle-right fa-2x" aria-hidden="true"></i>
+									</a>
+									</div>
+								</div>
+								<?php
+								}
+								?>
+								<div class="footer">
+								To share a ticket with a friend, just send them the Eventbrite URL
+								</div>
+							</div>
+							<?php
+							}
+							?>
+
+							<!-- Tasks -->
+							<?php
+							if(isset($entryData['tasks']) && !empty($entryData['tasks'])){
+								if(count($entryData['tasks']['toDo']) > 0 || count($entryData['tasks']['done'])>0){
+									$tasks = $entryData['tasks'];
+									?>
+
+									<button type="button" class="btn btn-default btn-no-border notifications-button toggle-popover" data-toggle="popover">TASKS
+									<div class="notification-counter toggle-popover" data-toggle="popover"><?php echo count($tasks['toDo']);?></div>
+									</button>
+									<div class="popover-content hidden">
+									<div class="manage-entry-popover row">
+										<div class="manage-links">
+										<?php
+										foreach($tasks['toDo'] as $task) { ?>
+											<a target="_blank" href="<?php echo $task['action_url'];?>"><?php echo $task['description'];?> <span class="todoTasks" style="color:red"><i class="fas fa-arrow-right" aria-hidden="true"></i>To Do</span></a>
+											<?php
+										}
+										?>
+										</div>
+										<div class="manage-links">
+										<!--<h4 class="tasks"><i class="fa fa-check" aria-hidden="true"></i>Done</h4>-->
+										<?php
+										foreach($tasks['done'] as $task) { ?>
+											<a target="_blank" href="<?php echo $task['action_url'];?>"><?php echo $task['description'];?> <span class="doneTasks" style="color:green"><i class="fas fa-check" aria-hidden="true"></i>Done</span></a>
+											<?php
+										}
+										?>
+										</div>
+									</div>
+									</div>
+									<?php
+								}
+							}
+							?>
+
+							<!-- Manage Entry links -->
+							<button type="button" class="btn btn-default btn-no-border manage-button toggle-popover" data-toggle="popover">MANAGE
+							<div class="toggle-popover fas fa-cog fa-lg" data-toggle="popover"></div>
+							</button>
+							<div class="popover-content hidden">
+								<div class="manage-entry-popover row">
+									<div class="manage-links">
+									<?php
+									//View Link
+									$url = do_shortcode('[gv_entry_link action="read" return="url" view_id="'.$viewID.'" entry_id="'.$entryData['id'].'"]');
+									$url = str_replace('/view/', '/', $url);  //remove view slug from URL
+									?><a href="<?php echo $url;?>">View Entry</a><?php
+
+									if($dispEditPub) { ?>
+										<a href="<?php echo $viewEditLink;?>">
+										View/Edit Public Information
+										</a>
+									<?php
+									}
+
+									if($dispRMTeditLink) {
+										echo $RMTeditLink;
+									}elseif($dispGVeditLink){
+										?>
+										<a href="<?php echo $GVeditLink;?>"><?php echo $editEntryLabel;?></a>
+										<?php
+									}
+									?>
+									</div>
+									<div>
+									<?php
+									if($dispCancel){  ?>
+										<a href="#cancelEntry" data-toggle="modal" data-entry-id="<?php echo $entryData['lead_id'];?>" data-projName="<?php echo $entryData['presentation_title'];?>">Cancel Entry</a>
+									<?php
+									}
+									//Delete Link
+									if($dispDelete){
+										?>
+										<a href="#deleteEntry" data-toggle="modal" data-entry-id="<?php echo $entryData['lead_id'];?>" data-projName="<?php echo $entryData['presentation_title'];?>">Delete Entry</a>
+										<?php
+									}
+									?>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div><!-- /entry-main-content-->
+				<div class="clear"></div>
+			</div> <!-- close .gv-list-view-title-maker-entry -->
+      	</div> <!-- close .maker-admin-list-wrp-->
+	  	<?php
+		//end maker portal html
+	} //end foreach
+}//end check for records
+?>
+</div> <!-- / .maker-admin-manage-faire-entries-mobile -->
+<?php
+/** @action `gravityview/template/list/body/after` */
+$template::body_after( $gravityview );
