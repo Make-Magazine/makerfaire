f5a111d9f032e6d0ab2ea8a89afacf7cfffe8fb7
Alicia Williams <alicia@make.co>
2023-07-25 18:20:14 -0600

    adding testing code

--- wp-content/themes/makerfaire/functions/gravity_forms/gravityforms_supplemental_forms.php
+++ wp-content/themes/makerfaire/functions/gravity_forms/gravityforms_supplemental_forms.php
@@ -17,6 +17,8 @@ function entry_accepted_cb( $entry ) {
       //GFAPI::update_entry( $master_data, $entry['master_entry_id'] );
     }else{        
       //otherwise, create master entry                  
+      error_log('master_data');
+      error_log(print_r($master_data,TRUE));
       $master_entry = GFAPI::submit_form($form['master_form_id'],$master_data);      
       if ( is_wp_error( $master_entry ) ) {
         $error_message = $master_entry->get_error_message();
