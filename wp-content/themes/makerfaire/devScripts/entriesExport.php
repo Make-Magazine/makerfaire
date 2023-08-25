854a23cf751d170bfd36afdf644836a3e8c3373a
Alicia Williams <alicia@make.co>
2023-08-25 10:40:39 -0600

    change GF custom export to export specific fields in a certain order

--- wp-content/themes/makerfaire/devScripts/entriesExport.php
+++ wp-content/themes/makerfaire/devScripts/entriesExport.php
@@ -1,116 +1,98 @@
 <?php
 include 'db_connect.php';
-//check that the request is valid
-/*
-$auth = (isset($_GET['auth'])?$_GET['auth']:'');
-if($auth==''){
-  exit();
-}
-//create a crypt key to pass to entriesExport.php to avoid outside from accessing
-$date  = date('mdY');
-$crypt = crypt($date, AUTH_SALT);
-if($auth != $crypt){
-  exit();
-}*/
-$form=(isset($_GET['formID'])?$_GET['formID']:46);
+$formID=(isset($_GET['formID'])?$_GET['formID']:260 );
 
 // output headers so that the file is downloaded rather than displayed
 header('Content-type: text/csv');
-header('Content-Disposition: attachment; filename="exportForm'.$form.'.csv"');
+header('Content-Disposition: attachment; filename="exportForm'.$formID.'.csv"');
 
 // do not cache the file
 header('Pragma: no-cache');
 header('Expires: 0');
 
-$mysqli->query("SET NAMES 'utf8'");
 //get form data
-$sql = 'select display_meta from wp_gf_form_meta where form_id='.$form;
-$result = $mysqli->query($sql) or trigger_error($mysqli->error."[$sql]");
-while ( $row = $result->fetch_array(MYSQLI_ASSOC) ) {
-  $json = json_decode($row['display_meta']);
-  $jsonArray = (array) $json->fields;
-  foreach($jsonArray as &$array){
-      $array->id = (float) $array->id;
-      $array = (array) $array;
+/* New export for BA23 */
+$field_array = array(303, 151, 302, 16, 27, 32, 45, 803, 806, 805, 60, 345, 344, 61, 66, 438, 65, 62, 347, 348,
+69, 71, 44, 144, 122, 72, 73, 74, 75, 76, 77, 78, 79, 81, 758, 64, 83, 84, 85, 317, 318, 99, 134, 304);
+
+$fieldData = array();
+
+//get form headers
+$form=GFAPI::get_form($formID);
+foreach($form['fields'] as $field){
+  if($field->type=='html' || $field->type=='section' || $field->type=='page'){
+    continue;
   }
-  usort($jsonArray, "cmp");
-  $fieldData = array();
-  $fieldData[]='entry id';
-
-  foreach($jsonArray as $field){
-    $fieldID = (string) $field['id'];
-
-    if($field['type']!='section' && $field['type']!='page'){
-      $label = (isset($field['adminLabel']) && trim($field['adminLabel']) != '' ? $field['adminLabel'] : $field['label']);
-      if($label=='' && $field['type']=='checkbox') $label = $field['choices'][0]->text;
-
-      if($field['type']=='checkbox'||$field['type']=='address' || $field['type']=='name'){
-        if(isset($field['inputs']) && !empty($field['inputs'])){
-          foreach($field['inputs'] as $choice){
-            $choiceID = (string) $choice->id;
-            $fieldData[$choiceID] = $label.' '.$choice->label.'('.$choiceID.')';
-          }
-        }else{
-          $fieldData[$fieldID] = $label.'('.$fieldID.')';
-        }
-      }else{
-        $fieldData[$fieldID] = $label.'('.$fieldID.')';
-      }
-    }
+  
+  if(in_array($field->id, $field_array)){    
+    $label = (isset($field['adminLabel']) && trim($field['adminLabel']) != '' ? $field['adminLabel'] : $field['label']);
+    $fieldData[$field->id] = array('id'=>$field->id,'label' => $label, 'type' => $field->type, 'inputs' => $field->inputs);
   }
-}
-
-//entry data
-$sql = "SELECT entry_id, wp_gf_entry_meta.meta_key, wp_gf_entry_meta.meta_value as value
-        FROM wp_gf_entry
-          left outer join wp_gf_entry_meta
-            on wp_gf_entry_meta.entry_id = wp_gf_entry.id
-        where wp_gf_entry.form_id = $form
-        and wp_gf_entry.status='active'
-        ORDER BY wp_gf_entry_meta.entry_id asc, wp_gf_entry_meta.meta_value asc";
-
-//loop thru entry data
-$entries = $mysqli->query($sql) or trigger_error($mysqli->error."[$sql]");
+}  
+  
 $entryData = array();
+$search_criteria = array();
+$sorting         = array();
+$paging          = array( 'offset' => 0, 'page_size' => 500 );
+$entries = GFAPI::get_entries( $formID, $search_criteria, $sorting, $paging );
 
 foreach($entries as $entry){
-  $fieldNum = (string) $entry['meta_key'];
-  //field 302 and 320 is stored as category number, use cross reference to find text value
-  if($fieldNum=='320' || strpos($fieldNum, '321.')!== false || strpos($fieldNum, '302.')!== false){
-    $value = get_CPT_name($entry['value']);
-  }else{
-    $value = $entry['value'];
+  $entryData[$entry['id']] =  array();
+  foreach($field_array as $fieldID){
+    $field = $fieldData[$fieldID];
+    //comma separate input values for checkbox, address, name    
+    if($field['type']=='checkbox'||$field['type']=='address' || $field['type']=='name'){
+      $inputArray = array();
+      foreach($field['inputs'] as $input){        
+        if(isset($entry[$input['id']]) && $entry[$input['id']]!='') {
+          //field 321 is stored as category number, use cross reference to find text value
+          if($fieldID=='321'){
+            $value = get_CPT_name($entry[$input['id']]);
+          }else{
+            $value = $entry[$input['id']];
+          }
+          $inputArray[] = $value;
+        }
+      }      
+      $entryData[$entry['id']][$fieldID] = implode(",",$inputArray);
+    }else{
+      $value = '';
+      if(isset($entry[$fieldID])){
+        //field 320 and 321 is stored as category number, use cross reference to find text value
+        if($fieldID=='320'){
+          $value = get_CPT_name($entry[$fieldID]);
+        }else{
+          $value = $entry[$fieldID];
+        }
+      }
+      $entryData[$entry['id']][$fieldID] = $value;
+    }
   }
-  $value = htmlspecialchars_decode ($value);
-  $entryData[$entry['entry_id']][$fieldNum]=$value;
 }
 
 // create a file pointer connected to the output stream
 $file = fopen('php://output', 'w');
 
 // send the column headers
-fputcsv($file, $fieldData);
+$headers=array('Entry ID');
+foreach($field_array as $fieldID){   
+  $headers[] = $fieldData[$fieldID]['label'];
+}
+
+/*echo '<br/>header is<br/>';
+var_dump($headers);*/
+fputcsv($file, $headers);
 
 //send the data
-foreach($entryData as $entrykey=>$entryvalue){
+foreach($entryData as $entry_id=>$entryvalue){
   $output=array();
-
-  foreach($fieldData as $fieldkey=>$fieldvalue){
-    if($fieldkey==0){
-      $output[]=$entrykey;
-    }else{
-      if(isset($entryvalue[$fieldkey])){
-        $output[]=$entryvalue[$fieldkey];
-      }else{
-        $output[]='';
-      }
-    }
-  }
+  $output[]=$entry_id;
+  $output = array_merge($output, $entryvalue);    
+  //echo '<br/>data is<br/>';
+  //var_dump($output);
   fputcsv($file, $output);
 }
+//echo '<br/>';
+die();
 
 exit();
-
-function cmp($a, $b) {
-    return $a["id"] - $b["id"];
-}
