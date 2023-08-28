388b7b6e65e5d34ab5ed20be373f4464cfb192ba
Alicia Williams <alicia@make.co>
2023-08-28 13:29:37 -0600

    removing national and barnes and noble from the dropdown. adding link to the main entry review for ba23

--- wp-content/themes/makerfaire/functions/gravity_forms/gravityforms_navigation.php
+++ wp-content/themes/makerfaire/functions/gravity_forms/gravityforms_navigation.php
@@ -74,7 +74,7 @@ function add_menu_item($menu_items) {
     return $menu_items;
 }
 
-add_action('admin_bar_menu', 'toolbar_link_to_mypage', 999);
+add_action('admin_bar_menu', 'toolbar_link_to_mypage', 50);
 
 function toolbar_link_to_mypage($wp_admin_bar) {
 
@@ -85,75 +85,7 @@ function toolbar_link_to_mypage($wp_admin_bar) {
     $menus = wp_get_nav_menus();
     $menu_locations = get_nav_menu_locations();
 
-    if ($is_national) {
-        $location_id = 'mf-admin-national-register-menu';
-        if (isset($menu_locations[$location_id])) {
-            foreach ($menus as $menu) {
-                // If the ID of this menu is the ID associated with the location we're searching for
-                if ($menu->term_id == $menu_locations[$location_id]) {
-                    // This is the correct menu
-                    $menu_items = wp_get_nav_menu_items($menu);
-
-                    $args = array(
-                        'id' => 'mf_admin_parent_national',
-                        'title' => 'National Admin',
-                        'meta' => array('class' => 'my-toolbar-page'),
-                    );
-
-                    $wp_admin_bar->add_node($args);
-                    buildFaireDrop($wp_admin_bar, 'NMF16');
-
-                    //build faire specific admin
-                    foreach ((array) $menu_items as $key => $menu_item) {
-                        if ($menu_item->menu_item_parent == 0) {
-                            // each MF Admin menu has a parent item set that will tell us which faire to add these menu item's too
-                            $faire = $menu_item->attr_title;
-                        } else {
-                            $args = array(
-                                'id' => $menu_item->object_id,
-                                'title' => $menu_item->title,
-                                'href' => $menu_item->url,
-                                'meta' => array('class' => 'my-toolbar-page'),
-                                'parent' => 'mf_admin_parent_national'
-                            );
-
-                            $wp_admin_bar->add_node($args);
-                        }
-                    }
-                }
-            }
-        }
-    } elseif ($is_barnesandnoble) {
-
-        $location_id = 'mf-admin-barnesandnoble-register-menu';
-        if (isset($menu_locations[$location_id])) {
-            foreach ($menus as $menu) {
-                // If the ID of this menu is the ID associated with the location we're searching for
-                if ($menu->term_id == $menu_locations[$location_id]) {
-                    // This is the correct menu
-                    $menu_items = wp_get_nav_menu_items($menu);
-                    $args = array(
-                        'id' => 'mf_admin_parent_barnesandnoble',
-                        'title' => 'Barnes And Noble Admin',
-                        'meta' => array('class' => 'my-toolbar-page'),
-                    );
-                    $wp_admin_bar->add_node($args);
-                    foreach ((array) $menu_items as $key => $menu_item) {
-
-                        $args = array(
-                            'id' => $menu_item->object_id,
-                            'title' => $menu_item->title,
-                            'href' => $menu_item->url,
-                            'meta' => array('class' => 'my-toolbar-page'),
-                            'parent' => 'mf_admin_parent_barnesandnoble'
-                        );
-
-                        $wp_admin_bar->add_node($args);
-                    }
-                }
-            }
-        }
-    } else {
+    
         //create MF admin with information from the faire table
         $args = array(
             'id' => 'mf_admin_parent',
@@ -222,7 +154,7 @@ function toolbar_link_to_mypage($wp_admin_bar) {
                 }
             }
         }
-    }
+    
 
     //add new navigation node
     $args = [
@@ -314,6 +246,16 @@ function buildFaireDrop(&$wp_admin_bar, $faire_id = null) {
                         'meta' => array('class' => 'my-toolbar-page'),
                         'parent' => 'mf_admin_child_' . $formRow->form_id));
                 }            
+            }
+
+            //add BA23 Main Entry View link
+            if ((isset($faire) && $faire=='BA23')) {
+                array_push($args, array(
+                    'id' => 'mf_admin_main_gv_review',
+                    'title' => 'Main Entry View',
+                    'href' => 'https://makerfaire.com/bay-area/main-entry-view-ba23/',
+                    'meta' => array('class' => 'my-toolbar-page'),
+                    'parent' => 'mf_admin_parent_' . $faire));                    
             }
 
             //add BA23 interest form admin review link
