b9f2480fb92b7d3952aaa09752d808767bd8cce2
Rio Roth-Barreiro <rio@make.co>
2023-10-02 17:06:50 -0700

    add lazyload exception for the elementor popup image

--- wp-content/themes/makerfaire/functions/images.php
+++ wp-content/themes/makerfaire/functions/images.php
@@ -7,3 +7,12 @@ function random_pic($dir = '/uploads'){
     	return str_replace(ABSPATH, get_site_url() . '/', $files[$file]);
 	}
 }
+
+
+function skip_photon_lazy_images( $blocked_classes ) {
+    $blocked_classes[] = 'wp-image-643979';
+    return $blocked_classes;
+}
+if ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'photon' ) ) {
+	add_filter( 'jetpack_lazy_images_blocked_classes', 'skip_photon_lazy_images' );
+}
