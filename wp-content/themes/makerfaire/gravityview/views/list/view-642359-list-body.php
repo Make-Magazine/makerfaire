6a96ccb037dbf542e441f4fa832795e5af86765d
Rio Roth-Barreiro <rio@make.co>
2023-10-03 15:47:29 -0700

    resize manage entry icons and unleash the power of flex on the action buttons

--- wp-content/themes/makerfaire/gravityview/views/list/view-642359-list-body.php
+++ wp-content/themes/makerfaire/gravityview/views/list/view-642359-list-body.php
@@ -259,8 +259,8 @@ if ( ! $gravityview->entries->count() ) {
 							//only display if there are tickets and if the entry has been accepted
 							if(!empty($entryData['ticketing']) && $entryStatus == 'Accepted'){ ?>
 							<button type="button" class="btn btn-default btn-no-border manage-button toggle-popover" data-toggle="popover">
-								<span class="hideSmall">GET YOUR </span>ENTRY PASSES
-									<div class="fa fa-ticket fa-lg toggle-popover" data-toggle="popover"></div>
+								<span class="hideSmall">GET YOUR</span>&nbsp;ENTRY PASSES
+									<div class="fa fa-ticket toggle-popover" data-toggle="popover"></div>
 							</button>
 							<div class="popover-content hidden">
 								<?php
@@ -275,7 +275,7 @@ if ( ! $gravityview->entries->count() ) {
 									</div>
 									<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 										<a target="_blank" href="<?php echo $ticket['link'];?>">
-											<i class="fa fa-chevron-circle-right fa-2x" aria-hidden="true"></i>
+											<i class="fa fa-chevron-circle-right" aria-hidden="true"></i>
 										</a>
 									</div>
 								</div>
@@ -328,7 +328,7 @@ if ( ! $gravityview->entries->count() ) {
 
 							<!-- Manage Entry links -->
 							<button type="button" class="btn btn-default btn-no-border manage-button toggle-popover" data-toggle="popover">MANAGE
-									<div class="toggle-popover fa fa-cog fa-lg" data-toggle="popover"></div>
+									<div class="toggle-popover fa fa-cog" data-toggle="popover"></div>
 							</button>
 							<div class="popover-content hidden">
 								<div class="manage-entry-popover row">
