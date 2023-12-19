4e0e479e1f855f0e66c436f5c069205c6e823ae2
Alicia Williams <alicia@make.co>
2023-12-19 10:04:32 -0700

    display password if one is set

--- wp-content/themes/makerfaire/devScripts/listPages.php
+++ wp-content/themes/makerfaire/devScripts/listPages.php
@@ -1,29 +1,10 @@
 <?php
 include '../../../../wp-load.php'; 
 
-$parsed_args = array(
-    'depth'        => 0,
-    'show_date'    => 'modified',
-    'date_format'  => get_option( 'date_format' ),
-    'child_of'     => 0,
-    'exclude'      => '',
-    'title_li'     => __( 'Pages' ),
-    'echo'         => 1,
-    'authors'      => '',
-    'sort_column'  => 'menu_order, post_title',
-    'link_before'  => '',
-    'link_after'   => '',
-    'item_spacing' => 'preserve',
-    'walker'       => ''
-);
-
-if(isset($_GET['post_status'])){
-    $parsed_args['post_status'] = $_GET['post_status'];
-}
-
 list_pages_with_status_and_hierarchy();
 
 function list_pages_with_status_and_hierarchy($parent_id = 0, $level = 0) {
+    $post_status = (isset($_GET['post_status'])?$_GET['post_status']:'');
     $expand=(isset($_GET['expand'])?TRUE:FALSE);
 
     // Get all pages with the specified parent ID
@@ -34,6 +15,9 @@ function list_pages_with_status_and_hierarchy($parent_id = 0, $level = 0) {
         'order'          => 'ASC',
         'orderby'        => 'menu_order'
     );
+    if(isset($_GET['post_status'])){
+        $args['post_status'] = $_GET['post_status'];
+    }
 
     $pages = get_posts($args);
 
@@ -48,7 +32,7 @@ function list_pages_with_status_and_hierarchy($parent_id = 0, $level = 0) {
             $modified_date = get_post_field( 'post_modified', $page->ID );
             
             echo '<li>';
-            echo '<strong><a href="'.$link.'">' . esc_html($page->post_title) . '</a></strong> (' . esc_html($status).') '.$modified_date;
+            echo '<strong><a href="'.$link.'">' . esc_html($page->post_title) . '</a></strong> (' . esc_html($status).') '.(isset($page->post_password) && $page->post_password!=''?' Password='.$page->post_password:'').' '.$modified_date;
             if($expand){
                 echo ' Created-> '. get_post_field( 'post_date', $page->ID );                
                 echo ' Author-> '. get_the_author_meta('display_name', $page->post_author);
