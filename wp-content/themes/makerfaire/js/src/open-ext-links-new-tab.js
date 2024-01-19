ba8e30af0e795d2c5c76aabd55c13bf928b71a27
Rio Roth-Barreiro <rio@make.co>
2024-01-19 11:44:51 -0800

    remove the open links in new tabs script entirely

--- wp-content/themes/makerfaire/js/src/open-ext-links-new-tab.js
+++
@@ -1,9 +0,0 @@
-// Open external links in new tab, unless it's a make site, or targeted self, or an email link
-jQuery(document).ready(function($) {
-	$(document.links).filter(function() {
-		if($(this).attr("target") != "_self" && $(this).not('[href*="mailto:"]') && $(this).not('[href*="javascript:void(0);"]') && $(this).attr("href") != "javascript:void(0);" ){
-			alert("here we go");
-			return (this.hostname.indexOf("make") == -1 || this.hostname.indexOf("mfaire") == -1);
-		}
-	}).attr('target', '_blank');
-});
