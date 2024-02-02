e7313de0aa4ae8278e768eaf2714fe2b688c079a
Alicia Williams <alicia@make.co>
2024-02-02 15:38:27 -0700

    removing maker checkin and onsite pinning functionality removed functionality that referred to the page-maker-directory template as we deleted that deleting some more old templates

--- wp-content/themes/makerfaire/functions/rewrite-rules.php
+++ wp-content/themes/makerfaire/functions/rewrite-rules.php
@@ -5,13 +5,7 @@ function maker_url_vars($rules) {
 
   //classic entry page for flagship faires
   $newrules['maker/entry/(\d*)/?(.*)$/?'] = 'index.php?post_type=page&pagename=entry-page-do-not-delete&e_id=$matches[1]&edit_slug=$matches[2]';
-  
-  //old meet the makers page - page are marked private and don't work
-  //$newrules['([^\/]*)/meet-the-makers/topics/([^\/]*)/([0-9]{1,})/?$'] = 'index.php?post_type=page&pagename=listing-page-do-not-delete&f=$matches[1]&t_slug=$matches[2]&offset=$matches[3]';
-  //$newrules['([^\/]*)/meet-the-makers/topics/([^\/]*)/?$'] = 'index.php?offset=1&post_type=page&pagename=listing-page-do-not-delete&f=$matches[1]&t_slug=$matches[2]';
-  //$newrules['([^\/]*)/meet-the-makers/search/([0-9]{1,})/?$'] = 'index.php?post_type=page&pagename=search-results-page-do-not-delete&f=$matches[1]&offset=$matches[2]';  
-  //$newrules['([^\/]*)/meet-the-makers/search/?$'] = 'index.php?offset=1&post_type=page&pagename=search-results-page-do-not-delete&f=$matches[1]';
-  
+   
   //classic schedule page
   $newrules['([^\/]*)/schedule/([^/]+)/?$'] = 'index.php?pagename=$matches[1]/schedule&sched_dow=$matches[2]';
   $newrules['([^\/]*)/schedule/([^/]+)/([^/]+)/?$'] = 'index.php?pagename=$matches[1]/schedule&sched_dow=$matches[2]&sched_type=$matches[3]';
@@ -59,20 +53,6 @@ function mf_filter_post_type_link( $link, $post ) {
 add_filter( 'post_type_link', 'mf_filter_post_type_link', 10, 2 );
 
 /* Rewrite Rules */
-//add_action('init', 'makerfaire_rewrite_rules');
-function makerfaire_rewrite_rules() {
-  //old makerfaire check in process
-  //add_rewrite_rule( 'onsitecheckin/?([^/]*)', 'index.php?onsitecheckin=true&token=$matches[1]', 'top' );
-  //add_rewrite_rule( 'processonsitecheckin/?([^/]*)', 'index.php?processonsitecheckin=true&token=$matches[1]', 'top' );
-  //add_rewrite_rule( 'onsitepinning/?([^/]*)', 'index.php?onsitepinning=true&token=$matches[1]', 'top' );
-  //add_rewrite_rule( 'processonsitepinning/?([^/]*)', 'index.php?processpinning=true&token=$matches[1]', 'top' );
-  
-  //page does not exist
-  //add_rewrite_rule('^mf/([^/]*)/([^/]*)/?', 'index.php?pagename=maker-faire-gravity-forms-display-page&makerfaire=$matches[1]&entryid=$matches[2]', 'top');
-  //old faire pages - linked only from ribbons.php - page is marked as private
-  //add_rewrite_rule('^mfarchives/([^/]*)/?', 'index.php?pagename=entry-archives&entryslug=$matches[1]', 'top');    
-}
-
 /*
   add_action( 'wp_loaded','my_flush_rules' );
 
@@ -91,24 +71,12 @@ function makerfaire_rewrite_rules() {
 
 /* Query Vars */
 add_filter( 'query_vars', 'makerfaire_register_query_var' );
-function makerfaire_register_query_var( $vars ) {
-    //$vars[] = 'processonsitecheckin'; //classes/makerfaire-helper.php
-    //$vars[] = 'onsitecheckin';        //classes/makerfaire-helper.php
-    //$vars[] = 'processonsitepinning'; //classes/makerfaire-helper.php
-    //$vars[] = 'onsitepinning';        //classes/makerfaire-helper.php
-    //$vars[] = 'makerid';
-    //$vars[] = "sched_dow";    
-    //$vars[] = 'api';
-    //$vars[] = 's_keyword'; //page-makers-bykeyword.php    
-    //$vars[] = 't_slug';     //page-makers-bytopic.php
-    //$vars[] = 'offset';     //page-makers-bykeyword.php, page-makers-bytopic.php
-    //$vars[] = 'f';          //page-makers-bykeyword.php, page-makers-bytopic.php
-
+function makerfaire_register_query_var( $vars ) {   
     $vars[] = 'type';       //page-api.php, page-mfapi.php
     $vars[] = 'e_id';       //page-entry.php
     $vars[] = 'edit_slug';  //page-entry.php    
     $vars[] = 'faire_id';   //page-mfscheduler.php
-    $vars[] = 'token';      //page-maker-checkin.php, page-mfscheduler.php, page-onsite-checkin.php, page-onsite-pinning.php
+    $vars[] = 'token';      //page-mfscheduler.php
     $vars[] = 'makersign'; //classes/makerfaire-helper.php
     $vars[] = 'faire';  //fpdi/makersigns.php
     $vars[] = 'eid';    //fpdi/makersigns.php
--- wp-content/themes/makerfaire/single-maker-entry-archive.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Template Name: maker-entry-archive custom post type
- *
- */
-include('page-entryarchives.php');
