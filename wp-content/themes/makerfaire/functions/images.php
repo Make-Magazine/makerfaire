d03c37f0f71345462b0655d0c38944d983c65542
Rio Roth-Barreiro <rio@make.co>
2024-02-06 10:04:42 -0800

    second round of stylesheet clean up (deleted icomoon, thankpage.less, newsletter.less, newsletter-landing.less, make-store.less, footer.less, button-overrides.less and magical.js)

--- wp-content/themes/makerfaire/functions/images.php
+++ wp-content/themes/makerfaire/functions/images.php
@@ -16,3 +16,13 @@ function skip_photon_lazy_images( $blocked_classes ) {
 if ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'photon' ) ) {
 	add_filter( 'jetpack_lazy_images_blocked_classes', 'skip_photon_lazy_images' );
 }
+
+/* preload the featured image on all pages for better page load speeds
+<<BETTER IDEA, ADD AN ACF FOR PRELOAD IMAGE AND PRELOAD ONLY IF THAT ACF IS SET>>
+function wpp_preloadimages() {
+	$post_id = get_queried_object_id();
+	if((is_single($post_id) || is_page($post_id)) && has_post_thumbnail($post_id)) {
+		echo '<link rel="preload" as="image" href="' . get_the_post_thumbnail_url($post_id) . '" />';
+	}
+}
+add_action( 'wp_head', 'wpp_preloadimages' );*/
