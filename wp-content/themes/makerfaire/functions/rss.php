b0d1a8e8e2c337e467cb0c022b33c3c98437e47f
Alicia Williams <alicia@make.co>
2024-02-21 15:45:19 -0700

    add featured images to rss feed

---
+++ wp-content/themes/makerfaire/functions/rss.php
@@ -0,0 +1,11 @@
+<?php
+//add the featured image to the RSS feed
+function featuredtoRSS($content) {
+    global $post;
+    if (has_post_thumbnail($post->ID)) {
+        $content = '<div>' . get_the_post_thumbnail($post->ID, 'medium', array('style' => 'margin-bottom: 15px;')) . '</div>' . $content;
+    }
+    return $content;
+}
+add_filter('the_excerpt_rss', 'featuredtoRSS', 20, 1);
+add_filter('the_content_feed', 'featuredtoRSS', 20, 1);
