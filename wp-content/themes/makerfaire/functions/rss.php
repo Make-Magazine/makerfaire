9a332f941578b03780fc53bb1831deb3d51b0fdd
Alicia Williams <alicia@make.co>
2024-02-21 16:36:57 -0700

    adding image and exhibit description to the projects rss feed

--- wp-content/themes/makerfaire/functions/rss.php
+++ wp-content/themes/makerfaire/functions/rss.php
@@ -2,9 +2,16 @@
 //add the featured image to the RSS feed
 function featuredtoRSS($content) {
     global $post;
-    if (has_post_thumbnail($post->ID)) {
-        $content = '<div>' . get_the_post_thumbnail($post->ID, 'medium', array('style' => 'margin-bottom: 15px;')) . '</div>' . $content;
-    }
+    $post_type = get_post_type($post->ID);
+    
+    if ($post_type == "projects") {
+        if (has_post_thumbnail($post->ID)) {
+            $content  = '<div>' . get_the_post_thumbnail($post->ID, 'medium', array('style' => 'margin-bottom: 15px;')) . '</div>' . $content;
+            //return the first 50 characeters of the exhibit description
+            $content .= substr(html_entity_decode(get_field("exhibit_description", $post->ID), ENT_QUOTES, get_bloginfo("")),0,50);
+        }
+    }    
+    
     return $content;
 }
 add_filter('the_excerpt_rss', 'featuredtoRSS', 20, 1);
