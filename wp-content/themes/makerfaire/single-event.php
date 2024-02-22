49f3508b3ff08c36f0286a3551d4a1a3814815b3
Alicia Williams <alicia@make.co>
2024-02-22 12:46:20 -0700

    Revert "let wordpress determine the srcset"
    This reverts commit c15d1f9715b1a382644b5e445b211f6ed3f8b410.

--- wp-content/themes/makerfaire/single-event.php
+++ wp-content/themes/makerfaire/single-event.php
@@ -1,10 +1,6 @@
 <?php 
 /*  Page layout for the Yearbook Individual Faire Pages */
 get_header(); 
-global $_wp_additional_image_sizes; 
-print '<pre>'; 
-print_r( $_wp_additional_image_sizes ); 
-print '</pre>';
 ?>
 
 <main id="content">
