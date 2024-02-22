189aa031fecaf3c87901991c1f9659569410a6f5
Alicia Williams <alicia@make.co>
2024-02-22 11:52:09 -0700

    only display the faire project image if one is set

--- wp-content/themes/makerfaire/single-event.php
+++ wp-content/themes/makerfaire/single-event.php
@@ -138,20 +138,21 @@ get_header();
 			<div class="blue-spacer"></div>
 			<div class="projects-wrapper">
 				<?php foreach($projects as $project){ 
-					$thumbnail_id  = get_post_thumbnail_id($project->ID);
-					//check if there is a featured image set
-					if($thumbnail_id) {
-						$image_src = wp_get_attachment_image_src( $thumbnail_id, 'full' );
-						$image_alt = get_post_meta ( $thumbnail_id, '_wp_attachment_image_alt', true );
-						$image_alt = !empty($image_alt) ? $image_alt : get_the_title($project->ID) . " Project Image for Maker Faire " . $faire_name . " " . $faire_year;;	
-					}
 					?>
 					<div class="faire-project">
 					  <a href="<?php echo get_permalink($project->ID); ?>">
 					    <div class="project-image">
-							<?php if($image_src){ ?>
-								<img src="<?php echo $image_src[0] ?>" alt="<?php echo $image_alt; ?>" />
-							<?php } ?>
+							<?php 
+							$thumbnail_id  = get_post_thumbnail_id($project->ID);
+							//check if there is a featured image set
+							if($thumbnail_id) {
+								$image_src = wp_get_attachment_image_src( $thumbnail_id, 'full' );
+								$image_alt = get_post_meta ( $thumbnail_id, '_wp_attachment_image_alt', true );
+								$image_alt = !empty($image_alt) ? $image_alt : get_the_title($project->ID) . " Project Image for Maker Faire " . $faire_name . " " . $faire_year;;	
+								?><img src="<?php echo $image_src[0] ?>" alt="<?php echo $image_alt; ?>" /><?php
+							}
+							?>	
+							
 						</div>
 						<h4><?php echo $project->post_title; ?></h4>
 						<p><?php echo get_field('exhibit_description', $project->ID); ?>
