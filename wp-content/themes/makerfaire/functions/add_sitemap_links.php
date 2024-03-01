adef2dce841e8478c4b6d5aa2f9a22ad6d1b3f70
Alicia Williams <alicia@make.co>
2024-03-01 11:19:32 -0700

    only add the form sitemaps to the index file if they have accepted entries

--- wp-content/themes/makerfaire/functions/add_sitemap_links.php
+++ wp-content/themes/makerfaire/functions/add_sitemap_links.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *  Creates a new custom yoast seo sitemap based on maker entry
  */
@@ -7,32 +8,41 @@
 
 //define valid form types
 $form_types = array('Exhibit', 'Presentation', 'Performance', 'Startup Sponsor', 'Sponsor', 'Workshop', 'Master');
-
-//for testing ONLY
-//add_filter("wpseo_enable_xml_sitemap_transient_caching", "__return_false");
+//entry search criteria
+$search_criteria = array();
+$search_criteria['status'] = 'active';
+$search_criteria['field_filters'][] = array('key' => '303', 'value' => 'Accepted');
 
 /**
  * When the sitemap_index.xml page is accessed, add links to form sitemaps
  */
 function faire_entries_sitemap_index($sitemap_index) {
    global $form_types;
+   global $search_criteria;
 
    //generate a sitemap for each exhibit form
-   $forms = GFAPI::get_forms(true, false);
-   
+   $forms = GFAPI::get_forms(NULL, false);
+
    foreach ($forms as $form) {
-   	if (isset($form['form_type']) && in_array($form['form_type'],$form_types)) {                  
-         $formId = $form['id'];     
-         $sitemap_url = home_url("form-$formId-entries-sitemap.xml");
-         $sitemap_date = date(DATE_W3C);  # Current date and time in sitemap format.
-         
-         $faire_entries = <<<SITEMAP_INDEX_ENTRY
-         <sitemap>
-            <loc>%s</loc>
-            <lastmod>%s</lastmod>
-         </sitemap>
-         SITEMAP_INDEX_ENTRY;
+      if (isset($form['form_type']) && in_array($form['form_type'], $form_types)) {
+         //maybe check for accepted entries here                 
+         $formId = $form['id'];
+
+         //see if there are any entries that match search criteria
+         $entries = GFAPI::get_entries((int)$formId, $search_criteria, null, array('offset' => 0, 'page_size' => 1));
+         //if there are entries, go ahead and add the sitemap 
+         if (!empty($entries)) {
+            $sitemap_url = home_url("form-$formId-entries-sitemap.xml");
+            $sitemap_date = date(DATE_W3C);  # Current date and time in sitemap format.
+
+            $faire_entries = <<<SITEMAP_INDEX_ENTRY
+<sitemap>
+   <loc>%s</loc>
+   <lastmod>%s</lastmod>
+</sitemap>
+SITEMAP_INDEX_ENTRY;
             $sitemap_index .= sprintf($faire_entries, $sitemap_url, $sitemap_date);
+         }
       }
    }
    return $sitemap_index;
@@ -47,17 +57,18 @@ add_action('init', 'register_entries_sitemap', 99);
  * On init, run the function that will register sitemaps for all gravity forms that match the defined form types
  */
 function register_entries_sitemap() {
-   global $wpseo_sitemaps; global $form_types;
+   global $wpseo_sitemaps;
+   global $form_types;
    if ($wpseo_sitemaps && is_array($form_types)) {
       //generate a sitemap for each exhibit form
       $forms = GFAPI::get_forms(true, false);
-      
+
       foreach ($forms as $form) {
-         if (isset($form['form_type']) && in_array($form['form_type'],$form_types)) {
-            $formId = $form['fields'][0]['formId'];
+         if (isset($form['form_type']) && in_array($form['form_type'], $form_types)) {
+            $formId = $form['id'];
             $wpseo_sitemaps->register_sitemap("form-$formId-entries", 'faire_entries_sitemap_generate');
          }
-      }      
+      }
    }
 }
 
@@ -66,9 +77,9 @@ function register_entries_sitemap() {
  * This is triggered when the specific form sitemap is accessed
  */
 function faire_entries_sitemap_generate() {
-   global $wpseo_sitemaps;   
+   global $wpseo_sitemaps;
    global $search_criteria;
-   
+
    //determine what form this sitemap this is for
    global $wp;
    $current_slug = add_query_arg(array(), $wp->request);
@@ -76,10 +87,6 @@ function faire_entries_sitemap_generate() {
    //get form id from current sitemap name ie) form-43-entries-sitemap.xml
    $form_id = str_replace('-entries-sitemap.xml', '', $current_slug);
    $form_id = str_replace('form-', '', $current_slug);
-
-   //entry search criteria
-   $search_criteria['status'] = 'active';
-   $search_criteria['field_filters'][] = array('key' => '303', 'value' => 'Accepted');
 
    //retrieve list of entries   
    $entries = GFAPI::get_entries((int)$form_id, $search_criteria, null, array('offset' => 0, 'page_size' => 999));
