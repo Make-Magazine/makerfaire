9c39d8852f5cc59b6553f31a86bc1c5f567333b8
Alicia Williams <alicia@make.co>
2024-03-22 12:08:33 -0600

    adding back in populate anything

---
+++ wp-content/plugins/gp-populate-anything/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '2.0.35',
+        'version' => '2.0.35.0',
+        'reference' => '8334bbff6d26397e00766bfbf5329cfbf2a3f621',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '2.0.35',
+            'version' => '2.0.35.0',
+            'reference' => '8334bbff6d26397e00766bfbf5329cfbf2a3f621',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
