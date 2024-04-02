16dd23394c60332bc4b3eab9d74e31756c664708
Alicia Williams <alicia@make.co>
2024-04-02 16:17:21 -0600

    allow direct access to gravity view views

--- wp-content/themes/makerfaire/single.php
+++ wp-content/themes/makerfaire/single.php
@@ -1,3 +1,27 @@
 <?php
-header( 'Location: ' . get_bloginfo('url') );
-?>
+if(get_post_type() == "gravityview") {
+    get_header();
+    /* Start the Loop */
+    while ( have_posts() ) :
+        the_post();
+        ?>
+        <article id="gravity-view-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+            <header class="entry-header alignwide">
+                <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+            </header><!-- .entry-header -->
+
+            <div class="entry-content">
+                <?php the_content(); ?>
+            </div><!-- .entry-content -->
+
+        </article><!-- #post-<?php the_ID(); 
+
+    endwhile; // End of the loop.
+
+    get_footer();
+} else {
+    header( 'Location: ' . get_bloginfo('url') );
+}
+
