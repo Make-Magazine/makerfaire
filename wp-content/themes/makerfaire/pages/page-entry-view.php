4408c6135016c6646ac7b814d325f2bda97ead16
Rio Roth-Barreiro <rio@make.co>
2024-07-01 11:41:20 -0700

    cache bust

--- wp-content/themes/makerfaire/pages/page-entry-view.php
+++ wp-content/themes/makerfaire/pages/page-entry-view.php
@@ -125,7 +125,7 @@
             <h4>More Event Info</h4>
             <div class="entry-box-items">
                 <?php if(isset($mainCategory ) && $mainCategory  != '') { ?>
-                    <span class="entry-box-item"><i class="fa fa-rocket"></i><a href="/<?php echo $url_sub_path; ?>/meet-the-makers/?category=<?php echo $mainCategory; ?>"><?php echo $mainCategory; ?></a></span>
+                    <span class="entry-box-item"><i class="fa fa-rocket"></i><a href="/<?php echo $url_sub_path; ?>/meet-the-makers/?category=<?php echo $mainCategory; ?>">See All <?php echo $mainCategory; ?></a></span>
                 <?php } ?>
                 <span class="entry-box-item"><i class="fa fa-calendar"></i><a href="/<?php echo $url_sub_path; ?>/schedule/">Event Schedule</a></span>
                 <span class="entry-box-item"><i class="fa fa-tools"></i><a href="/<?php echo $url_sub_path; ?>/meet-the-makers/">See All Makers</a></span>
