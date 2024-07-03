b9e4b3402e28cf5835c4c15f36265f8de736a168
Rio Roth-Barreiro <rio@make.co>
2024-07-03 16:24:24 -0700

    change markercluster cdn url to fix the error on the globalmap page

--- wp-content/themes/makerfaire/functions.php
+++ wp-content/themes/makerfaire/functions.php
@@ -204,7 +204,7 @@ function mf_map() {
     // Map page only
     if (is_page_template('page-makerfaire-map.php')) {
         wp_enqueue_script('google-map', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDtWsCdftU2vI9bkZcwLxGQwlYmNRnT2VM', false, false, true);
-        wp_enqueue_script('google-markers', 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js', array('google-map'), false, true);
+        wp_enqueue_script('google-markers', 'https://cdnjs.cloudflare.com/ajax/libs/js-marker-clusterer/1.0.0/markerclusterer_compiled.js', array('google-map'), false, true);
         wp_enqueue_script('vue', get_stylesheet_directory_uri() . '/js/mf-map/vue.min.js', false, $my_version, true);
         wp_enqueue_script('axios', get_stylesheet_directory_uri() . '/js/mf-map/axios.min.js', array('vue'), $my_version, true);
         wp_enqueue_script('vue-table-2', get_stylesheet_directory_uri() . '/js/mf-map/vue-tables-2.min.js', array('vue'), $my_version, true);
