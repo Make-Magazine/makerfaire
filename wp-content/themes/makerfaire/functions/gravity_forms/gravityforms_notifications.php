0e9bfc096ab4d251863ad7e0cf374538ac9974d1
Alicia Williams <alicia@make.co>
2024-07-29 17:58:43 -0600

    moving the definition of the mf_acceptance_status_changed notification to the same place as our other notifications removed the maker_updated_exhibit as there isn't any logic for it to trigger this notification

--- wp-content/themes/makerfaire/functions/gravity_forms/gravityforms_notifications.php
+++ wp-content/themes/makerfaire/functions/gravity_forms/gravityforms_notifications.php
@@ -6,9 +6,9 @@ add_filter( 'gform_notification_events', 'add_event' );
 function add_event( $notification_events ) {
     $notification_events['confirmation_letter']   = __( 'Confirmation Letter', 'gravityforms' );
     $notification_events['maker_cancel_exhibit']  = __( 'Maker Cancelled Exhibit', 'gravityforms' );
-    $notification_events['maker_delete_exhibit']  = __( 'Maker Deleted Exhibit', 'gravityforms' );
-    $notification_events['maker_updated_exhibit'] = __( 'Maker Updated Entry', 'gravityforms' );
+    $notification_events['maker_delete_exhibit']  = __( 'Maker Deleted Exhibit', 'gravityforms' );   
     $notification_events['manual']                = __( 'Send Manually', 'gravityforms' );
+    $notification_events['mf_acceptance_status_changed'] = __( 'Acceptance Status Changed', 'gravityforms' );
     return $notification_events;
 }
 
