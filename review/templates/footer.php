01222f23bf28d1f6258a24cbc5316e6a9e075053
Rio Roth-Barreiro <rio@make.co>
2024-08-13 17:29:10 -0700

    this style is being sticky

--- review/templates/footer.php
+++ review/templates/footer.php
@@ -6,7 +6,7 @@
     <footer>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         <!-- Required Stylesheets -->
-        <link type="text/css" rel="stylesheet" href="/review/css/style.min.css" />
+        <link type="text/css" rel="stylesheet" href="/review/css/style.min.css?v2" />
         <!-- Load polyfills to support older browsers -->
         <script src="https://polyfill.io/v3/polyfill.min.js?features=es2015%2CIntersectionObserver"></script>
         <link rel="stylesheet" href="https://c0.wp.com/c/6.4.3/wp-includes/css/admin-bar.min.css" />
