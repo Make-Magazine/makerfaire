ee3a286960f85492e86e31d998333b9159d74e66
Alicia Williams <alicia@make.co>
2024-08-19 14:38:18 -0600

    call rmt rules first then write to expofp

--- wp-content/themes/makerfaire/devScripts/expoFP_create_exhibitor.php
+++ wp-content/themes/makerfaire/devScripts/expoFP_create_exhibitor.php
@@ -25,6 +25,7 @@ $expoFP_count = 0;
 
 //loop through entries
 foreach ($entries as $entry) {   
+    GFRMTHELPER::buildRmtData($entry, $form);
     create_expofp_exhibitor( $entry, $form );
     $expoFP_count++;    
 }
