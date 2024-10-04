0d66b4b23f0eb0cc5b378e19cbfa7cbcaf51ce1c
Rio Roth-Barreiro <rio@make.co>
2024-10-04 12:46:06 -0700

    revise to allow for multiple occurrences of conf button merge tag

--- wp-content/themes/makerfaire/functions/gravity_forms/merge-tags.php
+++ wp-content/themes/makerfaire/functions/gravity_forms/merge-tags.php
@@ -336,13 +336,17 @@ function mf_replace_merge_tags($text, $form, $entry, $url_encode, $esc_html, $nl
     //Confirmation Button 
     if (strpos($text, '{CONF_BUTTON') !== false) {
         $suppToken  = (isset($entry['fg_easypassthrough_token']) ? $entry['fg_easypassthrough_token'] : '');
-        $loadInTime = (preg_match('/load_in_time=\"(.*?)\"/', $text, $match) == 1) ? "&load_in_time=" . $match[1] : "";
-        $btnTitle   = (preg_match('/title=\"(.*?)\"/', $text, $match) == 1) ? $match[1] : "Yes, I'll be there!";
-        $confButton = '<a href="https://'.$_SERVER['HTTP_HOST'].'/query/?type=entry&token=' . $suppToken . $loadInTime . '">' .
+        
+        // need to match multiple occurences of this button if necessary
+        preg_match_all('/{CONF_BUTTON(.*?)}/', $text, $confBtnArray); 
+        foreach($confBtnArray[0] as $confBtn) {
+            $loadInTime = (preg_match('/load_in_time=\"(.*?)\"/', $confBtn, $match) == 1) ? "&load_in_time=" . $match[1] : "";
+            $btnTitle   = (preg_match('/title=\"(.*?)\"/', $confBtn, $match) == 1) ? $match[1] : "Yes, I'll be there!";
+            $confBtnTxt = '<a href="https://'.$_SERVER['HTTP_HOST'].'/query/?type=entry&token=' . $suppToken . $loadInTime . '">' .
             ' <button style="border-radius:2px;border: solid 1px #eb002a;background:#eb002a;color:#fff;padding:0px 15px;height:30px;font-weight:500;cursor:pointer;">' . $btnTitle . '</button>' .
             '</a>';
-        preg_match('/{CONF_BUTTON(.*?)}/', $text, $oldMergeTag); // set the oldMergeTag value to replace with our new confButton
-        $text = str_replace($oldMergeTag, $confButton, $text);
+            $text = str_replace($confBtn, $confBtnTxt, $text);
+        }
     }
     //resource lock indicator
     if (strpos($text, '{rmt_res_cat_lock') !== false) {
