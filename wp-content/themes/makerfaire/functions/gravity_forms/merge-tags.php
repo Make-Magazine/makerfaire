ea9a38c7b6fe159c80ff7bf000b8fe1a86e158f1
Rio Roth-Barreiro <rio@make.co>
2024-10-04 14:07:01 -0700

    get rid of button tag, as gmail will not respect css cursor styles on buttons

--- wp-content/themes/makerfaire/functions/gravity_forms/merge-tags.php
+++ wp-content/themes/makerfaire/functions/gravity_forms/merge-tags.php
@@ -342,9 +342,10 @@ function mf_replace_merge_tags($text, $form, $entry, $url_encode, $esc_html, $nl
         foreach($confBtnArray[0] as $confBtn) {
             $loadInTime = (preg_match('/load_in_time=\"(.*?)\"/', $confBtn, $match) == 1) ? "&load_in_time=" . $match[1] : "";
             $btnTitle   = (preg_match('/title=\"(.*?)\"/', $confBtn, $match) == 1) ? $match[1] : "Yes, I'll be there!";
-            $confBtnTxt = '<a href="https://'.$_SERVER['HTTP_HOST'].'/query/?type=entry&token=' . $suppToken . $loadInTime . '">' .
-            ' <button style="border-radius:2px;border: solid 1px #eb002a;background:#eb002a;color:#fff;padding:0px 15px;height:30px;font-weight:500;cursor:pointer;">' . $btnTitle . '</button>' .
-            '</a>';
+            $confBtnTxt = '<a href="https://'.$_SERVER['HTTP_HOST'].'/query/?type=entry&token=' . $suppToken . $loadInTime . '" 
+                              style="border-radius:2px;border:solid 1px #eb002a;background:#eb002a;color:#fff;padding:5px 15px;height:30px;font-weight:500;text-decoration:none;">'
+                            . $btnTitle . 
+                          '</a>';
             $text = str_replace($confBtn, $confBtnTxt, $text);
         }
     }
