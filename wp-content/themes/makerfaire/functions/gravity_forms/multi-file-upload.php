6d9b17b67d63ead5bddaf81119407fbf72ac3da9
Alicia Williams <alicia@make.co>
2024-10-30 11:40:38 -0600

    new function for displaying images and videos with markup on gravity view page specific to form 296

---
+++ wp-content/themes/makerfaire/functions/gravity_forms/multi-file-upload.php
@@ -0,0 +1,30 @@
+<?php
+gw_multi_file_merge_tag()->register_settings( array(
+	'form_id' => 296,
+	'markup'  => array(
+		array(
+			'file_types' => array( 'jpg', 'jpeg', 'png', 'gif' ),
+			'markup'     => '<div class="gw-image grid-image">
+                                <a href="{url}" class="gw-image-link"><img src="{url}" width="100%" /></a>
+                            </div>'
+		),
+		array(
+			'file_types' => array( 'mp4', 'ogg', 'webm' ),
+			'markup'     => '<div class="grid-video">
+                                <video width="320" height="240" controls>
+			                        <source src="{url}" type="video/{ext}">
+			                        Your browser does not support the video tag.
+			                    </video>
+                            </div>'
+        ),
+		array(
+			'file_types' => array( 'MOV' ),
+			'markup'     => '<div class="grid-video">
+                                <video width="320" height="240" controls>
+                                    <source src="{url}" >
+                                    Your browser does not support the video tag.
+                                </video>
+                            </div>'
+        ),        
+	)
+) );
