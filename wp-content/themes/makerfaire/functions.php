33dc2650e1f63ff9c6018b0f04f8d4624e59c058
Alicia Williams <alicia@make.co>
2025-01-24 09:48:16 -0700

    adding in function to get primary tax id

--- wp-content/themes/makerfaire/functions.php
+++ wp-content/themes/makerfaire/functions.php
@@ -467,6 +467,27 @@ function validate_url($url) {
     return true;
 }
 
+if (!function_exists('get_primary_taxonomy_id')) {
+
+    function get_primary_taxonomy_id($post_id, $taxonomy) {
+        $prm_term = '';
+        if (class_exists('WPSEO_Primary_Term')) {
+            $wpseo_primary_term = new WPSEO_Primary_Term($taxonomy, $post_id);
+            $prm_term = $wpseo_primary_term->get_primary_term();
+        }
+        if (!is_object($wpseo_primary_term) || empty($prm_term)) {
+            $term = wp_get_post_terms($post_id, $taxonomy);
+            if (isset($term) && !empty($term)) {
+                return $term[0]->term_id;
+            } else {
+                return '';
+            }
+        }
+        return $wpseo_primary_term->get_primary_term();
+    }
+
+}
+
 //pull in custom elementor widgets
 require_once('elementor/make-widgets.php');
 
