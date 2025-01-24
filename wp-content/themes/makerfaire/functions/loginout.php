6a54ee6e24f618159e31c56ee7295500ce1ec12b
Rio Roth-Barreiro <rio@make.co>
2025-01-24 13:26:31 -0800

    update logout page html

--- wp-content/themes/makerfaire/functions/loginout.php
+++ wp-content/themes/makerfaire/functions/loginout.php
@@ -12,6 +12,16 @@ function login_add_footer() {
 add_action('login_footer', 'login_add_footer');
 
 function add_logout_link( $atts ){
-  return '<a href="' . wp_logout_url( home_url() ) . '" id="logoutPrompt">Do you really want to Logout?</a>';
+  return '<div class="site-content">
+            <div id="logoutPrompt">
+              <div class="left-side">
+                <img src="https://make.co/wp-content/universal-assets/v2/images/makey-spyglass.jpg" alt="Just making sure" />
+              </div>
+              <div class="right-side">
+                <p>You are attempting to log out of ' . get_bloginfo( 'name' ) . '</p>
+                <p>Do you really want to <a href="' . wp_logout_url( home_url() ) . '">log out?</a></p>
+              </div>
+            </div>
+          </div>';
 }
 add_shortcode( 'logout_link', 'add_logout_link' );
