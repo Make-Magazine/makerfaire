8aa3e0f20868ebecca0021851168b31f885f11a6
Alicia Williams <alicia@make.co>
2025-01-24 13:14:33 -0700

    display the selected makershed feed collection if it is set for a yb project category

--- wp-content/themes/makerfaire/functions/makershed_feed.php
+++ wp-content/themes/makerfaire/functions/makershed_feed.php
@@ -114,5 +114,25 @@ function makershedOutput($collection = MAKERSHED_DEFAULT_COLLECTION, $amount = "
     }
 }
 
+//add the makershed collection ACF field to the admin taxonomy page for mf-projects
+/**
+ * Add ACF thumbnail columns to Linen Category custom taxonomy
+ */
+function add_makershed_collection_column($columns) {
+    $columns['makershed_collection'] = __('Maker Shed Collection');    
+    return $columns;
+}
+add_filter('manage_edit-mf-project-cat_columns', 'add_makershed_collection_column');
 
+/**
+ * Output ACF thumbnail content in Linen Category custom taxonomy columns
+ */
+function collection_columns_content($content, $column_name, $term_id) {
+    if ('makershed_collection' == $column_name) {        
+        $primary_collection = get_field("makershed_collection", "mf-project-cat_" . $term_id);                
+        $content = $primary_collection;
 
+        }
+    return $content;
+}
+add_filter('manage_mf-project-cat_custom_column' , 'collection_columns_content' , 10 , 3);
